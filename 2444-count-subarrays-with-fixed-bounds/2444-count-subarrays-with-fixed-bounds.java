a289fa75354c21c09e7190b962672f40d0f0fcd0
Zening Chen <59945561+zeningc@users.noreply.github.com>
2022-10-16 00:24:18 -0500

    Time: 25 ms (25.00%), Space: 75.9 MB (50.00%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
@@ -1,73 +1,44 @@
 class Solution {
     public long countSubarrays(int[] nums, int minK, int maxK) {
-        if (minK > maxK)
-            return 0l;
         int lastBreak = -1;
         int n = nums.length;
         long ans = 0;
-        if (minK == maxK)   {
-            int idx = 0;
-            while (idx < n) {
-                if (nums[idx] != minK)  {
-                    lastBreak = idx;
-                    idx++;
-                    continue;
-                }
-                int j = idx + 1;
-                while (j < n && nums[idx] == nums[j])
-                    j++;
-                if (j == idx + 1){
-                    ans++;
-                }
-                else    {
-                   ans += comb(j - idx, 2) + j - idx; 
-                }
-                idx = j;
-            }
-        }
-        Deque<Integer> q1 = new LinkedList<>();
-        Deque<Integer> q2 = new LinkedList<>();
+        if (minK > maxK)
+            return 0l;
+        int lastMin = -1;
+        int lastMax = -1;
         
         for (int i = 0; i < n; i++) {
             if (nums[i] < minK || nums[i] > maxK)   {
-                q1.clear();
-                q2.clear();
+                lastMin = -1;
+                lastMax = -1;
                 lastBreak = i;
                 continue;
             }
-            if (nums[i] == minK)    {
-                q1.offerLast(i);
-                if (!q2.isEmpty())  {
-                    ans += (q2.peekLast() - lastBreak);
+            
+            if (minK == maxK)   {
+                if (nums[i] == minK)    {
+                    ans += (i - lastBreak);
+                }
+            }
+            else if (nums[i] == minK)    {
+                lastMin = i;
+                if (lastMax != -1)  {
+                    ans += (lastMax - lastBreak);
                 }
             }
             else if (nums[i] == maxK)    {
-                q2.offerLast(i);
-                if (!q1.isEmpty())  {
-                    ans += (q1.peekLast() - lastBreak);
+                lastMax = i;
+                if (lastMin != -1)  {
+                    ans += (lastMin - lastBreak);
                 }
             }
-            else    {
-                if (!q1.isEmpty() && !q2.isEmpty()) {
-                    ans += Math.min(q1.peekLast(), q2.peekLast()) - lastBreak;
-                }
+            else if (lastMin != -1 && lastMax != -1) {
+                    ans += Math.min(lastMin, lastMax) - lastBreak;
             }
-            // System.out.println(nums[i] + ": " + ans + " " + lastBreak);
         }
         
         return ans;
-    }
-    
-    long comb(int m,int n)  {
-        if(n==0)
-            return 1;
-        if (n==1) 
-            return m;
-        if(n>m/2)
-            return comb(m,m-n);
-        if(n>1)
-            return comb(m-1,n-1)+comb(m-1,n);  
-        return -1;
     }
 
 }
