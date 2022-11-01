da29fb6afe45bc6f558bd4d086d6b64787c0a0ea
Zening Chen <59945561+zeningc@users.noreply.github.com>
2022-11-01 17:35:16 -0400

    Time: 469 ms (43.43%), Space: 0B (100.00%) - LeetHub

--- 0615-average-salary-departments-vs-company/0615-average-salary-departments-vs-company.sql
+++ 0615-average-salary-departments-vs-company/0615-average-salary-departments-vs-company.sql
@@ -1,15 +1,14 @@
 # Write your MySQL query statement below
-with all_month_avg as (
-    select DATE_FORMAT(s.pay_date, '%Y-%m') pay_month, sum(s.amount) / count(s.amount) avg_salary 
-    from Employee e
-    join Salary s on e.employee_id = s.employee_id
-    group by month(s.pay_date)
-),
-dept_month_avg as (
-    select DATE_FORMAT(s.pay_date, '%Y-%m') pay_month, e.department_id, sum(s.amount) / count(s.amount) avg_salary 
+with dept_month_avg as (
+    select DATE_FORMAT(s.pay_date, '%Y-%m') pay_month, e.department_id, sum(s.amount) / count(s.amount) avg_salary, sum(s.amount) amt, count(s.amount) cnt 
     from Employee e
     join Salary s on e.employee_id = s.employee_id
     group by month(s.pay_date), e.department_id
+),
+all_month_avg as (
+    select pay_month, sum(amt) / sum(cnt) avg_salary
+    from dept_month_avg
+    group by pay_month
 )
 select d.pay_month, d.department_id, 
     case when d.avg_salary = a.avg_salary
@@ -20,3 +19,13 @@ select d.pay_month, d.department_id,
     end comparison
 from dept_month_avg d
 join all_month_avg a on d.pay_month = a.pay_month
+
+# with dept_month_avg as (
+#     select DATE_FORMAT(s.pay_date, '%Y-%m') pay_month, e.department_id, sum(s.amount) / count(s.amount) avg_salary, sum(s.amount) amt, count(s.amount) cnt 
+#     from Employee e
+#     join Salary s on e.employee_id = s.employee_id
+#     group by month(s.pay_date), e.department_id
+# )
+#     select pay_month, sum(amt) / sum(cnt) avg_salary
+#     from dept_month_avg
+#     group by pay_month, department_id
