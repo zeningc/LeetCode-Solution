dc4095ff81537fa448915d5e2ae05c4d59cdb42b
Zening Chen <59945561+zeningc@users.noreply.github.com>
2022-11-15 14:48:48 -0500

    Time: 13 ms (71.38%), Space: 51.5 MB (52.89%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -3,16 +3,13 @@ class Solution {
         int n = s.length();
         boolean[][] dp = new boolean[n + 1][n + 1];
         for (int i = n - 1; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
-                if (i == j) {
+            for (int j = i; j < Math.min(i + k + 1, n); j++) {
+                if (i == j)
                     dp[i][j] = true;
-                }
-                else if (s.charAt(i) == s.charAt(j))    {
-                    if (i == j - 1)
-                        dp[i][j] = true;
-                    else
-                        dp[i][j] = dp[i + 1][j - 1];
-                }
+                else if (j == i + 1)
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
+                else
+                    dp[i][j] = s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1];
             }
         }
         
