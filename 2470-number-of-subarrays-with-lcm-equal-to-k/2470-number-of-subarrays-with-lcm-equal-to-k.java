4f98498ec861df6aef215a4ce4af6cb11edcdc98
Zening Chen <59945561+zeningc@users.noreply.github.com>
2022-11-15 15:15:29 -0500

    Time: 14 ms (92.03%), Space: 46.2 MB (5.44%) - LeetHub

--- 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.java
+++ 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.java
@@ -1,33 +1,35 @@
 class Solution {
     public int subarrayLCM(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
         int ans = 0;
         for (int i = 0; i < n; i++) {
-            long l = nums[i];
-            if (l == (long)k){
-                ans++;
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            Map<Integer, Integer> m1 = new HashMap<>();
+            for (Map.Entry<Integer, Integer> e : map.entrySet())    {
+                int d = e.getKey();
+                int cnt = e.getValue();
+                int l = lcm(nums[i], d);
+                if (k == l)
+                    ans += cnt;
+                if (k % l == 0)
+                    m1.put(l, m1.getOrDefault(l, 0) + cnt);
             }
-            for (int j = i + 1; j < n && l <= (long)k; j++) {
-                l = lcm(l, nums[j]);
-                if (l == (long)k){
-                    ans++;
-                }
-            }
+            map = m1;
         }
         
         return ans;
     }
     
-    long gcd(long m, long n)   {
-        if (m < n)
+    int gcd(int m, int n)   {
+        if (n > m)
             return gcd(n, m);
         if (m % n == 0)
             return n;
-        
         return gcd(n, m % n);
     }
     
-    long lcm(long a, long b)   {
-        return a * b / gcd(a, b);
+    int lcm(int m, int n)   {
+        return m * n / gcd(m, n);
     }
 }
