f1e8699c008bc190668da17f01b4f4aa940fe9df
Zening Chen <59945561+zeningc@users.noreply.github.com>
2022-11-21 16:01:45 -0500

    Time: 6 ms (97.88%), Space: 41.7 MB (68.32%) - LeetHub

--- 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.java
+++ 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.java
@@ -1,19 +1,17 @@
 class Solution {
     public int unequalTriplets(int[] nums) {
-        int n = nums.length;
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int num : nums)
+            m.put(num, m.getOrDefault(num, 0) + 1);
         int ans = 0;
-        for (int i = 0; i < n; i++)   {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] == nums[j])
-                    continue;
-                for (int k = j + 1; k < n; k++) {
-                    if (nums[i] == nums[k] || nums[j] == nums[k])
-                        continue;
-                    ans++;
-                }
-            }
+        int lo = 0;
+        int hi = nums.length;
+        for (Map.Entry<Integer, Integer> e : m.entrySet())  {
+            int cnt = e.getValue();
+            hi -= cnt;
+            ans += lo * cnt * hi;
+            lo += cnt;
         }
-        
         return ans;
     }
 }
