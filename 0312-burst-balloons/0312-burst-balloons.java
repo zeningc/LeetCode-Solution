4cd625c1e84fde43550110b567392adaf0430b7b
Zening Chen <59945561+zeningc@users.noreply.github.com>
2022-11-22 01:41:28 -0500

    Time: 98 ms (69.91%), Space: 43 MB (54.28%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -5,11 +5,11 @@ class Solution {
         int ans = 0;
         for (int i = n - 1; i >= 0; i--)    {
             for (int j = i; j < n; j++) {
+                int leftReach = i == 0 ? 1 : nums[i - 1];
+                int rightReach = j == n - 1 ? 1 : nums[j + 1];
                 for (int k = i; k <= j; k++)    {
                     int left = k - 1 < i ? 0 : dp[i][k - 1];
                     int right = k + 1 > j ? 0 : dp[k + 1][j];
-                    int leftReach = i == 0 ? 1 : nums[i - 1];
-                    int rightReach = j == n - 1 ? 1 : nums[j + 1];
                     dp[i][j] = Math.max(dp[i][j], left + right + leftReach * nums[k] * rightReach);
                 }
             }
