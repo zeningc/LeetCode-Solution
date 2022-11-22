05335b8773b1fb29da894a5c019b2a968b7dcf09
Zening Chen <59945561+zeningc@users.noreply.github.com>
2022-11-21 21:10:14 -0500

    Time: 19 ms (31.77%), Space: 41.1 MB (81.77%) - LeetHub

--- 0813-largest-sum-of-averages/0813-largest-sum-of-averages.java
+++ 0813-largest-sum-of-averages/0813-largest-sum-of-averages.java
@@ -2,14 +2,6 @@ class Solution {
     public double largestSumOfAverages(int[] nums, int k) {
         int n = nums.length;
         double[][] dp = new double[n + 1][k + 1];
-        double[][] avg = new double[n + 1][n + 1];
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += nums[j];
-                avg[i + 1][j + 1] = (double)sum / (j - i + 1);
-            }
-        }
         
         for (int i = 0; i <= n; i++)
             Arrays.fill(dp[i], (double)(Integer.MIN_VALUE / 2));
@@ -18,8 +10,10 @@ class Solution {
         
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= Math.min(k, i); j++)   {
-                for (int x = j; x <= i; x++)    {
-                    dp[i][j] = Math.max(dp[i][j], dp[x - 1][j - 1] + avg[x][i]);
+                int sum = 0;
+                for (int x = i; x <= n; x++)    {
+                    sum += nums[x - 1];
+                    dp[x][j] = Math.max(dp[x][j], dp[i - 1][j - 1] + (double)sum / (x - i + 1));
                 }
             }
         }
