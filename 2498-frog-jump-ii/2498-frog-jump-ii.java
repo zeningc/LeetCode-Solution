526ee249cc2cdb0829c3951d7ccb81c6d4603e22
Zening Chen <59945561+zeningc@users.noreply.github.com>
2022-12-16 14:05:45 -0500

    Time: 3 ms (82.81%), Space: 55.3 MB (97.51%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.java
+++ 2498-frog-jump-ii/2498-frog-jump-ii.java
@@ -1,33 +1,38 @@
 class Solution {
     public int maxJump(int[] stones) {
         int ans = 0;
-        int n = stones.length;
-        if (n <= 3) {
-            return stones[n - 1];
-        }
         int i = 0;
-        int last = 0;
-        while (i != n - 1)   {
-            last = i;
-            if (i >= 2)  {
+        int j = 0;
+        int n = stones.length;
+        while (i != n - 1)  {
+            if (i + 2 <= n - 1) {
+                i += 2;
                 ans = Math.max(ans, stones[i] - stones[i - 2]);
+                if (i == n - 1) {
+                    ans = Math.max(ans, stones[n - 1] - stones[n - 2]);
+                    i = n - 2;
+                    break;
+                }
             }
-            i = Math.min(i + 2, n - 1);
+            else    {
+                ans = Math.max(ans, stones[n - 1] - stones[i]);
+                i = n - 1;
+                break;
+            }
         }
-        ans = Math.max(ans, stones[i] - stones[last]);
-        if (last == n - 3){
-            i = n - 2;
-            ans = Math.max(ans, stones[n - 1] - stones[n - 2]);
-        }
-
+        
         while (i != 0)  {
-            last = i;
-            if (i <= n - 3)  {
+            if (i - 2 >= 0) {
+                i -= 2;
                 ans = Math.max(ans, stones[i + 2] - stones[i]);
             }
-            i = Math.max(0, i - 2);
+            else    {
+                ans = Math.max(ans, stones[i] - stones[i - 1]);
+                break;
+            }
         }
-        ans = Math.max(ans, stones[last] - stones[0]);
+        
         return ans;
+        
     }
 }
