b6f7e826cf33283d2a2a803213f595eb079f5ab8
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-06-25 23:26:56 -0500

    Time: 28 ms (32.98%), Space: 43.7 MB (29.01%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
@@ -1,76 +1,43 @@
 class Solution {
     public int countCompleteComponents(int n, int[][] edges) {
-        int[] edgeCnt = new int[n];
-        UnionFind uf = new UnionFind(n);
+        Map<Integer, List<Integer>> map = new HashMap<>();
         for (int[] edge : edges)    {
             int u = edge[0];
             int v = edge[1];
-            uf.union(u, v);
-            edgeCnt[u]++;
-            edgeCnt[v]++;
+            if (!map.containsKey(u))
+                map.put(u, new LinkedList<>());
+            if (!map.containsKey(v))
+                map.put(v, new LinkedList<>());
+            map.get(u).add(v);
+            map.get(v).add(u);
         }
-        
-        Map<Integer, List<Integer>> union = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            int root = uf.find(i);
-            if (!union.containsKey(root))
-                union.put(root, new LinkedList<>());
-            union.get(root).add(i);
-        }
-        
         int ans = 0;
-        for (Map.Entry<Integer, List<Integer>> e : union.entrySet())   {
-            int k = e.getKey();
-            List<Integer> v = e.getValue();
-            boolean complete = true;
-            for (int i : v) {
-                if (edgeCnt[i] != uf.cnt[k] - 1){
-                    complete = false;
-                    break;
+        boolean[] vis = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (vis[i])
+                continue;
+            Deque<Integer> q = new LinkedList<>();
+            q.offer(i);
+            int visCnt = 0;
+            int edgeCnt = 0;
+            while (!q.isEmpty())    {
+                int u = q.poll();
+                if (vis[u])
+                    continue;
+                vis[u] = true;
+                visCnt++;
+                if (!map.containsKey(u))
+                    continue;
+                edgeCnt += map.get(u).size();
+                for (int v : map.get(u))    {
+                    if (!vis[v])
+                        q.offer(v);
                 }
             }
-            if (complete)
+            edgeCnt /= 2;
+            if (visCnt * (visCnt - 1) / 2 == edgeCnt)
                 ans++;
         }
-        
         return ans;
-    }
-}
-
-class UnionFind {
-    int[] parents;
-    int[] cnt;
-    
-    public UnionFind(int n)    {
-        parents = new int[n];
-        cnt = new int[n];
-        for (int i = 0; i < n; i++) {
-            parents[i] = i;
-            cnt[i] = 1;
-        }
-    }
-    
-    int find(int node)  {
-        if (parents[node] != node)  {
-            parents[node] = find(parents[node]);
-        }
-        return parents[node];
-    }
-    
-    void union(int p, int q)    {
-        int pRoot = find(p);
-        int qRoot = find(q);
-        
-        if (pRoot == qRoot)
-            return;
-        
-        if (cnt[pRoot] > cnt[qRoot])  {
-            int t = pRoot;
-            pRoot = qRoot;
-            qRoot = t;
-        }
-        
-        parents[pRoot] = qRoot;
-        cnt[qRoot] += cnt[pRoot];
     }
 }
