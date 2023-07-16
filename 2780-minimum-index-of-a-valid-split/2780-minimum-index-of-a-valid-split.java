39369ed5ce6946ce8e4a3a79b9238ed1d20e606d
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-07-16 12:45:39 -0500

    Time: 43 ms (20.00%), Space: 59.9 MB (20.00%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.java
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.java
@@ -5,26 +5,25 @@ class Solution {
         for (int num : nums)
             freq.put(num, freq.getOrDefault(num, 0) + 1);
         int dominant = -1;
+        int maxFreq = -1;
         for (Map.Entry<Integer, Integer> e : freq.entrySet())   {
             int k = e.getKey();
             int v = e.getValue();
             if (v * 2 > n)  {
                 dominant = k;
+                maxFreq = v;
                 break;
             }
         }
         
         int[] presum = new int[n + 1];
-        int[] sufsum = new int[n + 1];
         for (int i = 1; i <= n; i++)
             presum[i] = presum[i - 1] + (nums.get(i - 1) == dominant ? 1 : 0);
-        for (int i = n - 1; i >= 0; i--)
-            sufsum[i] = sufsum[i + 1] + (nums.get(i) == dominant ? 1 : 0);
         int ans = -1;
         
         for (int i = 0; i < n - 1; i++) {
             int leftCnt = presum[i + 1];
-            int rightCnt = sufsum[i + 1];
+            int rightCnt = maxFreq - leftCnt;
             int leftLen = i + 1;
             int rightLen = n - i - 1;
             if (leftCnt * 2 > leftLen && rightCnt * 2 > rightLen)   {
