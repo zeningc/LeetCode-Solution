0e75b27659c09b30b54252d7189598a5d18370b4
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-07-16 12:43:01 -0500

    Time: 220 ms (60.00%), Space: 185.3 MB (20.00%) - LeetHub

---
+++ 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int longestValidSubstring(String word, List<String> forbidden) {
+        Trie root = new Trie('#', false);
+        Set<String> set = new HashSet<>(forbidden);
+        for (String s : set)
+            Trie.insert(root, s);
+        int n = word.length();
+        int last = n;
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--)    {
+            Trie node = root;
+            for (int j = i; j < Math.min(n, i + 10); j++)    {
+                if (node.children[word.charAt(j) - 'a'] == null)
+                    break;
+                node = node.children[word.charAt(j) - 'a'];
+                if (node.isBan)
+                    last = Math.min(last, j);
+            }
+            ans = Math.max(ans, last - i);
+        }
+        
+        return ans;
+    }
+    
+    
+}
+
+class Trie  {
+    char c;
+    boolean isBan;
+    Trie[] children;
+    String word;
+    
+    public Trie(char c, boolean isBan)  {
+        this.c = c;
+        this.isBan = isBan;
+        this.children = new Trie[26];
+    }
+    
+    static void insert(Trie root, String s)   {
+        Trie node = root;
+        for (char c : s.toCharArray())  {
+            if (node.children[c - 'a'] == null)
+                node.children[c - 'a'] = new Trie(c, false);
+            node = node.children[c - 'a'];
+        }
+        node.isBan = true;
+        node.word = s;
+    }
+}
