f0480763d8b9ba4b0dd20932033b0251fe79ab92
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-07-22 12:49:53 -0500

    Time: 5 ms (10.76%), Space: 43.9 MB (69.51%) - LeetHub

--- 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.java
+++ 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.java
@@ -1,10 +1,7 @@
 class Solution {
     public int getLastMoment(int n, int[] left, int[] right) {
-        int ret = 0;
-        for (int num : left)
-            ret = Math.max(ret, num);
-        for (int num : right)
-            ret = Math.max(ret, n - num);
-        return ret;
+        int maxLeft = IntStream.of(left).max().orElse(0);
+        int maxRight = IntStream.of(right).map(num -> n - num).max().orElse(0);
+        return Math.max(maxLeft, maxRight);
     }
 }
