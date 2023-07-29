9908422850d173695ecd153ed2d43e1d1e9cd05f
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-07-29 14:32:20 -0500

    Time: 2 ms (100.00%), Space: 40.5 MB (99.49%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -1,14 +1,14 @@
 class Solution {
     public int distinctSubseqII(String s) {
         long[] last = new long[26];
-        long dp = 1;
+        long dp = 0;
         int mod = (int)1e9 + 7;
         for (char c : s.toCharArray())  {
             long preDp = dp;
-            dp = (preDp * 2 - last[c - 'a'] + mod) % mod;
-            last[c - 'a'] = preDp % mod;
+            dp = (preDp * 2 + 1 - last[c - 'a'] + mod) % mod;
+            last[c - 'a'] = preDp + 1 % mod;
         }
         
-        return (int)((dp + mod - 1) % mod);
+        return (int)(dp);
     }
 }
