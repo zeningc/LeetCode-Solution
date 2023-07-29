879ce6ea98741e4119ddfb184f56ce651cddee41
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-07-29 14:25:03 -0500

    Time: 2 ms (100.00%), Space: 40.6 MB (99.49%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -1,39 +1,14 @@
 class Solution {
     public int distinctSubseqII(String s) {
-        int n = s.length();
+        long[] last = new long[26];
+        long dp = 1;
         int mod = (int)1e9 + 7;
-        int[][] lastSeen = new int[n][26];
-        for (int i = 0; i < 26; i++)
-            lastSeen[0][i] = -1;
-        for (int i = 1; i < s.length(); i++)   {
-            char c = s.charAt(i - 1);
-            for (int j = 0; j < 26; j++)    {
-                lastSeen[i][j] = lastSeen[i - 1][j];
-            }
-            lastSeen[i][c - 'a'] = i - 1;
-        }
-        int[] dp = new int[n];
-        dp[0] = 2;
-        for (int i = 1; i < n; i++) {
-            char c = s.charAt(i);
-            dp[i] = (dp[i - 1] * 2) % mod;
-            if (lastSeen[i][c - 'a'] != -1) {
-                if (lastSeen[i][c - 'a'] == 0)  {
-                    dp[i] = (dp[i] + mod - 1) % mod;
-                }
-                else    {
-                    dp[i] = (dp[i] + mod - dp[lastSeen[i][c - 'a'] - 1]) % mod;
-                }
-            }
+        for (char c : s.toCharArray())  {
+            long preDp = dp;
+            dp = (preDp * 2 - last[c - 'a'] + mod) % mod;
+            last[c - 'a'] = preDp % mod;
         }
         
-        return (dp[n - 1] - 1 + mod) % mod;
+        return (int)((dp + mod - 1) % mod);
     }
 }
-
-/*
-lee
-[] l e le
-e  le  ee  lee
-[] l   e
-*/
