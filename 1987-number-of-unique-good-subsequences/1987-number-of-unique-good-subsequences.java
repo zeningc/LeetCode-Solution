d6feb2df9f2542121939dd666edce423da0b6398
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-07-29 15:32:41 -0500

    Time: 9 ms (100.00%), Space: 43.9 MB (98.15%) - LeetHub

--- 1987-number-of-unique-good-subsequences/1987-number-of-unique-good-subsequences.java
+++ 1987-number-of-unique-good-subsequences/1987-number-of-unique-good-subsequences.java
@@ -8,12 +8,14 @@ class Solution {
             return 1;
         long[] last = new long[2];
         int mod = (int)1e9 + 7;
-        long dp = 1;
-        i++;
+        long dp = 0;
         for (; i < n; i++)    {
             char c = binary.charAt(i);
             long preDp = dp;
-            dp = (preDp * 2 + mod - last[c - '0']) % mod;
+            if (preDp == 0)
+                dp = 1;
+            else
+                dp = (preDp * 2 + mod - last[c - '0']) % mod;
             last[c - '0'] = preDp;
         }
         return (int)((dp + mod + (binary.contains("0") ? 1 : 0)) % mod);
