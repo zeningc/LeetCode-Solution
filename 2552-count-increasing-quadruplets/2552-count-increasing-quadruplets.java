a764426d86ec30dfcb8ee0a06186ec49ab60d6c6
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-07-31 23:58:29 -0500

    Time: 48 ms (99.60%), Space: 43.2 MB (94.74%) - LeetHub

--- 2552-count-increasing-quadruplets/2552-count-increasing-quadruplets.java
+++ 2552-count-increasing-quadruplets/2552-count-increasing-quadruplets.java
@@ -1,34 +1,20 @@
 class Solution {
     public long countQuadruplets(int[] nums) {
         int n = nums.length;
-        int[][] pre = new int[n][n + 1];
-        int[][] post = new int[n][n + 1];
-        for (int i = 0; i < n; i++) {
-            int v = nums[i];
-            for (int j = 0; j < n; j++) {
-                pre[j][v] = (j >= 1 ? pre[j - 1][v] : 0);
-                if (nums[j] < v)
-                    pre[j][v]++;
-            }
-        }
-        
-        for (int i = 0; i < n; i++) {
-            int v = nums[i];
-            for (int j = n - 1; j >= 0; j--) {
-                post[j][v] = (j < n - 1 ? post[j + 1][v] : 0);
-                if (nums[j] > v)
-                    post[j][v]++;
-            }
-        }
+        long[] cnt = new long[n];
         long ans = 0;
-        for (int k = 0; k < n - 1; k++) {
-            for (int j = 1; j < k; j++) {
-                if (nums[k] > nums[j])
-                    continue;
-                ans += (long)post[k + 1][nums[j]] * pre[j - 1][nums[k]];
+        for (int j = 0; j < n; j++) {
+            int lowerCnt = 0;
+            for (int i = 0; i < j; i++) {
+                if (nums[i] < nums[j])  {
+                    lowerCnt++;
+                    ans += cnt[i];
+                }
+                else    {
+                    cnt[i] += lowerCnt;
+                }
             }
         }
-        
         return ans;
     }
 }
