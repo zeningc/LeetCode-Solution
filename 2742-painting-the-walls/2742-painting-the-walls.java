9d808bfbb57e51ff29d67b2a94cd2fb5d64054ec
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-08-07 23:40:43 -0500

    Time: 16 ms (78.04%), Space: 44.2 MB (70.53%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.java
+++ 2742-painting-the-walls/2742-painting-the-walls.java
@@ -1,26 +1,17 @@
 class Solution {
     public int paintWalls(int[] cost, int[] time) {
-        int n = cost.length;
-        int[][] dp = new int[n + 1][2 * n + 1];
+        int n = time.length;
+        int[][] dp = new int[n + 1][n + 1];
         for (int i = 0; i <= n; i++)
             Arrays.fill(dp[i], Integer.MAX_VALUE / 2);
-        
-        dp[0][n] = 0;
-        for (int i = 0; i < n; i++)    {
-            for (int j = 0; j <= 2 * n; j++)    {
-                if (dp[i][j] >= Integer.MAX_VALUE / 2)
-                    continue;
-                int cappedJ = Math.max(0, j - 1);
-                dp[i + 1][cappedJ] = Math.min(dp[i + 1][cappedJ], dp[i][j]);
-                cappedJ = Math.min(j + time[i], 2 * n);
-                dp[i + 1][cappedJ] = Math.min(dp[i + 1][cappedJ], dp[i][j] + cost[i]);
+        dp[0][0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= n; j++)    {
+                int cap = Math.min(n, j + time[i] + 1);
+                dp[i + 1][cap] = Math.min(dp[i + 1][cap], dp[i][j] + cost[i]);
+                dp[i + 1][j] = Math.min(dp[i + 1][j], dp[i][j]);
             }
         }
-        
-        int ans = Integer.MAX_VALUE;
-        for (int i = n; i <= 2 * n; i++)
-            ans = Math.min(ans, dp[n][i]);
-        return ans;
-        
+        return dp[n][n];
     }
 }
