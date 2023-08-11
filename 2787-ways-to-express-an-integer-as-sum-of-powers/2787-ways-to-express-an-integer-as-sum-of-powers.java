1ba4ac8f6b44b6dc74e253a9929dd8a5c0b2d748
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-08-10 23:36:48 -0500

    Time: 73 ms (77.74%), Space: 42.5 MB (86.45%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.java
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.java
@@ -1,20 +1,19 @@
 class Solution {
     public int numberOfWays(int n, int x) {
-        long[][] dp = new long[n + 1][n + 1];
+        long[] dp = new long[n + 1];
         int[] element = new int[n + 1];
         int mod = (int)1e9 + 7;
         for (int i = 1; i <= n; i++)
             element[i] = (int)Math.pow(i, x);
-        dp[0][0] = 1;
+        dp[0] = 1;
         
         for (int i = 1; i <= n; i++)    {
-            for (int j = 0; j <= n; j++)    {
-                dp[i][j] = dp[i - 1][j];
+            for (int j = n; j >= 0; j--)    {
                 if (j >= element[i])
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j - element[i]]) % mod;
+                    dp[j] = (dp[j] + dp[j - element[i]]) % mod;
             }
         }
         
-        return (int)(dp[n][n] % mod);
+        return (int)(dp[n] % mod);
     }
 }
