7f6c1c2cf8ff5a479c4c63b300e100b62dd42d3b
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-08-11 00:23:58 -0500

    Time: 126 ms (10.88%), Space: 50.9 MB (17.62%) - LeetHub

---
+++ 2809-minimum-time-to-make-array-sum-at-most-x/2809-minimum-time-to-make-array-sum-at-most-x.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minimumTime(List<Integer> nums1, List<Integer> nums2, int x) {
+        int n = nums1.size();
+        int[][] dp = new int[n + 1][n + 1];
+        int[] presum = new int[n + 1];
+        Integer[] idx = new Integer[n];
+        for (int i = 0; i < n; i++)
+            idx[i] = i;
+        Arrays.sort(idx, (a, b) -> nums2.get(a) - nums2.get(b));
+        for (int i = 1; i <= n; i++)
+            presum[i] = presum[i - 1] + nums2.get(idx[i - 1]);
+        int sum = 0;
+        for (int num : nums1)
+            sum += num;
+        dp[0][0] = 0;
+        
+        for (int i = 1; i <= n; i++)    {
+            int k = idx[i - 1];
+            for (int j = 0; j <= n; j++)    {
+                dp[i][j] = Integer.MAX_VALUE / 2;
+                dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + nums1.get(k) + j * nums2.get(k));
+                if (j >= 1)
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1] + presum[i - 1]);
+            }
+        }
+        
+        for(int i = 0; i <= n; i++)
+            if (dp[n][i] <= x)
+                return i;
+        
+        return -1;
+    }
+}
