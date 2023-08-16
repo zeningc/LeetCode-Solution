b3161ded069df432075b81aa30b18d00fc45cede
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-08-16 17:14:13 -0500

    Time: 15 ms (40.34%), Space: 42.8 MB (97.31%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -1,35 +1,28 @@
 class Solution {
     public int swimInWater(int[][] grid) {
-        int[][] direction = new int[][] {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        int m = grid.length;
-        int n = grid[0].length;
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
-        boolean[][] visited = new boolean[m][n];
+        int n = grid.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
         pq.offer(new int[] {0, 0, grid[0][0]});
-        
+        int[][] dir = new int[][] {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        boolean[][] vis = new boolean[n][n];
         while (!pq.isEmpty())   {
-            int[] curr = pq.poll();
-            int x = curr[0];
-            int y = curr[1];
-            int val = curr[2];
-            
-            if (x == m - 1 && y == n - 1)    {
-                return val;
-            }
-            
-            for (int i = 0; i < 4; i++) {
-                int newX = x + direction[i][0];
-                int newY = y + direction[i][1];
-                if (newX < 0 || newX > m - 1 || newY < 0 || newY > n - 1 || visited[newX][newY])    {
+            int[] poll = pq.poll();
+            int x = poll[0];
+            int y = poll[1];
+            int h = poll[2];
+            if (vis[x][y])
+                continue;
+            vis[x][y] = true;
+            if (x == n - 1 && y == n - 1)
+                return h;
+            for (int[] d : dir) {
+                int nx = d[0] + x;
+                int ny = d[1] + y;
+                if (nx < 0 || nx > n - 1 || ny < 0 || ny > n - 1 || vis[nx][ny])
                     continue;
-                }
-                
-                visited[newX][newY] = true;
-                
-                pq.offer(new int[] {newX, newY, Math.max(val, grid[newX][newY])});
+                pq.offer(new int[] {nx, ny, Math.max(h, grid[nx][ny])});
             }
         }
-        
         return -1;
     }
 }
