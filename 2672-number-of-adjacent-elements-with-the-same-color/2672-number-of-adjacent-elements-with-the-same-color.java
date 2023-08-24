250647e9bde0089f231510ea17500fc2f5f2f986
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-08-23 21:36:53 -0500

    Time: 5 ms (81.63%), Space: 95.5 MB (75.51%) - LeetHub

--- 2672-number-of-adjacent-elements-with-the-same-color/2672-number-of-adjacent-elements-with-the-same-color.java
+++ 2672-number-of-adjacent-elements-with-the-same-color/2672-number-of-adjacent-elements-with-the-same-color.java
@@ -2,73 +2,29 @@ class Solution {
     public int[] colorTheArray(int n, int[][] queries) {
         int[] colors = new int[n];
         int[] ans = new int[queries.length];
-        int cur = 0;
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int cnt = 0;
         for (int i = 0; i < queries.length; i++)    {
-            int idx = queries[i][0];
-            int color = queries[i][1];
-            if (colors[idx] == color) {
-                ans[i] = cur;
+            int[] query = queries[i];
+            int idx = query[0];
+            int color = query[1];
+            if (color == colors[idx])   {
+                ans[i] = cnt;
                 continue;
             }
-            Integer lo = map.floorKey(idx);
-            boolean connectToLeft = false;
-            if (lo != null && colors[lo] != color) {
-                int hi = map.get(lo);
-                if (hi == idx)  {
-                    if (lo == hi)   {
-                        map.remove(lo);
-                    }
-                    else    {
-                        map.put(lo, hi - 1);
-                        cur--;
-                    }
-                }
-                else if (hi > idx)  {
-                    if (lo == idx)  {
-                        map.remove(lo);
-                        map.put(lo + 1, hi);
-                        cur--;
-                    }
-                    else    {
-                        map.put(lo, idx - 1);
-                        map.put(idx + 1, hi);
-                        cur-=2;
-                    }
-                }
-            }
             
-            lo = map.floorKey(idx);
-            if (lo != null && map.get(lo) >= idx - 1 && colors[idx - 1] == color)  {
-                connectToLeft = true;
-                int hi = map.get(lo);
-                if (hi == idx - 1)  {
-                    map.put(lo, idx);
-                    cur++;
-                }
-            }
+            if (idx > 0 && colors[idx] != 0 && colors[idx] == colors[idx - 1])
+                cnt--;
+            if (idx < n - 1 && colors[idx + 1] != 0 && colors[idx] == colors[idx + 1])
+                cnt--;
             
-            boolean connectToRight = false;
-            if (idx < n - 1 && colors[idx + 1] == color)    {
-                connectToRight = true;
-                int hi = map.get(idx + 1);
-                map.remove(idx + 1);
-                if (connectToLeft)  {
-                    map.put(lo, hi);
-                }
-                else    {
-                    map.put(idx, hi);
-                }
-                cur++;
-            }
+            if (idx > 0 && color == colors[idx - 1])
+                cnt++;
+            if (idx < n - 1 && color == colors[idx + 1])
+                cnt++;
             
-            if (!connectToLeft && !connectToRight)  {
-                map.put(idx, idx);
-            }
-            ans[i] = cur;
             colors[idx] = color;
+            ans[i] = cnt;
         }
-        
         return ans;
     }
 }
