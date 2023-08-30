743dac954ccf0e30e631431e702f629cb257d34d
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-08-29 19:00:07 -0500

    Time: 67 ms (13.77%), Space: 54.9 MB (36.11%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,33 +1,22 @@
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
         int n = startTime.length;
-        int[][] jobs = new int[n][3];
-        for (int i = 0; i < n; i++) {
-            jobs[i][0] = startTime[i];
-            jobs[i][1] = endTime[i];
-            jobs[i][2] = profit[i];
+        TreeMap<Integer, Integer> dp = new TreeMap<>();
+        List<int[]> jobs = new ArrayList<>(n);
+        for (int i = 0; i < n; i++)
+            jobs.add(new int[] {startTime[i], endTime[i], profit[i]});
+        Collections.sort(jobs, (a, b) -> a[1] - b[1]);
+        dp.put(-1, 0);
+        
+        for (int[] job : jobs)  {
+            int start = job[0];
+            int end = job[1];
+            int earn = job[2];
+            dp.put(end, Math.max(dp.floorEntry(end).getValue(), dp.floorEntry(start).getValue() + earn));
         }
         
-        Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
-        
-        int[] dp = new int[n];
-        dp[0] = jobs[0][2];
-        for (int i = 1; i < n; i++)    {
-            int lo = 0;
-            int hi = i - 1;
-            while (lo <= hi)    {
-                int mid = lo + (hi - lo) / 2;
-                if (jobs[mid][1] > jobs[i][0])  {
-                    hi = mid - 1;
-                }
-                else    {
-                    lo = mid + 1;
-                }
-            }
-            int j = hi;
-            dp[i] = Math.max((j >= 0 ? dp[j] : 0) + jobs[i][2], dp[i - 1]);
-        }
-        
-        return dp[n - 1];
+        return dp.lastEntry().getValue();
     }
 }
+
+// dp[i] = max(dp[i], dp[startTime] + profit)
