ec2dea65e571f258a116aa249efe4687d45ce9e8
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-09-05 21:54:01 -0500

    Time: 66 ms (21.81%), Space: 90.6 MB (12.44%) - LeetHub

--- 2813-maximum-elegance-of-a-k-length-subsequence/2813-maximum-elegance-of-a-k-length-subsequence.java
+++ 2813-maximum-elegance-of-a-k-length-subsequence/2813-maximum-elegance-of-a-k-length-subsequence.java
@@ -1,36 +1,36 @@
 class Solution {
     public long findMaximumElegance(int[][] items, int k) {
+        Map<Integer, Integer> freq = new HashMap<>();
         Arrays.sort(items, (a, b) -> b[0] - a[0]);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        Map<Integer, Integer> map = new HashMap<>();
-        long profit = 0;
+        Queue<Integer> pq = new PriorityQueue<>((a, b) -> items[a][0] - items[b][0]);
         long ans = 0;
         for (int i = 0; i < k; i++) {
-            pq.offer(items[i]);
-            map.put(items[i][1], map.getOrDefault(items[i][1], 0) + 1);
+            pq.offer(i);
+            freq.put(items[i][1], freq.getOrDefault(items[i][1], 0) + 1);
             ans += items[i][0];
         }
-        profit = ans;
-        ans += (long)map.size() * map.size();
+        long profit = ans;
+        ans += (long)freq.size() * freq.size();
+        
         for (int i = k; i < items.length; i++)  {
-            if (map.containsKey(items[i][1]))
+            boolean findReplace = false;
+            if (freq.containsKey(items[i][1]))
                 continue;
             while (!pq.isEmpty())   {
-                int[] poll = pq.poll();
-                int c = poll[1];
-                int val = poll[0];
-                if (map.getOrDefault(c, 0) <= 1)
+                int j = pq.poll();
+                if (freq.get(items[j][1]) == 1)
                     continue;
-                profit -= val;
-                map.put(c, map.getOrDefault(c, 0) - 1);
-                profit += items[i][0];
-                map.put(items[i][1], map.getOrDefault(items[i][1], 0) + 1);
-                pq.offer(items[i]);
+                freq.put(items[j][1], freq.get(items[j][1]) - 1);
+                findReplace = true;
+                profit -= items[j][0];
                 break;
             }
-            
-            ans = Math.max(profit + (long)map.size() * map.size(), ans);
-                
+            if (!findReplace)
+                break;
+            pq.offer(i);
+            profit += items[i][0];
+            freq.put(items[i][1], freq.getOrDefault(items[i][1], 0) + 1);
+            ans = Math.max(ans, profit + (long)freq.size() * freq.size());
         }
         
         return ans;
