7e82b1b30db778ad984e88c42935134ed28ffd4b
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-09-11 16:04:01 -0500

    Time: 245 ms (57.61%), Space: 129.1 MB (69.30%) - LeetHub

--- 2836-maximize-value-of-function-in-a-ball-passing-game/2836-maximize-value-of-function-in-a-ball-passing-game.java
+++ 2836-maximize-value-of-function-in-a-ball-passing-game/2836-maximize-value-of-function-in-a-ball-passing-game.java
@@ -1,35 +1,40 @@
 class Solution {
     public long getMaxFunctionValue(List<Integer> receiver, long k) {
-        int n = receiver.size();
-        int m = 35;
-        long[][][] dp = new long[m][n][2];
-        
-        for (int j = 0; j < n; j++) {
-            dp[0][j][0] = receiver.get(j).longValue();
-            dp[0][j][1] = receiver.get(j).longValue();
-        }
-        
-        for (int i = 1; i < m; i++)    {
-            for (int j = 0; j < n; j++) {
-                int x = (int)dp[i - 1][j][0];
-                dp[i][j][0] = dp[i - 1][x][0];
-                dp[i][j][1] = dp[i - 1][x][1] + dp[i - 1][j][1];
-            }
-        }
-        
         long ans = 0;
-        for (int j = 0; j < n; j++) {
-            long profit = j;
-            int now = j;
-            for (int i = 0; i < m; i++) {
-                if ((k & (1l << i)) == 0)
-                    continue;
-                profit += dp[i][now][1];
-                now = (int)dp[i][now][0];
+        int m = 35;
+        int n = receiver.size();
+        int[][] jump = new int[n][m];
+        long[][] val = new long[n][m];
+        
+        for (int i = 0; i < n; i++) {
+            jump[i][0] = receiver.get(i);
+            val[i][0] = receiver.get(i);
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int nextHop = jump[j][i - 1];
+                jump[j][i] = jump[nextHop][i - 1];
+                val[j][i] = val[j][i - 1] + val[nextHop][i - 1];
             }
-            ans = Math.max(ans, profit);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            long curVal = i;
+            int cur = i;
+            for (int j = 0; j < m; j++) {
+                if ((k & (1L << j)) != 0L) {
+                    curVal += val[cur][j];
+                    cur = jump[cur][j];
+                }
+            }
+            ans = Math.max(ans, curVal);
         }
         
         return ans;
     }
 }
+
+
+// dp[i][j] i x^j dest
+// dp1[i][j] i x^j value - id
