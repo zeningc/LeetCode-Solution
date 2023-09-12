9fe6ced7c47a85b22c40b74f823c8aacdd40c53d
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-09-12 18:47:36 -0500

    Time: 90 ms (41.19%), Space: 59.1 MB (20.48%) - LeetHub

--- 2846-minimum-edge-weight-equilibrium-queries-in-a-tree/2846-minimum-edge-weight-equilibrium-queries-in-a-tree.java
+++ 2846-minimum-edge-weight-equilibrium-queries-in-a-tree/2846-minimum-edge-weight-equilibrium-queries-in-a-tree.java
@@ -1,84 +1,92 @@
 class Solution {
     public int[] minOperationsQueries(int n, int[][] edges, int[][] queries) {
-        Map<Integer, List<int[]>> g = new HashMap<>();
+        int c = 26;
+        int[][] prefix = new int[n][c + 1];
+        int[] parent = new int[n];
+        int[] depth = new int[n];
+        Map<Integer, List<int[]>> graph = new HashMap<>();
         for (int[] edge : edges)    {
             int u = edge[0];
             int v = edge[1];
             int w = edge[2];
-            if (!g.containsKey(u))
-                g.put(u, new LinkedList<>());
-            if (!g.containsKey(v))
-                g.put(v, new LinkedList<>());
-            g.get(u).add(new int[] {v, w});
-            g.get(v).add(new int[] {u, w});
+            if (!graph.containsKey(u))
+                graph.put(u, new LinkedList<>());
+            if (!graph.containsKey(v))
+                graph.put(v, new LinkedList<>());
+            graph.get(u).add(new int[] {v, w});
+            graph.get(v).add(new int[] {u, w});
         }
-        int[] d = new int[n];
-        int[][] p = new int[n][27];
-        int m = 15;
-        int[][] dp = new int[n][m];
-        dfs(g, d, dp, p, 0, 0, 0);
-        for (int j = 1; j < m; j++)
-            for (int i = 0; i < n; i++)
-                dp[i][j] = dp[dp[i][j - 1]][j - 1];
-
+        
+        dfs(graph, prefix, parent, depth, 0, 0, 0);
+        
+        int[][] dp = new int[n][32];
+        
+        for (int i = 0; i < n; i++)
+            dp[i][0] = parent[i];
+        
+        for (int i = 1; i < 32; i++)
+            for (int j = 0; j < n; j++)
+                dp[j][i] = dp[dp[j][i - 1]][i - 1];
+        
         int[] ans = new int[queries.length];
-        for (int j = 0; j < queries.length; j++)    {
-            int[] query = queries[j];
-            int x = query[0];
-            int y = query[1];
-            int ancestor = lca(d, dp, m, x, y);
-            int maxFreq = -1;
-            int len = d[x] + d[y] - 2 * d[ancestor];
-            for (int i = 1; i <= 26; i++)   {
-                int freq = p[x][i] + p[y][i] - 2 * p[ancestor][i];
+        for (int i = 0; i < queries.length; i++)    {
+            int a = queries[i][0];
+            int b = queries[i][1];
+            int anc = lca(dp, depth, a, b);
+            int maxFreq = 0;
+            for (int j = 1; j <= 26; j++)   {
+                int freq = prefix[a][j] + prefix[b][j] - 2 * prefix[anc][j];
                 maxFreq = Math.max(maxFreq, freq);
             }
-            ans[j] = len - maxFreq;
+            int len = depth[a] + depth[b] - 2 * depth[anc];
+            ans[i] = len - maxFreq;
         }
         
         return ans;
     }
     
-    void dfs(Map<Integer, List<int[]>> g, int[] d, int[][] dp, int[][] p, int parent, int node, int depth)  {
-        d[node] = depth;
-        dp[node][0] = parent;
-        if (!g.containsKey(node))
-            return;
-        for (int[] next : g.get(node))   {
-            if (next[0] == parent)
-                continue;
-            int v = next[0];
-            int w = next[1];
-            p[v] = p[node].clone();
-            p[v][w]++;
-            dfs(g, d, dp, p, node, v, depth + 1);
-        }
-    }
-    
-    int lca(int[] d, int[][] dp, int m, int x, int y)    {
-        if (d[x] > d[y])    {
-            int t = x;
-            x = y;
-            y = t;
+    int lca(int[][] dp, int[] depth, int a, int b)   {
+        if (depth[a] > depth[b])    {
+            int t = a;
+            a = b;
+            b = t;
         }
         
-        int gap = d[y] - d[x];
+        int depthGap = depth[b] - depth[a];
         
-        for (int i = 0; i < m; i++)
-            if ((gap & (1 << i)) != 0)
-                y = dp[y][i];
-        
-        if (x == y)
-            return x;
-        
-        for (int i = m - 1; i >= 0; i--)    {
-            if (dp[x][i] != dp[y][i])   {
-                x = dp[x][i];
-                y = dp[y][i];
+        for (int i = 0; i < 32; i++)    {
+            if ((depthGap & (1 << i)) != 0) {
+                b = dp[b][i];
             }
         }
         
+        if (a == b)
+            return a;
         
-        return dp[x][0];
+        for (int i = 31; i >= 0; i--)    {
+            if (dp[a][i] == dp[b][i])
+                continue;
+            a = dp[a][i];
+            b = dp[b][i];
+        }
+        
+        
+        return dp[a][0];
+    }
+    
+    void dfs(Map<Integer, List<int[]>> graph, int[][] prefix, int[] parent, int[] depth, int p, int u, int d)    {
+        depth[u] = d;
+        parent[u] = p;
+        if (!graph.containsKey(u))
+            return;
+        for (int[] next : graph.get(u))    {
+            int v = next[0];
+            int w = next[1];
+            if (v == p)
+                continue;
+            prefix[v] = prefix[u].clone();
+            prefix[v][w]++;
+            dfs(graph, prefix, parent, depth, u, v, d + 1);
+        }
     }
 }
