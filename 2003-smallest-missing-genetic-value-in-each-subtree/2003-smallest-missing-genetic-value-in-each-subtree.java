f65c274df95324bab31dcb04e91d03695642feea
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-09-25 20:43:12 -0700

    Time: 223 ms (16.67%), Space: 99.3 MB (45.83%) - LeetHub

--- 2003-smallest-missing-genetic-value-in-each-subtree/2003-smallest-missing-genetic-value-in-each-subtree.java
+++ 2003-smallest-missing-genetic-value-in-each-subtree/2003-smallest-missing-genetic-value-in-each-subtree.java
@@ -1,64 +1,72 @@
 class Solution {
-    Map<Integer, Set<Integer>> children;
+    Map<Integer, List<Integer>> tree;
     int[] ans;
-    Map<Integer, Set<Integer>> setMap;
+    Set<Integer> left;
+    int p = 2;
+    boolean[] has;
     int[] nums;
-    Map<Integer, Integer> missMap;
+    Set<Integer> seen;
     public int[] smallestMissingValueSubtree(int[] parents, int[] nums) {
+        tree = new HashMap<>();
+        left = new HashSet<>();
+        has = new boolean[200000];
+        seen = new HashSet<>();
         this.nums = nums;
-        int n = parents.length;
-        setMap = new HashMap<>();
-        children = new HashMap<>();
+        int startNode = -1;
+        int n = nums.length;
         ans = new int[n];
-        missMap = new HashMap<>();
-        Arrays.fill(ans, 1);
+        for (int i = 0; i < n; i++)
+            if (nums[i] == 1)
+                startNode = i;
+        
+        for (int node = startNode; node != -1; node = parents[node])
+            left.add(node);
+        
         for (int i = 0; i < n; i++) {
-            if (!children.containsKey(parents[i]))   {
-                children.put(parents[i], new HashSet<>());
-            }
-            children.get(parents[i]).add(i);
+            if (!tree.containsKey(parents[i]))
+                tree.put(parents[i], new LinkedList<>());
+            tree.get(parents[i]).add(i);
         }
-        dfs(0);
+        
+        markAsOne(0);
+
+        for (int node = startNode; node != -1; node = parents[node])   {
+            if (tree.containsKey(node)) {
+                for (int child : tree.get(node))    {
+                    gather(child);
+                }
+            }
+            has[nums[node]] = true;
+            while (has[p])
+                p++;
+            ans[node] = p;
+        }
         
         return ans;
     }
     
-    
-    private void dfs(int node)  {
-        if  (!children.containsKey(node))   {
-            Set<Integer> set = new HashSet<>();
-            set.add(nums[node]);
-            setMap.put(node, set);
-            missMap.put(node, nums[node] == 1 ? 2 : 1);
-            ans[node] = missMap.get(node);
+    void markAsOne(int node)    {
+        if (!left.contains(node))
+            ans[node] = 1;
+        if (!tree.containsKey(node))
             return;
-        }
-        
-        for (int child : children.get(node))  {
-            dfs(child);
-        }
-        int maxSetChildren = -1;
-        int maxStart = 0;
-        for (int child : children.get(node))  {
-            if (maxSetChildren == -1 || setMap.get(maxSetChildren).size() < setMap.get(child).size())    {
-                maxSetChildren = child;
+        for (int child : tree.get(node))
+            markAsOne(child);
+    }
+    
+    
+    void gather(int node)   {
+        if (seen.contains(node))
+            return;
+        seen.add(node);
+        if (tree.containsKey(node)) {
+            for (int child : tree.get(node))    {
+                gather(child);
             }
-            maxStart = Math.max(maxStart, missMap.get(child));
         }
         
-        setMap.put(node, setMap.get(maxSetChildren));
-        Set<Integer> thisSet = setMap.get(node);
-        
-        for (int child : children.get(node))  {
-            if (child == maxSetChildren)   {
-                continue;
-            }
-            thisSet.addAll(setMap.get(child));
-        }
-        thisSet.add(nums[node]);
-        while (thisSet.contains(maxStart))
-            maxStart++;
-        missMap.put(node, maxStart);
-        ans[node] = maxStart;
+        has[nums[node]] = true;
+        while(has[p])
+            p++;
     }
 }
