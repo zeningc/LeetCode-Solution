dec2393ada45a21bdb079a300bbf811e2dc3f4a2
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-10-01 14:04:49 -0700

    Time: 14 ms (73.34%), Space: 45.9 MB (13.04%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -2,25 +2,28 @@ class Solution {
     public int maximalRectangle(char[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int[][] presum = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1')
-                    presum[i][j] = i > 0 ? presum[i - 1][j] + 1 : 1;
-            }
-        }
+        int[] h = new int[n];
         int ans = 0;
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                h[j] = matrix[i][j] == '0' ? 0 : h[j] + 1;
+            }
+            
             Deque<Integer> stack = new LinkedList<>();
-            for (int j = 0; j <= n; j++) {
-                while (!stack.isEmpty() && (j == n || presum[i][j] <= presum[i][stack.peek()])) {
+            for (int j = 0; j <= n; j++)
+            {
+                while (!stack.isEmpty() && (j == n || h[stack.peek()] > h[j]))
+                {
                     int pop = stack.pop();
-                    int prev = stack.isEmpty() ? -1 : stack.peek();
-                    ans = Math.max(ans, presum[i][pop] * (j - prev - 1));
+                    int left = stack.isEmpty() ? -1 : stack.peek();
+                    ans = Math.max(ans, h[pop] * (j - left - 1));
                 }
                 stack.push(j);
             }
         }
+        
         
         return ans;
     }
