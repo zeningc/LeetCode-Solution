88345d7f43b4bce80e1305dfef7945d234bcc75c
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-09-30 19:15:29 -0700

    Time: 12 ms (40.20%), Space: 43.8 MB (66.45%) - LeetHub

--- 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.java
+++ 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.java
@@ -2,33 +2,25 @@ class Solution {
     public int numSubmat(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
-        int[][] dp = new int[m][n];
+        int[] h = new int[n];
+        
         int ans = 0;
-        
-        for (int i = 0; i < n; i++)
-            if (mat[m - 1][i] == 1){
-                dp[m - 1][i] = 1;
-                ans++;
-            }
-        
-        for (int i = m - 2; i >= 0; i--)    {
-            for (int j = n - 1; j >= 0; j--)    {
-                if (mat[i][j] == 0)
-                    continue;
-                dp[i][j] = 1 + dp[i + 1][j];
-                ans += dp[i][j];
-            }
-        }
-        
-        for (int i = m - 1; i >= 0; i--)    {
-            for (int j = n - 1; j >= 0; j--)    {
-                int min = dp[i][j];
-                for (int k = j + 1; k < n; k++) {
-                    if (mat[i][k] == 0)
-                        break;
-                    min = Math.min(dp[i][k], min);
-                    ans += min;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++)
+                h[j] = mat[i][j] == 1 ? h[j] + 1 : 0;
+            
+            Deque<Integer> stack = new LinkedList<>();
+            int cnt = 0;
+            for (int j = 0; j < n; j++)    {
+                while (!stack.isEmpty() && h[stack.peek()] > h[j])  {
+                    int pop = stack.pop();
+                    int left = stack.isEmpty() ? -1 : stack.peek();
+                    cnt -= (pop - left) * (h[pop] - h[j]);
                 }
+                
+                stack.push(j);
+                cnt += h[j];
+                ans += cnt;
             }
         }
         
