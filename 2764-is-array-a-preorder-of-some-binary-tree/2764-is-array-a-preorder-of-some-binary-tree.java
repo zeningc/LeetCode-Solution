cb922cbb1c712cb19331c645ebfc0f3a459c701a
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-10-02 10:45:29 -0700

    Time: 12 ms (55.00%), Space: 86 MB (73.75%) - LeetHub

--- 2764-is-array-a-preorder-of-some-binary-tree/2764-is-array-a-preorder-of-some-binary-tree.java
+++ 2764-is-array-a-preorder-of-some-binary-tree/2764-is-array-a-preorder-of-some-binary-tree.java
@@ -1,16 +1,11 @@
 class Solution {
     public boolean isPreorder(List<List<Integer>> nodes) {
-        Map<Integer, Integer> childCnt = new HashMap<>();
         int pre = -1;
         Deque<Integer> stack = new LinkedList<>();
         for (List<Integer> node : nodes)
         {
             int cur = node.get(0);
             int p = node.get(1);
-            // if (childCnt.getOrDefault(p, 0) >= 2)
-            // {
-            //     return false;
-            // }
             
             while (!stack.isEmpty() && stack.peek() != p)
             {
