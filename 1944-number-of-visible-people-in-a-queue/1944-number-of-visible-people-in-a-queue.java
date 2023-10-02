ea3273cfb4eb976e27ceb47c5e1cef71d5c96b04
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-10-02 15:20:11 -0700

    Time: 21 ms (94.90%), Space: 58.8 MB (42.79%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
@@ -3,21 +3,20 @@ class Solution {
         int n = heights.length;
         int[] ans = new int[n];
         Deque<Integer> stack = new LinkedList<>();
-        for (int i = n - 1; i >= 0; i--)    {
+        for (int i = n - 1; i >= 0; i--)
+        {
             int cnt = 0;
-            while (!stack.isEmpty() && heights[i] >= heights[stack.peek()]) {
+            while (!stack.isEmpty() && heights[stack.peek()] < heights[i])
+            {
+                cnt++;
                 stack.pop();
-                cnt++;
             }
-            if (!stack.isEmpty())
-                cnt++;
-            ans[i] = cnt;
+            
+            
+            ans[i] = cnt + (stack.isEmpty() ? 0 : 1);
             stack.push(i);
         }
+        
         return ans;
     }
 }
-
-
-
-    
