6f60668f941640602d355993d29e964396ac3731
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-10-02 14:43:32 -0700

    Time: 21 ms (24.55%), Space: 43.7 MB (59.55%) - LeetHub

--- 2646-minimize-the-total-price-of-the-trips/2646-minimize-the-total-price-of-the-trips.java
+++ 2646-minimize-the-total-price-of-the-trips/2646-minimize-the-total-price-of-the-trips.java
@@ -1,63 +1,77 @@
 class Solution {
+    Map<Integer, List<Integer>> graph;
+    Map<Integer, Integer> freq;
+    int[] price;
     public int minimumTotalPrice(int n, int[][] edges, int[] price, int[][] trips) {
-        List<List<Integer>> tree = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            tree.add(new ArrayList<>());            
+        graph = new HashMap<>();
+        freq = new HashMap<>();
+        this.price = price;
+        for (int[] edge : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+            graph.computeIfAbsent(u, x -> new LinkedList<>()).add(v);
+            graph.computeIfAbsent(v, x -> new LinkedList<>()).add(u);
         }
-        for (int[] edge : edges) {
-            tree.get(edge[0]).add(edge[1]);
-            tree.get(edge[1]).add(edge[0]);
+
+        for (int[] trip : trips)
+        {
+            calcPath(n, trip[0], trip[1]);
         }
-        Map<Integer, Integer> counts = new HashMap<>();
-        for (int[] trip : trips) {
-            bfs(tree, trip[0], trip[1], counts);
-        }
-        int[] currPrice = new int[n];
-        for (int i = 0; i < n; i++) {
-            currPrice[i] = counts.getOrDefault(i, 0) * price[i];
-        }
-        int[] result = helper(tree, 0, -1, currPrice);
-        return Math.min(result[0], result[1]);
+        
+        int[] ret = dfs(0, -1);
+        Map<Integer, Integer> f = freq;
+        return Math.min(ret[0], ret[1]);
     }
-    private int[] helper(List<List<Integer>> tree, int curr, int parent, int[] currPrice) {
-        List<Integer> neighbors = tree.get(curr);
-        int whole = 0;
-        int halved = 0;
-        for (int nei : neighbors) {
-            if (nei == parent) {
+    
+    int[] dfs(int u, int p)
+    {
+        int[] cur = new int[2];
+        cur[0] = price[u] * freq.getOrDefault(u, 0);
+        cur[1] = price[u] * freq.getOrDefault(u, 0) / 2;
+        if (!graph.containsKey(u))
+            return cur;
+        for (int v : graph.get(u))
+        {
+            if (p == v)
                 continue;
-            }
-            int[] neiResult = helper(tree, nei, curr, currPrice);
-            whole += neiResult[0];
-            halved += Math.min(neiResult[0], neiResult[1]);
+            int[] nxt = dfs(v, u);
+            cur[0] += Math.min(nxt[0], nxt[1]);
+            cur[1] += nxt[0];
         }
-        return new int[] {currPrice[curr] + halved, currPrice[curr] / 2 + whole};
+        
+        return cur;
     }
-    private void bfs(List<List<Integer>> tree, int src, int dst, Map<Integer, Integer> counts) {
-        int n = tree.size();
-        Queue<Integer> queue = new ArrayDeque<>();
-        boolean[] visited = new boolean[n];
-        int[] parent = new int[n];
-        Arrays.fill(parent, -1);
-        queue.offer(src);
-        visited[src] = true;
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-            if (curr == dst) {
+    
+    int calcPath(int n, int start, int target)
+    {
+        Deque<int[]> q = new LinkedList<>();
+        q.offer(new int[] {start, -1});
+        int[] prev = new int[n];
+        while (!q.isEmpty())
+        {
+            int[] cur = q.poll();
+            int u = cur[0];
+            int p = cur[1];
+            prev[u] = p;
+            if (u == target)
                 break;
-            }
-            for (int nei : tree.get(curr)) {
-                if (!visited[nei]) {
-                    visited[nei] = true;
-                    parent[nei] = curr;
-                    queue.offer(nei);
-                }
+            for (int v : graph.get(u))
+            {
+                if (v == p)
+                    continue;
+                q.offer(new int[] {v, u});
             }
         }
-        int curr = dst;
-        while (curr != -1) {
-            counts.put(curr, counts.getOrDefault(curr, 0) + 1);
-            curr  = parent[curr];
+        
+        int t = target;
+        int ans = 0;
+        while (t != -1)
+        {
+            freq.put(t, freq.getOrDefault(t, 0) + 1);
+            t = prev[t];
         }
+        
+        return ans;
     }
 }
