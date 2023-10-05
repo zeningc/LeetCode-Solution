51fa34ebb8ea020c02b0f6634ed7d19ae7fb9370
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-10-05 16:07:02 -0500

    Time: 8 ms (89.19%), Space: 54.4 MB (5.41%) - LeetHub

---
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        Deque<Integer> stack = new LinkedList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++)
+        {
+            if (stack.isEmpty() || nums[stack.peek()] >= nums[i])
+            {
+                stack.push(i);
+            }
+        }
+        
+        int ans = 0;
+        
+        for (int i = n - 1; i >= 0; i--)
+        {
+            while (!stack.isEmpty() && nums[stack.peek()] <= nums[i])
+            {
+                ans = Math.max(ans, i - stack.peek());
+                stack.pop();
+            }
+        }
+        
+        return ans;
+    }
+}
