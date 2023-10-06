7a9240f36950fb34837bf3c8df1885a5e9fd9f60
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-10-06 13:50:42 -0500

    Time: 3 ms (87.76%), Space: 55.9 MB (46.94%) - LeetHub

--- 2863-maximum-length-of-semi-decreasing-subarrays/2863-maximum-length-of-semi-decreasing-subarrays.java
+++ 2863-maximum-length-of-semi-decreasing-subarrays/2863-maximum-length-of-semi-decreasing-subarrays.java
@@ -1,32 +1,20 @@
-public class Solution {
+class Solution {
     public int maxSubarrayLength(int[] nums) {
-        Stack<Integer> stack = new Stack<>();
         int n = nums.length;
-
-        for (int i = n - 1; i >= 0; i--) {
-            if (stack.isEmpty() || nums[i] < nums[stack.peek()]) {
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < n; i++)
+            if (stack.isEmpty() || nums[stack.peek()] < nums[i])
                 stack.push(i);
-            }
-        }
-
-        int r = 0;
-        int m = Integer.MIN_VALUE;
-
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && stack.peek() <= i) {
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            while (!stack.isEmpty() && nums[stack.peek()] > nums[i])
+            {
+                ans = Math.max(ans, i - stack.peek() + 1);
                 stack.pop();
             }
-
-            if (nums[i] > m) {
-                m = nums[i];
-
-                while (!stack.isEmpty() && nums[stack.peek()] < m) {
-                    r = Math.max(r, stack.peek() - i + 1);
-                    stack.pop();
-                }
-            }
         }
-
-        return r;
+        
+        return ans;
     }
 }
