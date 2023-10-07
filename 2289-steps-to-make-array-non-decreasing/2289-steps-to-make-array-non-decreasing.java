7c6a66657848953118b91fbfc099a46082a5db5a
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-10-07 13:27:03 -0500

    Time: 21 ms (74.82%), Space: 56.7 MB (39.36%) - LeetHub

---
+++ 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int totalSteps(int[] nums) {
+        int n = nums.length;
+        Deque<Integer> stack = new LinkedList<>();
+        int[] t = new int[n];
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            int cnt = 0;
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i])
+            {
+                int pop = stack.pop();
+                cnt = Math.max(cnt + 1, t[pop]);
+                
+            }
+            t[i] = cnt;
+            ans = Math.max(ans, t[i]);
+            stack.push(i);
+        }
+        
+        return ans;
+    }
+}
