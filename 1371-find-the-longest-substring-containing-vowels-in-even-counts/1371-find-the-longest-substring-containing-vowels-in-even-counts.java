5d62ea737f417eee71742f1fd4d56bf814d10748
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-10-08 13:23:04 -0500

    Time: 29 ms (74.38%), Space: 47.7 MB (46.28%) - LeetHub

---
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findTheLongestSubstring(String s) {
+        int n = s.length();
+        Map<Character, Integer> vowelToIndex = Map.of('a', 0, 'e', 1, 'i', 2, 'o', 3, 'u', 4);
+        int[] firstSeen = new int[32];
+        Arrays.fill(firstSeen, -1);
+        int state = 0, maxLen = 0;
+        firstSeen[0] = -1; // important initialization
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (vowelToIndex.containsKey(c)) {
+                state ^= (1 << vowelToIndex.get(c)); // flip the bit corresponding to the vowel
+            }
+            if (firstSeen[state] == -1 && state != 0) {
+                firstSeen[state] = i;
+            } else {
+                maxLen = Math.max(maxLen, i - firstSeen[state]);
+            }
+        }
+        return maxLen;
+    }
+}
