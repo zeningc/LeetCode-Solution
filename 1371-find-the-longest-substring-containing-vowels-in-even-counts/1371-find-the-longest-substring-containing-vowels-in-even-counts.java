a90c04dca2e19951db9726f6cba18117e04c4965
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-10-08 20:53:02 -0500

    Time: 28 ms (74.38%), Space: 47.9 MB (37.19%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.java
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -5,11 +5,11 @@ class Solution {
         int[] firstSeen = new int[32];
         Arrays.fill(firstSeen, -1);
         int state = 0, maxLen = 0;
-        firstSeen[0] = -1; // important initialization
+        firstSeen[0] = -1;
         for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
             if (vowelToIndex.containsKey(c)) {
-                state ^= (1 << vowelToIndex.get(c)); // flip the bit corresponding to the vowel
+                state ^= (1 << vowelToIndex.get(c));
             }
             if (firstSeen[state] == -1 && state != 0) {
                 firstSeen[state] = i;
