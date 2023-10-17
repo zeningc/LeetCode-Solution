9f0c2694789441890f40d79e9e92922f1f845399
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-10-16 22:52:37 -0500

    Time: 110 ms (6.52%), Space: 60 MB (13.04%) - LeetHub

--- 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.java
+++ 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.java
@@ -1,15 +1,23 @@
 class Solution {
-    public long beautifulSubarrays(int[] A) {
-        HashMap<Integer, Integer> dp = new HashMap<>();
-        dp.put(0, 1);
-        int pre = 0;
-        long res = 0;
-        for (int a : A) {
-            pre ^= a;
-            int v = dp.getOrDefault(pre, 0);
-            res += v;
-            dp.put(pre, v + 1);
+    public long beautifulSubarrays(int[] nums) {
+        long ans = 0;
+        Map<Integer, Long> m = new HashMap<>();
+        int state = 0;
+        m.put(state, 1L);
+        for (int num : nums)
+        {
+            for (int i = 0; i < 32; i++)
+            {
+                if ((num & (1 << i)) != 0)
+                {
+                    state ^= (1 << i);
+                }
+            }
+            ans += m.getOrDefault(state, 0L);
+            m.put(state, m.getOrDefault(state, 0L) + 1);
         }
-        return res;
+        
+        return ans;
     }
 }
+
