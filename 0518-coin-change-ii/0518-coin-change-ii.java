4cae4a2a3e0f33143e0f01242c7132a08bc974b2
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-10-19 13:28:01 -0500

    Time: 3 ms (80.60%), Space: 39.9 MB (77.07%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, 0);
+        dp[0] = 1;
+        for (int coin : coins)
+        {
+            for (int j = coin; j <= amount; j++)
+            {
+                dp[j] += dp[j - coin];
+            }
+        }
+        
+        return dp[amount];
+    }
+}
