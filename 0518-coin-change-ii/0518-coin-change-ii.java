846f51ad1bcb24b380ebc30683927b13436a5246
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-10-19 13:32:15 -0500

    Time: 3 ms (80.60%), Space: 40.7 MB (61.45%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -4,12 +4,8 @@ class Solution {
         Arrays.fill(dp, 0);
         dp[0] = 1;
         for (int coin : coins)
-        {
             for (int j = coin; j <= amount; j++)
-            {
                 dp[j] += dp[j - coin];
-            }
-        }
         
         return dp[amount];
     }
