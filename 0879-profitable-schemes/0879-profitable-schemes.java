ee656dfc407d098bc340759c35bff3b4005b9c76
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-10-23 20:07:06 -0500

    Time: 27 ms (82.69%), Space: 41.3 MB (86.06%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.java
+++ 0879-profitable-schemes/0879-profitable-schemes.java
@@ -1,13 +1,6 @@
 class Solution {
     public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
         int m = group.length;
-        int profitSum = 0;
-        int groupSum = 0;
-        for (int i = 0; i < m; i++)
-        {
-            profitSum += profit[i];
-            groupSum += group[i];
-        }
         long[][] dp = new long[n + 1][minProfit + 1];
         dp[0][0] = 1L;
         int mod = (int)1e9 + 7;
@@ -17,7 +10,6 @@ class Solution {
             {
                 for (int k = minProfit; k >= 0; k--)
                 {
-                    dp[j][k] = dp[j][k];
                     if (j - group[i - 1] >= 0)
                         dp[j][k] = (dp[j][k] + dp[j - group[i - 1]][Math.max(k - profit[i - 1], 0)]) % mod;
                 }
