c1e51a01439aa542416594158fa8c588d9db3b3f
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-10-27 19:48:39 -0500

    Time: 48 ms (67.05%), Space: 40.7 MB (79.55%) - LeetHub

--- 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.java
+++ 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.java
@@ -2,22 +2,22 @@ class Solution {
     public int waysToReachTarget(int target, int[][] types) {
         int mod = (int)1e9 + 7;
         int n = types.length;
-        long[][] dp = new long[n + 1][target + 1];
-        dp[0][0] = 1L;
+        long[] dp = new long[target + 1];
+        dp[0] = 1L;
         
         for (int i = 1; i <= n; i++)
         {
             int cnt = types[i - 1][0];
             int point = types[i - 1][1];
-            for (int j = 0; j <= target; j++)
+            for (int j = target; j >= 0; j--)
             {
-                for (int k = 0; k <= cnt && j >= k * point; k++)
+                for (int k = 1; k <= cnt && j >= k * point; k++)
                 {
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j - k * point]) % mod;
+                    dp[j] = (dp[j] + dp[j - k * point]) % mod;
                 }
             }
         }
         
-        return (int)(dp[n][target] % mod);
+        return (int)(dp[target] % mod);
     }
 }
