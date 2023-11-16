ecf35d978d31d426d421569c725f452a4bb907e0
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-11-15 20:27:00 -0600

    Time: 118 ms (36.53%), Space: 123.5 MB (13.29%) - LeetHub

--- 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.java
+++ 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.java
@@ -1,56 +1,50 @@
 class Solution {
-    Map<Integer, List<Integer>> graph;
-    Map<Integer, Integer> bobVisited;
-    int[] parent;
     public int mostProfitablePath(int[][] edges, int bob, int[] amount) {
-        graph = new HashMap<>();
-        for (int[] edge : edges)
-        {
+        int n = amount.length;
+        Map<Integer, List<Integer>> edgeMap = new HashMap<>();
+        for (int[] edge : edges)    {
             int u = edge[0];
             int v = edge[1];
-            graph.computeIfAbsent(u, a -> new LinkedList<Integer>()).add(v);
-            graph.computeIfAbsent(v, a -> new LinkedList<Integer>()).add(u);
+            edgeMap.computeIfAbsent(u, x -> new LinkedList<>()).add(v);
+            edgeMap.computeIfAbsent(v, x -> new LinkedList<>()).add(u);
         }
-        parent = new int[amount.length];
-        bobVisited = new HashMap<>();
-        dfs(0, -1);
-        for (int i = 0; bob != -1; i++)
-        {
-            bobVisited.put(bob, i);
-            bob = parent[bob];
+        int[] parents = new int[n];
+        findParent(edgeMap, parents, 0, -1);
+        
+        int[] bobArriveTime = new int[n];
+        Arrays.fill(bobArriveTime, Integer.MAX_VALUE);
+        int p = bob;
+        int t = 0;
+        while (p != -1) {
+            bobArriveTime[p] = t++;
+            p = parents[p];
         }
         
+        int ans = Integer.MIN_VALUE;
         Deque<int[]> q = new LinkedList<>();
         q.offer(new int[] {0, 0});
-        int ans = Integer.MIN_VALUE;
-        int t = 0;
-        while (!q.isEmpty())
-        {
+        t = 0;
+        while (!q.isEmpty())    {
             int size = q.size();
-            while (size-- > 0)
-            {
+            while (size-- > 0)  {
                 int[] cur = q.poll();
                 int u = cur[0];
-                int amt = cur[1];
-                int bobVistedTime = bobVisited.getOrDefault(u, Integer.MAX_VALUE);
-                if (bobVistedTime == t)
-                    amt += amount[u] / 2;
-                else if (bobVistedTime > t)
-                    amt += amount[u];
-                if (!graph.containsKey(u))
+                int val = cur[1];
+                
+                if (bobArriveTime[u] == t)
+                    val += amount[u] / 2;
+                else if (bobArriveTime[u] > t)
+                    val += amount[u];
+                
+                if (edgeMap.get(u).size() == 1 && edgeMap.get(u).get(0) == parents[u]) {
+                    ans = Math.max(ans, val);
                     continue;
-                boolean hasChild = false;
-                for (int v : graph.get(u))
-                {
-                    if (v == parent[u])
-                        continue;
-                    hasChild = true;
-                    q.offer(new int[] {v, amt});
                 }
                 
-                if (!hasChild)
-                {
-                    ans = Math.max(ans, amt);
+                for (int v : edgeMap.get(u))    {
+                    if (v == parents[u])
+                        continue;
+                    q.offer(new int[] {v, val});
                 }
             }
             t++;
@@ -59,17 +53,15 @@ class Solution {
         return ans;
     }
     
-    
-    void dfs(int u, int p)
-    {
-        parent[u] = p;
-        if (!graph.containsKey(u))
-            return;
-        for (int v : graph.get(u))
-        {
+    void findParent(Map<Integer, List<Integer>> edgeMap, int[] parents, int u, int p) {
+        parents[u] = p;
+        
+        for (int v : edgeMap.get(u))    {
             if (v == p)
                 continue;
-            dfs(v, u);
+            findParent(edgeMap, parents, v, u);
         }
     }
+    
+    
 }
