f67f049f964a36a16f62dff247561995b3659d6d
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-11-22 20:27:37 -0600

    Time: 1002 ms (5.26%), Space: 172 MB (26.40%) - LeetHub

---
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int findMaximumXOR(int[] nums) {
+        TrieNode root = new TrieNode();
+        for (int num : nums)
+            root.insert(num);
+        int ans = 0;
+        for (int num : nums)    {
+            root.delete(num);
+            ans = Math.max(ans, root.findMaxXor(num) ^ num);
+        }
+        return ans;
+    }
+}
+
+class TrieNode  {
+    int n;
+    int cnt;
+    TrieNode[] children;
+    
+    public TrieNode()   {
+        n = -1;
+        cnt = 0;
+        children = new TrieNode[2];
+    }
+    
+    public void insert(int num)    {
+        TrieNode node = this;
+        for (int i = 31; i >= 0; i--)   {
+            node.cnt++;
+            int b = (num & (1 << i)) != 0 ? 1 : 0;
+            if (node.children[b] == null)
+                node.children[b] = new TrieNode();
+            node = node.children[b];
+        }
+        node.cnt++;
+        node.n = num;
+    }
+    
+    public int findMaxXor(int num)  {
+        TrieNode node = this;
+        for (int i = 31; i >= 0; i--)   {
+            int b = (num & (1 << i)) != 0 ? 1 : 0;
+            if (node.children[1 - b] != null && node.children[1 - b].cnt > 0)
+                node = node.children[1 - b];
+            else if (node.children[b] != null && node.children[b].cnt > 0)
+                node = node.children[b];
+            else
+                return -1;
+        }
+        
+        return node.n;
+    }
+    
+    public void delete(int num) {
+        TrieNode node = this;
+        for (int i = 31; i >= 0; i--)   {
+            node.cnt--;
+            int b = (num & (1 << i)) != 0 ? 1 : 0;
+            if (node.children[b] == null)
+                return;
+            node = node.children[b];
+        }
+        node.cnt--;
+    }
+}
