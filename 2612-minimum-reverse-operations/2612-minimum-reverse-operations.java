f15034190c9e81de5163f44c1160a41c0438f197
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-12-01 18:10:57 -0800

    Time: 170 ms (75.00%), Space: 58.1 MB (85.94%) - LeetHub

--- 2612-minimum-reverse-operations/2612-minimum-reverse-operations.java
+++ 2612-minimum-reverse-operations/2612-minimum-reverse-operations.java
@@ -1,89 +1,46 @@
+import java.util.NavigableSet;
+
 class Solution {
     public int[] minReverseOperations(int n, int p, int[] banned, int k) {
-        TreeSet<Integer> even = new TreeSet<>();
+        boolean[] skip = new boolean[n];
+        for (int ban : banned)
+            skip[ban] = true;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        
+        Deque<Integer> q = new LinkedList<>();
         TreeSet<Integer> odd = new TreeSet<>();
-        
-        TreeSet<Integer> set;
-        
-        boolean [] skip = new boolean [n];
-        
-        for (int num : banned) {
-            skip[num] = true;
-        }
-        
-        int start = p;
-        
-        // Add values to the set that are not banned
-        for (int i = 0; i < n; ++i) {
-            if (skip[i] || i == p)
-                continue;
-            
-            if (i % 2 == 1)
-                odd.add(i);
-            else
+        TreeSet<Integer> even = new TreeSet<>();
+        for (int i = 0; i < n; i++)
+            if (i % 2 == 0 && !skip[i] && i != p)
                 even.add(i);
-        }
+            else if (i % 2 == 1 && !skip[i] && i != p)
+                odd.add(i);
         
-        int [] result = new int [n];
-        Arrays.fill(result, -1);
-        
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(p);
-        
-        int size;
-        int current;
-        int moves = 0;
-        
-        int min, max;
-        
-        Integer key;
-        
-        int mCurrent;
-        
-        while (!queue.isEmpty()) {
-            size = queue.size();
-            
-            
-            while (size-- > 0) {
-                current = queue.remove();
-                
-                result[current] = moves;
-                
-                
-                // calculate min index
-                if (current < k - 1) {
-                    min = (k - 1) - current;
-                }else {
-                    min = current - k + 1;
-                }
-                
-                // calculate max index
-                mCurrent = (n - 1) - current;
-                if (mCurrent < k - 1) {
-                    max = (k - 1) - mCurrent;
-                }else {
-                    max = mCurrent - k + 1;
-                }
-                max = (n - 1) - max;
-                
-                
-                // chose the correct parity set
-                set = min % 2 == 0 ? even : odd;
-                
-                key = set.ceiling(min);
-                
-                // add all values in range to the queue and remove from set
-                while (key != null && key <= max) {
-                    queue.add(key);
-                    set.remove(key);
-                    key = set.ceiling(min);
+        q.offer(p);
+        int cnt = 0;
+        while (!q.isEmpty())    {
+            int size = q.size();
+            while (size-- > 0)  {
+                int x = q.poll();
+                ans[x] = cnt;
+                int start = Math.max(0, x - k + 1);
+                int end = Math.min(n - 1, x + k - 1);
+                int lo = 2 * start + k - x - 1;
+                int hi = 2 * end - x - k + 1;
+                TreeSet<Integer> set;
+                set = lo % 2 == 0 ? even : odd;
+                NavigableSet<Integer> subSet = set.subSet(lo, true, hi, true); 
+                Iterator<Integer> iterator = subSet.iterator();
+                while (iterator.hasNext()) {
+                    Integer nxt = iterator.next();
+                    q.offer(nxt);
+                    iterator.remove();
                 }
             }
-            
-            ++moves;
+            cnt++;
         }
         
-        
-        return result;
+        return ans;
     }
 }
