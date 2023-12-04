c62e5a1020b9e713dc62a7134d5bb349f8855e19
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-12-03 16:32:32 -0800

    Time: 53 ms (52.46%), Space: 68.8 MB (30.00%) - LeetHub

--- 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.java
+++ 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.java
@@ -1,50 +1,45 @@
-import java.util.NavigableSet;
-import java.util.NavigableMap;
 class Solution {
     public int[] leftmostBuildingQueries(int[] heights, int[][] queries) {
-        TreeMap<Integer, TreeSet<int[]>> qs = new TreeMap<>();
-        int[][] qss = new int[queries.length][3];
+        int[][] qs = new int[queries.length][3];
         int[] ans = new int[queries.length];
-        Arrays.fill(ans, -1);
-        for (int i = 0; i < queries.length; i++)    {
-            qss[i] = new int[] {queries[i][0], queries[i][1], i};
-        }
-            
-        Arrays.sort(qss, (a, b) -> Math.max(a[0], a[1]) - Math.max(b[0], b[1]));
-        int p = 0;
+        for (int i = 0; i < queries.length; i++)
+            qs[i] = queries[i][0] > queries[i][1] ? new int[] {queries[i][1], queries[i][0], i} : new int[] {queries[i][0], queries[i][1], i};
         
-        for (int i = 0; i < heights.length; i++)    {
-            while (p < qss.length && qss[p][0] <= i && qss[p][1] <= i)    {
-                qs.computeIfAbsent(heights[qss[p][0]], x -> new TreeSet<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0])).add(new int[] {heights[qss[p][1]], qss[p][2]});
-                p++;
-            }
-            if (qs.lowerKey(heights[i]) != null)    {
-                NavigableMap<Integer, TreeSet<int[]>> subMap = qs.subMap(qs.firstKey(), true, qs.lowerKey(heights[i]), true);
-                Iterator<Map.Entry<Integer, TreeSet<int[]>>> iterator = subMap.entrySet().iterator();
-                while (iterator.hasNext())  {
-                    TreeSet<int[]> querySet = iterator.next().getValue();
-                    if (querySet.lower(new int[] {heights[i], -1}) != null) {
-                        NavigableSet<int[]> subSet = querySet.subSet(querySet.first(), true, querySet.lower(new int[] {heights[i], -1}), true);
-                        Iterator<int[]> setIterator = subSet.iterator();
-                        while (setIterator.hasNext())   {
-                            int[] cur = setIterator.next();
-                            ans[cur[1]] = i;
-                            setIterator.remove();
-                        }
-                    }
-                    if (querySet.isEmpty())
-                        iterator.remove();
+        Arrays.sort(qs, (a, b) -> b[1] - a[1]);
+        List<int[]> stack = new ArrayList<>();
+        int p = heights.length - 1;
+        for (int i = 0; i < qs.length; i++) {
+            int a = qs[i][0];
+            int b = qs[i][1];
+            int idx = qs[i][2];
+            while (p >= b)    {
+                while (!stack.isEmpty() && stack.get(stack.size() - 1)[0] <= heights[p])    {
+                    stack.remove(stack.size() - 1);
                 }
+                stack.add(new int[] {heights[p], p});
+                p--;
             }
-        }
-        
-        for (int i = 0; i < qss.length; i++)    {
-            if (qss[i][0] == qss[i][1])
-                ans[qss[i][2]] = qss[i][0];
-            else if (qss[i][0] < qss[i][1] && heights[qss[i][0]] < heights[qss[i][1]])
-                ans[qss[i][2]] = qss[i][1];
-            else if (qss[i][1] < qss[i][0] && heights[qss[i][1]] < heights[qss[i][0]])
-                ans[qss[i][2]] = qss[i][0];
+            
+            if (a == b || heights[b] > heights[a]) {
+                ans[idx] = b;
+                continue;
+            }
+            
+            int lo = 0;
+            int hi = stack.size() - 1;
+            while (lo <= hi)    {
+                int mid = lo + (hi - lo) / 2;
+                if (stack.get(mid)[0] > Math.max(heights[a], heights[b]))
+                    lo = mid + 1;
+                else
+                    hi = mid - 1;
+            }
+            
+            if (hi == -1)
+                ans[idx] = -1;
+            else
+                ans[idx] = stack.get(hi)[1];
+            
         }
         
         return ans;
