01aed38a7a8f88351f01050ab385578cd80ca885
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-12-06 21:46:22 -0800

    Time: 37 ms (11.65%), Space: 55.1 MB (87.86%) - LeetHub

--- 2860-happy-students/2860-happy-students.java
+++ 2860-happy-students/2860-happy-students.java
@@ -1,27 +1,20 @@
 class Solution {
     public int countWays(List<Integer> nums) {
         Collections.sort(nums);
+        int i = 0;
         int n = nums.size();
-        Set<Integer> set = new HashSet<>(nums);
         int ans = 0;
-        for (int selected = 0; selected <= n; selected++)   {
-            if (set.contains(selected))
-                continue;
-            int lo = 0;
-            int hi = n - 1;
-            while (lo <= hi)    {
-                int mid = lo + (hi - lo) / 2;
-                if (nums.get(mid) >= selected)
-                    hi = mid - 1;
-                else
-                    lo = mid + 1;
-            }
+        while (i < n)   {
+            int j = i;
+            while (j < n && nums.get(i) == nums.get(j))
+                j++;
             
-            if (selected != hi + 1)
-                continue;
-            ans++;
+            if (j < n && j > nums.get(i) && j < nums.get(j))
+                ans++;
+            
+            i = j;
         }
         
-        return ans;
+        return ans + (nums.get(0) > 0 ? 1 : 0) + (nums.get(n - 1) < n ? 1 : 0);
     }
 }
