b5891d2323c7ff78c75fb95b44e1784bb0d6bfaf
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-12-08 13:00:03 -0800

    Time: 39 ms (53.70%), Space: 60.4 MB (37.57%) - LeetHub

---
+++ 2910-minimum-number-of-groups-to-create-a-valid-assignment/2910-minimum-number-of-groups-to-create-a-valid-assignment.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minGroupsForValidAssignment(int[] nums) {
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int num : nums)
+            m.put(num, m.getOrDefault(num, 0) + 1);
+        int hi = m.values().stream().max(Integer::compareTo).orElse(0);
+        int ans = nums.length;
+        for (int i = hi; i >= 1; i--)  {
+            int total = 0;
+            boolean valid = true;
+            for (int freq : m.values())  {
+                int groupCnt = freq / i;
+                int r = freq % i;
+                if (r == 0) {
+                    total += groupCnt;
+                    continue;
+                }
+                
+                if (groupCnt >= i - 1 - r)  {
+                    total += groupCnt + 1;
+                    continue;
+                }
+                
+                valid = false;
+                break;
+            }
+            
+            if (valid)  {
+                ans = total;
+                break;
+            }
+            
+        }
+        
+        return ans;
+    }
+}
+
+
+
