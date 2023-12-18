fc72bdfb3ef8bf4ccf5d8c456fc3a91eef5249ab
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-12-18 16:24:36 -0600

    Time: 6 ms (98.85%), Space: 41.5 MB (98.85%) - LeetHub

--- 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.java
+++ 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.java
@@ -1,24 +1,46 @@
 class Solution {
     public String findLexSmallestString(String s, int a, int b) {
+        String ans = s;
         int n = s.length();
-        String smallest = s;
-        Queue<String> q = new LinkedList<>();
-        q.offer(s);
-        Set<String> seen = new HashSet<>(q);
-        while (!q.isEmpty()) {
-            String cur = q.poll();
-            if (smallest.compareTo(cur) > 0)
-                smallest = cur;
-            char[] ca = cur.toCharArray();
-            for (int i = 1; i < ca.length; i += 2) // add operation.
-                ca[i] = (char)((ca[i] - '0' + a) % 10 + '0');
-            String addA = String.valueOf(ca);
-            if (seen.add(addA))
-                q.offer(addA);
-            String rotate = cur.substring(n - b) + cur.substring(0, n - b); // rotation.
-            if (seen.add(rotate))
-                q.offer(rotate);
+        int[] minDigit = new int[10];
+        for (int i = 0; i < 10; i++)    {
+            int r = (i + a) % 10;
+            int min = Math.min(i, r);
+            while (r != i)  {
+                r = (r + a) % 10;
+                min = Math.min(min, r);
+            }
+            minDigit[i] = min;
         }
-        return smallest;
+        boolean first = true;
+        for (int i = n; i != 0 || first; i = (i + n - b) % n)    {
+            first = false;
+            String cur = s.substring(i) + s.substring(0, i);
+            char[] ch = cur.toCharArray();
+            while (minDigit[cur.charAt(1) - '0'] != ch[1] - '0')  {
+                for (int j = 1; j < n; j += 2) {
+                    ch[j] = (char)('0' + (ch[j] - '0' + a) % 10);
+                }
+            }
+            
+            if (b % 2 != 0) {
+                while (minDigit[cur.charAt(0) - '0'] != ch[0] - '0')  {
+                    for (int j = 0; j < n; j += 2) {
+                        ch[j] = (char)('0' + (ch[j] - '0' + a) % 10);
+                    }
+                }
+            }
+            
+            for (int j = 0; j < n; j++) {
+                if (ans.charAt(j) == ch[j])
+                    continue;
+                if (ch[j] < ans.charAt(j))  {
+                    ans = new String(ch);
+                }
+                break;
+            }
+        }
+        
+        return ans;
     }
 }
