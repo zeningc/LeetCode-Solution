4581a3e3c238ef52bc3436e8aaffb4d8ee40b122
Zening Chen <59945561+zeningc@users.noreply.github.com>
2023-12-18 20:53:21 -0600

    Time: 124 ms (72.55%), Space: 55.3 MB (52.94%) - LeetHub

--- 2768-number-of-black-blocks/2768-number-of-black-blocks.java
+++ 2768-number-of-black-blocks/2768-number-of-black-blocks.java
@@ -1,49 +1,25 @@
-import java.util.*;
-
 class Solution {
     public long[] countBlackBlocks(int m, int n, int[][] coordinates) {
-        Map<Long, Integer> countMap = new HashMap<>(); // Map to store the count of black blocks
-        List<Long> res = new ArrayList<>(5); // Result list to store the final counts
-
-        for (int[] coor : coordinates) {
-            int x = coor[0];
-            int y = coor[1];
-
-            for (int dx = 0; dx <= 1; dx++) {
-                for (int dy = 0; dy <= 1; dy++) {
-                    if (x - dx >= 0 && y - dy >= 0 && x - dx < m - 1 && y - dy < n - 1) {
-                        // Calculate the key for the map using the coordinates
-                        long key = (long) (x - dx) * n + y - dy;
-                        countMap.put(key, countMap.getOrDefault(key, 0) + 1); // Increment the count of black blocks for the key
-                    }
-                }
+        long[] ans = new long[5];
+        int[][] offset = new int[][] {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+        Map<Pair<Integer, Integer>, Integer> map = new HashMap<>();
+        for (int[] c : coordinates) {
+            int x = c[0];
+            int y = c[1];
+            for (int[] d : offset)  {
+                int nx = x + d[0];
+                int ny = y + d[1];
+                if (nx >= m || ny >= n || nx - 1 < 0 || ny - 1 < 0)
+                    continue;
+                Pair<Integer, Integer> p = new Pair<>(nx, ny);
+                map.put(p, map.getOrDefault(p, 0) + 1);
             }
         }
-
-        // Initialize the result array
-        for (int i = 0; i < 5; i++) {
-            res.add(0L);
+        ans[0] = (long)(m - 1) * (n - 1);
+        for (int v : map.values())   {
+            ans[v]++;
+            ans[0]--;
         }
-
-        // Iterate over the countMap and update the result list
-        for (Map.Entry<Long, Integer> entry : countMap.entrySet()) {
-            int count = entry.getValue();
-            res.set(count, res.get(count) + 1); // Increment the count in the result list at the corresponding index
-        }
-
-        long sum = 0;
-        for (long count : res) {
-            sum += count;
-        }
-
-        res.set(0, 1L * (m - 1) * (n - 1) - sum); // Calculate the count of white blocks and store it at index 0
-
-        // Convert the result list to an array
-        long[] resultArray = new long[res.size()];
-        for (int i = 0; i < res.size(); i++) {
-            resultArray[i] = res.get(i);
-        }
-
-        return resultArray; // Return the final result array
+        return ans;
     }
 }
