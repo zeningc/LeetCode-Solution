ddb143728459f746e2d34ad97540e617cc4946bb
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-01-02 21:51:37 -0600

    Time: 247 ms (8.58%), Space: 70.1 MB (11.59%) - LeetHub

--- 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.java
+++ 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.java
@@ -1,18 +1,37 @@
+/*
+The philosophy here is that:
+1. we maintain m such that m is a legal representation of the frequency
+inside the window.
+2. the length of longest array can only be incresed one at a time
+3. we want to maintain the window such that it is legal
+
+So at the end of each iteration, we use the following statement to make the
+window legal:
+if (hi - lo + 1 - maxFreq > k)    {
+    m.put(nums.get(lo), m.get(nums.get(lo)) - 1);
+    lo++;
+}
+It is not intuitive that we are trying to maintain a window by
+hi - lo + 1 - maxFreq > k
+we have already made maxFreq array
+if there is a better solution, and the maxFreq gets updated
+the length of the window will only be the current window + the new element we found
+*/
 class Solution {
     public int longestEqualSubarray(List<Integer> nums, int k) {
-        Map<Integer, List<Integer>> m = new HashMap<>();
-        Map<Integer, Integer> ptrs = new HashMap<>();
-        int ans = 0;
-        for (int i = 0; i < nums.size(); i++)   {
-            m.computeIfAbsent(nums.get(i), x -> new ArrayList<>()).add(i);
-            int ptr = ptrs.getOrDefault(nums.get(i), 0);
-            List<Integer> list = m.getOrDefault(nums.get(i), new ArrayList<>());
-            while (ptr < list.size() && i - list.get(ptr) + 1 - (list.size() - ptr) > k)   {
-                ptr++;
+        int n = nums.size();
+        int lo = 0;
+        int maxFreq = 0;
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int hi = 0; hi < n; hi++) {
+            int num = nums.get(hi);
+            m.put(num, m.getOrDefault(num, 0) + 1);
+            maxFreq = Math.max(maxFreq, m.get(num));
+            if (hi - lo + 1 - maxFreq > k)    {
+                m.put(nums.get(lo), m.get(nums.get(lo)) - 1);
+                lo++;
             }
-            ptrs.put(nums.get(i), ptr);
-            ans = Math.max(ans, list.size() - ptr);
         }
-        return ans;
+        return maxFreq;
     }
 }
