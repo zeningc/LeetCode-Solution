f17f197cc4fa127687c85a419ad3a0383d9d1dca
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-01-11 21:50:28 -0600

    Time: 179 ms (69.04%), Space: 72.9 MB (13.24%) - LeetHub

--- 2953-count-complete-substrings/2953-count-complete-substrings.java
+++ 2953-count-complete-substrings/2953-count-complete-substrings.java
@@ -2,38 +2,37 @@ class Solution {
     public int countCompleteSubstrings(String word, int k) {
         int lo = 0;
         int n = word.length();
-        int[][] pos = new int[26][n + 1];
         int ans = 0;
-        for (int hi = 0; hi < n; hi++)    {
+        int[][] presum = new int[26][n + 1];
+        for (int hi = 0; hi < n; hi++)  {
             char c = word.charAt(hi);
+            char prevCh = word.charAt(hi == 0 ? hi : hi - 1);
             for (int i = 0; i < 26; i++)
-                pos[i][hi + 1] = pos[i][hi];
-            pos[c - 'a'][hi + 1]++;
-            
-            if (hi == 0 || Math.abs(c - word.charAt(hi - 1)) > 2)   {
+                presum[i][hi + 1] = presum[i][hi] + (i == c - 'a' ? 1 : 0);
+            if (Math.abs(prevCh - c) > 2)
                 lo = hi;
-            }
-            if (pos[c - 'a'][hi + 1] - pos[c - 'a'][lo] >= k) {
-                for (int i = 1; i <= 26; i++)   {
-                    int start = hi - i * k + 1;
-                    if (start < lo)
+            for (int i = 1; i <= 26 && i * k <= hi - lo + 1; i++)   {
+                boolean valid = true;
+                for (int j = 0; j < 26; j++)    {
+                    int cnt = presum[j][hi + 1] - presum[j][hi - i * k + 1];
+                    if (cnt != 0 && cnt != k) {
+                        valid = false;
                         break;
-                    boolean valid = true;
-                    for (int j = 0; j < 26; j++)   {
-                        if (pos[j][hi + 1] - pos[j][start] != 0 && pos[j][hi + 1] - pos[j][start] != k)    {
-                            valid = false;
-                            break;
-                        }
                     }
-                    if (valid)  {
-                        ans++;
-                    }
-                        
                 }
+                if (valid)
+                    ans++;
             }
-            
         }
         
         return ans;
     }
 }
+
+// xxxxxxx|xxx|xxxxxxx
+
+// presum[a-z][idx]
+
+// k 2*k ... 26*k
+
+// 26*k*n
