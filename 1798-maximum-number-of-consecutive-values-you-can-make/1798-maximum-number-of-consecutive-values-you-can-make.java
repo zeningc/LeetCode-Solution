225db18d5bfcec0dcc406706bfbb08e715567114
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-01-16 20:52:40 -0600

    Time: 16 ms (100.00%), Space: 52.5 MB (40.74%) - LeetHub

--- 1798-maximum-number-of-consecutive-values-you-can-make/1798-maximum-number-of-consecutive-values-you-can-make.java
+++ 1798-maximum-number-of-consecutive-values-you-can-make/1798-maximum-number-of-consecutive-values-you-can-make.java
@@ -2,10 +2,10 @@ class Solution {
     public int getMaximumConsecutive(int[] coins) {
         int r = 0;
         Arrays.sort(coins);
-        for (int coin : coins)  {
+        for (int coin : coins)
             if (coin <= r + 1)
                 r += coin;
-        }
+        
         
         return r + 1;
     }
