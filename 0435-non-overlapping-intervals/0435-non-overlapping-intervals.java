e8fec7cd843e9b0a6a600898489110ede90bef7f
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-01-20 15:19:19 -0600

    Time: 62 ms (22.22%), Space: 73.2 MB (95.02%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[1] != b[1] ? a[1] - b[1] : b[0] - a[0]);
+        int ans = 0;
+        int i = 0;
+        int minReach = Integer.MIN_VALUE;
+        for (int[] interval : intervals)    {
+            int a = interval[0];
+            int b = interval[1];
+            if (a < minReach)
+                ans++;
+            else
+                minReach = Math.max(minReach, b);
+        }
+        
+        return ans;
+    }
+}
