d9da1ded2be4f498201fe5890d95588a23993012
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-01-21 19:24:09 -0600

    Time: 53 ms (74.11%), Space: 69 MB (67.40%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,13 +1,13 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
         int cnt = 0;
-        Arrays.sort(points, (a, b) -> a[1] == b[1] ? 0 : (a[1] > b[1] ? 1 : -1));
-        long r = Long.MIN_VALUE;
-        for (int[] point : points)  {
-            if (point[0] <= r)
+        Arrays.sort(points, (a, b) -> a[0] == b[0] ? 0 : (a[0] > b[0] ? 1 : -1));
+        long r = Long.MAX_VALUE;
+        for (int i = points.length - 1; i >= 0; i--)    {
+            if (r <= points[i][1])
                 cnt++;
-            else if (point[1] > r)
-                r = point[1];
+            else if (r > points[i][0])
+                r = points[i][0];
         }
         
         return points.length - cnt;
