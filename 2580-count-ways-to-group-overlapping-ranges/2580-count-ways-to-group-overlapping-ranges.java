64b45eca5a96c5f54cdb8877003b4624cc360e3c
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-01-28 12:58:55 -0600

    Time: 19 ms (70.45%), Space: 81.3 MB (39.77%) - LeetHub

--- 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.java
+++ 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.java
@@ -1,25 +1,21 @@
 class Solution {
     public int countWays(int[][] ranges) {
-        int lo = -1;
-        int hi = -1;
         Arrays.sort(ranges, (a, b) -> a[0] - b[0]);
         int grp = 0;
+        int far = Integer.MIN_VALUE;
         for (int[] range : ranges)  {
-            if (hi < range[0])  {
+            if (range[0] > far) {
                 grp++;
-                lo = range[0];
-                hi = range[1];
+                far = range[1];
                 continue;
             }
             
-            hi = Math.max(hi, range[1]);
+            far = Math.max(far, range[1]);
         }
         
-        
-        int mod = (int)1e9 + 7;
         long ans = 1;
         for (int i = 0; i < grp; i++)
-            ans = (ans * 2) % mod;
-        return (int)(ans % mod);
+            ans = (ans * 2) % 1000000007;
+        return (int)ans;
     }
 }
