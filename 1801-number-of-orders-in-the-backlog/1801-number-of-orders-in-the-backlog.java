51538412c0297d4e577a32f4be00c3d0cf33d13f
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-02-27 23:44:02 -0600

    Time: 44 ms (33.90%), Space: 84.6 MB (33.90%) - LeetHub

--- 1801-number-of-orders-in-the-backlog/1801-number-of-orders-in-the-backlog.java
+++ 1801-number-of-orders-in-the-backlog/1801-number-of-orders-in-the-backlog.java
@@ -1,47 +1,51 @@
 class Solution {
     public int getNumberOfBacklogOrders(int[][] orders) {
-        Queue<int[]> sellPQ = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        Queue<int[]> buyPQ = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        
+        PriorityQueue<int[]> sellPQ = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> buyPQ = new PriorityQueue<>((a, b) -> b[0] - a[0]);
         
         for (int[] order : orders)  {
+            int type = order[2];
             int price = order[0];
             int amount = order[1];
-            int orderType = order[2];
-            if (orderType == 0) {
+            if (type == 0)  {
                 while (!sellPQ.isEmpty() && sellPQ.peek()[0] <= price && amount > 0)  {
-                    int[] poll = sellPQ.poll();
-                    int consume = Math.min(amount, poll[1]);
+                    int[] sellOrder = sellPQ.poll();
+                    int sellAmount = sellOrder[1];
+                    int consume = Math.min(amount, sellAmount);
                     amount -= consume;
-                    poll[1] -= consume;
-                    if (poll[1] != 0)
-                        sellPQ.offer(poll);
+                    sellAmount -= consume;
+                    if (sellAmount != 0)    {
+                        sellPQ.offer(new int[] {sellOrder[0], sellAmount});
+                    }
                 }
-                if (amount != 0)    {
+                
+                if (amount != 0)
                     buyPQ.offer(new int[] {price, amount});
-                }
             }
             else    {
                 while (!buyPQ.isEmpty() && buyPQ.peek()[0] >= price && amount > 0)  {
-                    int[] poll = buyPQ.poll();
-                    int consume = Math.min(amount, poll[1]);
+                    int[] buyOrder = buyPQ.poll();
+                    int buyAmount = buyOrder[1];
+                    int consume = Math.min(amount, buyAmount);
                     amount -= consume;
-                    poll[1] -= consume;
-                    if (poll[1] != 0)
-                        buyPQ.offer(poll);
+                    buyAmount -= consume;
+                    if (buyAmount != 0)    {
+                        buyPQ.offer(new int[] {buyOrder[0], buyAmount});
+                    }
                 }
-                if (amount != 0)    {
+                if (amount != 0)
                     sellPQ.offer(new int[] {price, amount});
-                }
             }
         }
         
         long ans = 0;
-        for (int[] p : buyPQ)
-            ans = (ans + p[1]) % 1000000007;
-        for (int[] p : sellPQ)
-            ans = (ans + p[1]) % 1000000007;
+        int mod = (int)1e9 + 7;
+        while (!buyPQ.isEmpty())
+            ans = (ans + buyPQ.poll()[1]) % mod;
         
-        return (int)(ans % 1000000007);
+        while (!sellPQ.isEmpty())
+            ans = (ans + sellPQ.poll()[1]) % mod;
+        
+        return (int)(ans % mod);
     }
 }
