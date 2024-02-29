3d91660cdcb6a0de6c460bf171f926d2bf70c6d2
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-02-28 20:19:03 -0600

    Time: 56 ms (28.85%), Space: 54.4 MB (7.69%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
@@ -1,74 +1,31 @@
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
-        TreeMap<Integer, Integer> rangeMap = new TreeMap<>();
-        Map<Integer, Integer> arriveMap = new HashMap<>();
-        Map<Integer, List<Integer>> leaveMap = new HashMap<>();
-        Map<Integer, Integer> chairMap = new HashMap<>();
-        TreeSet<Integer> timeSet = new TreeSet<>();
-        for (int i = 0; i < times.length; i++)    {
-            int arrive = times[i][0];
-            int leave = times[i][1];
-            timeSet.add(arrive);
-            timeSet.add(leave);
-            if (!leaveMap.containsKey(leave))
-                leaveMap.put(leave, new LinkedList<>());
-            arriveMap.put(arrive, i);
-            leaveMap.get(leave).add(i);
+        PriorityQueue<int[]> leaveQ = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> arriveQ = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> empty = new PriorityQueue<>();
+        int[] seat = new int[times.length];
+        for (int i = 0; i < times.length; i++)  {
+            leaveQ.offer(new int[] {times[i][1], i});
+            arriveQ.offer(new int[] {times[i][0], i});
         }
-        
-        for (int t : timeSet)    {
-            if (leaveMap.containsKey(t))    {
-                for (int p : leaveMap.get(t))   {
-                    int pos = chairMap.get(p);
-                    Integer start = rangeMap.floorKey(pos);
-                    int end = rangeMap.get(start);
-                    rangeMap.remove(start);
-                    if (pos != start)   {
-                        rangeMap.put(start, pos - 1);
-                    }
-                    if (pos != end) {
-                        rangeMap.put(pos + 1, end);
-                    }
-                    chairMap.remove(p);
+        int ans = 0;
+        int cnt = 0;
+        for (int i = 0; !arriveQ.isEmpty(); i++)  {
+            while (!leaveQ.isEmpty() && leaveQ.peek()[0] == i)
+                empty.offer(seat[leaveQ.poll()[1]]);
+            if (!arriveQ.isEmpty() && arriveQ.peek()[0] == i)  {
+                int p = arriveQ.poll()[1];
+                if (empty.isEmpty())    {
+                    seat[p] = cnt++;
                 }
-            }
-            
-            if (arriveMap.containsKey(t))   {
-                int p = arriveMap.get(t);
-                int pos = 0;
-                if (!rangeMap.isEmpty())    {
-                    Integer first = rangeMap.firstKey();
-                    if (first > 0)
-                        pos = 0;
-                    else    {
-                        pos = rangeMap.get(first) + 1;
-                    }
+                else    {
+                    seat[p] = empty.poll();
                 }
-                
                 if (p == targetFriend)
-                    return pos;
-                
-                Integer prev = rangeMap.floorKey(pos);
-                Integer next = rangeMap.ceilingKey(pos);
-                int start = pos;
-                int end = pos;
-                if (prev != null && rangeMap.get(prev) == pos - 1) {
-                    start = prev;
-                    end = pos;
-                    rangeMap.remove(prev);
-                }
-                
-                if (next != null && next == pos + 1)    {
-                    end = rangeMap.get(next);
-                    rangeMap.remove(next);
-                }
-                
-                rangeMap.put(start, end);
-                chairMap.put(p, pos);
+                    return seat[p];
             }
         }
         
         return -1;
     }
 }
-
