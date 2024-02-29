cbe1bfed85a0c55b2f1f929df57080a194850d97
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-02-28 20:24:15 -0600

    Time: 94 ms (7.69%), Space: 55.1 MB (7.69%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
@@ -3,14 +3,17 @@ class Solution {
         Map<Integer, List<Integer>> leave = new HashMap<>();
         Map<Integer, Integer> arrive = new HashMap<>();
         PriorityQueue<Integer> empty = new PriorityQueue<>();
+        TreeSet<Integer> set = new TreeSet<>();
         int[] seat = new int[times.length];
         for (int i = 0; i < times.length; i++)  {
             leave.computeIfAbsent(times[i][1], x -> new ArrayList<>()).add(i);
             arrive.put(times[i][0], i);
+            set.add(times[i][0]);
+            set.add(times[i][1]);
         }
         int ans = 0;
         int cnt = 0;
-        for (int i = 0; ; i++)  {
+        for (int i : set)  {
             for (int p : leave.getOrDefault(i, new ArrayList<>()))
                 empty.offer(seat[p]);
             if (arrive.containsKey(i))  {
@@ -25,6 +28,6 @@ class Solution {
                     return seat[p];
             }
         }
-        
+        return -1;
     }
 }
