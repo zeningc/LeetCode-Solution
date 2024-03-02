c621271b438333e48d5ab6f4c465cf8164db25a3
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-03-02 12:04:17 -0600

    Time: 264 ms (28.57%), Space: 71.9 MB (92.31%) - LeetHub

--- 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.java
+++ 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.java
@@ -1,40 +1,29 @@
 class SORTracker {
-    Map<Integer, String> map;
-    Queue<int[]> maxQ;
-    Queue<int[]> minQ;
-    int idx;
+    PriorityQueue<int[]> minPQ;
+    PriorityQueue<int[]> maxPQ;
+    Map<Integer, String> nameMap;
+    int queryCnt;
+    int cnt;
     public SORTracker() {
-        map = new HashMap<>();
-        minQ = new PriorityQueue<>((a, b) -> a[1] == b[1] ? map.get(b[0]).compareTo(map.get(a[0])) : a[1] - b[1]);
-        maxQ = new PriorityQueue<>((a, b) -> b[1] == a[1] ? map.get(a[0]).compareTo(map.get(b[0])) : b[1] - a[1]);
-        idx = 0;
+        minPQ = new PriorityQueue<>((a, b) -> a[1] != b[1] ? a[1] - b[1] : nameMap.get(b[0]).compareTo(nameMap.get(a[0])));
+        maxPQ = new PriorityQueue<>((a, b) -> a[1] != b[1] ? b[1] - a[1] : nameMap.get(a[0]).compareTo(nameMap.get(b[0])));
+        nameMap = new HashMap<>();
+        queryCnt = 0;
+        cnt = 0;
     }
     
     public void add(String name, int score) {
-        int index = idx;
-        idx++;
-        map.put(index, name);
-        int[] insert = new int[] {index, score};
-        int[] peek = minQ.peek();
-        if (minQ.isEmpty() || peek[1] > insert[1] || peek[1] == insert[1] && map.get(peek[0]).compareTo(name) < 0) {
-            maxQ.offer(insert);
-        }
-        else    {
-            minQ.offer(insert);
-            maxQ.offer(minQ.poll());
-        }
-        
-        return;
+        int id = cnt++;
+        nameMap.put(id, name);
+        minPQ.offer(new int[] {id, score});
+        maxPQ.offer(minPQ.poll());
     }
     
     public String get() {
-        String ret = map.get(maxQ.peek()[0]);
-        
-        minQ.offer(maxQ.poll());
-        
-        return ret;
+        queryCnt++;
+        minPQ.offer(maxPQ.poll());
+        return nameMap.get(minPQ.peek()[0]);
     }
-    
 }
 
 /**
