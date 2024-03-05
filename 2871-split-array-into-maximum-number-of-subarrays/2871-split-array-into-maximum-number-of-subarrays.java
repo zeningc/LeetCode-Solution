80cb5b7d08aaeec1f1db87a7b697f2114b11609d
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-03-04 22:15:35 -0600

    Time: 3 ms (100.00%), Space: 56.2 MB (85.33%) - LeetHub

--- 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.java
+++ 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.java
@@ -3,37 +3,17 @@ class Solution {
         int res = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++)
             res &= nums[i];
-        
-        boolean[] pos = new boolean[32];
-        for (int i = 0; i < 32; i++)    
-            if ((res & (1 << i)) != 0)
-                pos[i] = true;
-        
+        if (res > 0)
+            return 1;
         int ans = 0;
         int pre = Integer.MAX_VALUE;
-        boolean valid = true;
         for (int i = 0; i < nums.length; i++)   {
             pre &= nums[i];
-            valid = true;
-            for (int j = 0; j < 32; j++)    {
-                if (pos[j])
-                    continue;
-                if ((pre & (1 << j)) != 0)  {
-                    valid = false;
-                    break;
-                }
-            }
-            if (valid)  {
+            if (pre == 0)  {
                 pre = Integer.MAX_VALUE;
                 ans++;
-                for (int j = 0; j < 32; j++)    {
-                    if (pos[j])
-                        pos[j] = false;
-                }
             }
-            
         }
-        
         return ans;
     }
 }
