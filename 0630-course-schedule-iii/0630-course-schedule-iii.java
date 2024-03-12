e6563cf437688bb0da7e97d17a619b18a3de2a33
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-03-12 09:33:26 -0500

    Time: 37 ms (45.79%), Space: 54.5 MB (79.49%) - LeetHub

--- 0630-course-schedule-iii/0630-course-schedule-iii.java
+++ 0630-course-schedule-iii/0630-course-schedule-iii.java
@@ -1,24 +1,16 @@
 class Solution {
     public int scheduleCourse(int[][] courses) {
-        Arrays.sort(courses, (a, b) -> a[1] != b[1] ? a[1] - b[1] : a[0] - b[0]);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] != b[0] ? b[0] - a[0] : a[1] - b[1]);
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
         int used = 0;
         for (int[] course : courses)    {
             int d = course[0];
             int last = course[1];
-            if (d + used <= last)   {
-                used += d;
-                pq.offer(course);
-                continue;
-            }
-            int[] peek = pq.peek();
-            if (peek == null || peek[0] < d)   {
-                continue;
-            }
-            pq.poll();
-            pq.offer(course);
-            used -= peek[0];
             used += d;
+            pq.offer(course);
+            if (used > last)    {
+                used -= pq.poll()[0];
+            }
         }
         
         return pq.size();
