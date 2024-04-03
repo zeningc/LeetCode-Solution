b9cda8321ab5dc2423c8868e67047e852488d8da
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-04-02 23:17:51 -0500

    Time: 42 ms (24.72%), Space: 57.8 MB (17.60%) - LeetHub

--- 3082-find-the-sum-of-the-power-of-all-subsequences/3082-find-the-sum-of-the-power-of-all-subsequences.java
+++ 3082-find-the-sum-of-the-power-of-all-subsequences/3082-find-the-sum-of-the-power-of-all-subsequences.java
@@ -1,21 +1,26 @@
 class Solution {
     public int sumOfPower(int[] nums, int k) {
         int n = nums.length;
-        long[][] dp = new long[n + 1][k + 1];
-        dp[0][0] = 1;
+        long[][][] dp = new long[n + 1][n + 1][k + 1];
+        for (int i = 0; i <= n; i++)
+            dp[i][0][0] = 1;
+
         long power = 1;
         int mod = (int)1e9 + 7;
-        for (int num : nums) {
-            for (int v = k; v >= num; v--) {
-                  for (int j = 1; j <= n; j++) {
-                    dp[j][v] = (dp[j][v] + dp[j - 1][v - num]) % mod;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                for (int v = 1; v <= k; v++)  {
+                    dp[i][j][v] = dp[i - 1][j][v];
+                    if (nums[i - 1] > v)
+                        continue;
+                    dp[i][j][v] = (dp[i][j][v] + dp[i - 1][j - 1][v - nums[i - 1]]) % mod;
                 }
             }
         }
-                                                     
+
         long ans = 0;
         for (int i = n; i >= 1; i--)    {
-            ans = (ans + dp[i][k] * power) % mod;
+            ans = (ans + dp[n][i][k] * power) % mod;
             power = (power * 2) % mod;
         }
         return (int)(ans % mod);
