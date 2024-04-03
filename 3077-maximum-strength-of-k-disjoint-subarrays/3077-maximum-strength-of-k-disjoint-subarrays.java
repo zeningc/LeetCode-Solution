0e843c4d929405ca8c563304d7523986098b608e
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-04-02 20:47:44 -0500

    Time: 51 ms (64.13%), Space: 63.2 MB (58.73%) - LeetHub

--- 3077-maximum-strength-of-k-disjoint-subarrays/3077-maximum-strength-of-k-disjoint-subarrays.java
+++ 3077-maximum-strength-of-k-disjoint-subarrays/3077-maximum-strength-of-k-disjoint-subarrays.java
@@ -1,29 +1,26 @@
 class Solution {
     public long maximumStrength(int[] nums, int k) {
         int n = nums.length;
-        long[][] dp = new long[n][k];
-        for (int j = 0; j < k; j++)
-            for (int i = 0; i < n; i++)
+        long[][] dp = new long[n + 1][k + 1];
+        for (int i = 0; i <= n; i++)
+            for (int j = 1; j <= k; j++)
                 dp[i][j] = Long.MIN_VALUE / 2;
-        int sign = 1;
-        long[] prev = new long[n];
-        for (int j = 0; j < k; j++) {
-            long mul = k - j;
-            int left = k - j - 1;
-            for (int i = j; i < n - left; i++) {
-                dp[i][j] = Math.max(i == 0 ? Long.MIN_VALUE / 2 : dp[i - 1][j], j == 0 ? 0 : prev[i - 1]) + mul * sign * nums[i];
+        long sign = 1;
+        long[] prev = new long[n + 1];
+        for (int j = 1; j <= k; j++)    {
+            long mul = k - j + 1;
+            long[] cur = new long[n + 1];
+            Arrays.fill(cur, Long.MIN_VALUE / 2);
+            for (int i = j; i <= n - k + j; i++)    {
+                dp[i][j] = Math.max(dp[i - 1][j], prev[i - 1]) + mul * sign * nums[i - 1];
+                cur[i] = Math.max(cur[i - 1], dp[i][j]);
             }
-            
-            prev = new long[n];
-            Arrays.fill(prev, Long.MIN_VALUE / 2);
-            for (int i = j; i < n - left; i++) {
-                prev[i] = Math.max(i == 0 ? Long.MIN_VALUE / 2 : prev[i - 1], dp[i][j]);
-            }
+            prev = cur;
             sign = -sign;
         }
         
         long ans = Long.MIN_VALUE;
-        for (int i = k - 1; i < n; i++)
+        for (int i = 0; i <= n; i++)
             ans = Math.max(ans, prev[i]);
         
         return ans;
