1643c6eb24123eedf78f211236b12049f138c827
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-04-02 22:27:29 -0500

    Time: 25 ms (41.06%), Space: 43.9 MB (79.47%) - LeetHub

--- 3082-find-the-sum-of-the-power-of-all-subsequences/3082-find-the-sum-of-the-power-of-all-subsequences.java
+++ 3082-find-the-sum-of-the-power-of-all-subsequences/3082-find-the-sum-of-the-power-of-all-subsequences.java
@@ -5,10 +5,10 @@ class Solution {
         dp[0][0] = 1;
         long power = 1;
         int mod = (int)1e9 + 7;
-        for (int i = 1; i <= n; i++) {
-            for (int j = n; j >= 1; j--) {
-                  for (int v = nums[i - 1]; v <= k; v++) {
-                    dp[j][v] = (dp[j][v] + dp[j - 1][v - nums[i - 1]]) % mod;
+        for (int num : nums) {
+            for (int v = k; v >= num; v--) {
+                  for (int j = 1; j <= n; j++) {
+                    dp[j][v] = (dp[j][v] + dp[j - 1][v - num]) % mod;
                 }
             }
         }
