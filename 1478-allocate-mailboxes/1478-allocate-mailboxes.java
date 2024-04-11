3a78f4b1c18f4e73c26354099c03cca31ba8e736
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-04-10 22:39:17 -0500

    Time: 9 ms (92.99%), Space: 41.6 MB (66.88%) - LeetHub

--- 1478-allocate-mailboxes/1478-allocate-mailboxes.java
+++ 1478-allocate-mailboxes/1478-allocate-mailboxes.java
@@ -1,28 +1,34 @@
 class Solution {
     public int minDistance(int[] houses, int k) {
-        int n = houses.length;
         Arrays.sort(houses);
+        int n = houses.length;
+        int[][] ranges = new int[n][n];
         int[] presum = new int[n + 1];
+        for (int i = 1; i <= n; i++)
+            presum[i] = presum[i - 1] + houses[i - 1];
         for (int i = 0; i < n; i++)
-            presum[i + 1] = presum[i] + houses[i];
-        int[][] range = new int[n + 1][n + 1];
-        for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                int mid = (i + j) / 2;
-                if ((j - i + 1) % 2 == 0)
-                    range[i + 1][j + 1] = presum[j + 1] - 2 * presum[mid + 1] + presum[i];
-                else
-                    range[i + 1][j + 1] = presum[j + 1] - presum[mid + 1] - presum[mid] + presum[i];
+                int mid = i + (j - i) / 2;
+                ranges[i][j] = houses[mid] * (mid - i) - (presum[mid] - presum[i]) + (presum[j + 1] - presum[mid + 1]) - houses[mid] * (j - mid);
+            }
+        
+        int[][] dp = new int[n][k];
+        for (int i = 0; i < n; i++)
+            Arrays.fill(dp[i], Integer.MAX_VALUE / 2);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                if (i == 0) {
+                    dp[i][j] = 0;
+                }
+                for (int x = 0; x < i; x++) {
+                    if (j == 0)
+                        dp[i][j] = ranges[0][i];
+                    else
+                        dp[i][j] = Math.min(dp[i][j], dp[x][j - 1] + ranges[x + 1][i]);
+                }
             }
         }
-        int[][] dp = new int[n + 1][k + 1];
-        for (int i = 1; i <= n; i++)
-            Arrays.fill(dp[i], Integer.MAX_VALUE / 2);
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= k; j++)
-                for (int x = 0; x < i; x++)
-                    dp[i][j] = Math.min(dp[i][j], dp[x][j - 1] + range[x + 1][i]);
-
-        return dp[n][k];
+        
+        return dp[n - 1][k - 1];
     }
 }
