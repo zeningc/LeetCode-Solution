96bebf7c32ba7af776eab951c2b3e6f38c986c2f
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-04-11 21:50:49 -0500

    Time: 2 ms (98.91%), Space: 41.3 MB (94.09%) - LeetHub

--- 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.java
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.java
@@ -1,68 +1,109 @@
 class Solution {
     public String nearestPalindromic(String n) {
         long num = Long.valueOf(n);
-        if (num < 10)
-            return String.valueOf(num - 1);
-        String flipPartStr = n.substring(0, ((n.length() + 1) / 2));
-        long flipPart = Long.valueOf(flipPartStr);
-            
-        long a = flip(flipPart, num, n);
-        if (a == num)   {
-            if (String.valueOf(flipPart + 1).length() > flipPartStr.length())
-                a = Long.valueOf("1" + repeat('0', n.length() - 1) + "1");
-            else
-                a = flip(flipPart + 1, num, n);
+        long a = getNextGreaterPolindrome(num);
+        long b = getNextSmallerPolindrome(num);
+        if (Math.abs(a - num) < Math.abs(b - num))
+            return new String(numToCharArr(a));
+        return new String(numToCharArr(b));
+    }
+    
+    long getNextGreaterPolindrome(long num) {
+        char[] ch = numToCharArr(num);
+        char[] flip = flip(ch);
+        if (compare(ch, flip) < 0)
+            return charArrToNum(flip);
+        int c = 1;
+        for (int i = ch.length % 2 == 0 ? (ch.length - 1) / 2 : ch.length / 2; i >= 0; i--)    {
+            int bit = ch[i] - '0' + c;
+            ch[i] = (char)('0' + bit % 10);
+            c = bit / 10;
         }
-        long b;
-        if (a > num)    {
-            if (flipPart - 1 == 0 || String.valueOf(flipPart - 1).length() < flipPartStr.length())
-                b = Long.valueOf(repeat('9', n.length() - 1));
-            else
-                b = flip(flipPart - 1, num, n);
+        if (c == 1)
+            return smallestWithNBit(ch.length + 1);
+        return charArrToNum(flip(ch));
+    }
+    
+    long getNextSmallerPolindrome(long num) {
+        if (num == 1)
+            return 0;
+        char[] ch = numToCharArr(num);
+        char[] flip = flip(ch);
+        if (compare(ch, flip) > 0)
+            return charArrToNum(flip);
+        int c = 1;
+        for (int i = ch.length % 2 == 0 ? (ch.length - 1) / 2 : ch.length / 2; i >= 0; i--)    {
+            int bit = ch[i] - '0' - c;
+            ch[i] = (char)('0' + (bit < 0 ? bit + 10 : bit));
+            c = bit < 0 ? 1 : 0;
         }
-        else    {
-            if (String.valueOf(flipPart + 1).length() > flipPartStr.length())
-                b = Long.valueOf("1" + repeat('0', n.length() - 1) + "1");
-            else
-                b = flip(flipPart + 1, num, n);
+        if (c == 1 || ch[0] == '0')
+            return largestWithNBit(ch.length - 1);
+        return charArrToNum(flip(ch));
+    }
+    
+    char[] numToCharArr(long num)   {
+        if (num == 0)
+            return new char[] {'0'};
+        long t = num;
+        int n = 0;
+        while (t != 0)  {
+            t /= 10;
+            n++;
+        }
+        char[] ch = new char[n];
+        t = num;
+        for (int i = n - 1; i >= 0; i--)    {
+            ch[i] = (char)((int)(t % 10) + '0');
+            t /= 10;
         }
         
-        return closest(a, b, num);
+        return ch;
     }
     
-    long flip(long part, long num, String nStr)    {
-        String partStr = String.valueOf(part);
-        if (partStr.length() * 2 == nStr.length())    {
-            return Long.valueOf(partStr + new StringBuilder(partStr).reverse().toString());
-        }
-        return Long.valueOf(partStr + new StringBuilder(partStr.substring(0, partStr.length() - 1)).reverse().toString());
-    }
-    
-    String repeat(char c, int len)  {
-        StringBuilder sb = new StringBuilder();
-        while (len-- > 0)
-            sb.append(c);
-        return sb.toString();
-    }
-    
-    String closest(long a, long b, long num)    {
-        long gap1 = Math.abs(a - num);
-        long gap2 = Math.abs(b - num);
-        if (gap1 == gap2)
-            return String.valueOf(Math.min(a, b));
-        return String.valueOf(gap1 < gap2 ? a : b);
-    }
-    
-    
-    boolean isPalindrome(String n)  {
+    char[] flip(char[] ch)   {
         int i = 0;
-        int j = n.length() - 1;
-        while (i < j)   {
-            if (n.charAt(i) != n.charAt(j))
-                return false;
-            i++;
-            j--;
+        int j = ch.length - 1;
+        char[] ret = new char[ch.length];
+        ret = ch.clone();
+        while (i <= j)  {
+            ret[j--] = ret[i++];
         }
-        return true;
+        return ret;
+    }
+    
+    int compare(char[] a, char[] b) {
+        for (int i = 0; i < a.length; i++)  {
+            if (a[i] == b[i])
+                continue;
+            if (a[i] < b[i])
+                return -1;
+            return 1;
+        }
+        return 0;
+    }
+    
+    long charArrToNum(char[] ch)    {
+        long num = 0;
+        for (char c : ch)   {
+            num = num * 10 + (c - '0');
+        }
+        
+        return num;
+    }
+    
+    long smallestWithNBit(int n)    {
+        long num = 1;
+        for (int i = 0; i < n - 2; i++)
+            num = num * 10;
+        num = num * 10 + 1;
+        return num;
+    }
+    
+    long largestWithNBit(int n)    {
+        long num = 9;
+        for (int i = 0; i < n - 1; i++)
+            num = num * 10 + 9;
+        return num;
     }
 }
