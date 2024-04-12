f537bc8b4b8d052c636accfb3e249372b767df20
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-04-11 21:44:52 -0500

    Time: 16 ms (71.90%), Space: 56.2 MB (58.68%) - LeetHub

--- 2967-minimum-cost-to-make-array-equalindromic/2967-minimum-cost-to-make-array-equalindromic.java
+++ 2967-minimum-cost-to-make-array-equalindromic/2967-minimum-cost-to-make-array-equalindromic.java
@@ -1,57 +1,125 @@
 class Solution {
     public long minimumCost(int[] nums) {
-        int n = nums.length;
         Arrays.sort(nums);
-        long median = n % 2 == 0 ? (nums[n / 2] + nums[(n - 1) / 2]) / 2 : nums[n / 2];
-        if (median < 10)
-            return getCost(nums, median);
-        
-        String numStr = String.valueOf(median);
-        String flipPartStr = numStr.substring(0, (numStr.length() + 1) / 2);
-        long flipPart = Long.valueOf(flipPartStr);
-        long a = flip(numStr.length(), flipPartStr);
-        if (a == median)
-            return getCost(nums, a);
-        long b;
-        if (a < median) {
-            if (String.valueOf(flipPart + 1).length() > String.valueOf(flipPart).length())    {
-                StringBuilder sb = new StringBuilder();
-                sb.append('1');
-                for (int i = 0; i < numStr.length() - 1; i++)
-                    sb.append('0');
-                sb.append('1');
-                b = Long.valueOf(sb.toString());
-            }
-            else    {
-                b = flip(numStr.length(), String.valueOf(flipPart + 1));
-            }
-        }
-        else    {
-            if (flipPart - 1 == 0 || String.valueOf(flipPart - 1).length() < String.valueOf(flipPart).length())    {
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < numStr.length() - 1; i++)
-                    sb.append('9');
-                b = Long.valueOf(sb.toString());
-            }
-            else    {
-                b = flip(numStr.length(), String.valueOf(flipPart - 1));
-            }
-        }
-        
-        return Math.min(getCost(nums, a), getCost(nums, b));
+        int n = nums.length;
+        int m = n % 2 == 1 ? nums[n / 2] : (nums[(n - 1) / 2] + nums[n / 2]) / 2;
+        long a = getNextGreaterPolindrome(m);
+        long b = getNextSmallerPolindrome(m);
+        return Math.min(isPalindrome(m) ? getCost(nums, m) : Long.MAX_VALUE, Math.min(getCost(nums, a), getCost(nums, b)));
     }
     
-    long flip(int len, String str)  {
-        if (len % 2 == 0)   {
-            return Long.valueOf(str + new StringBuilder(str).reverse().toString());
-        }
-        return Long.valueOf(str + new StringBuilder(str.substring(0, str.length() - 1)).reverse().toString());
-    }
-    
-    long getCost(int[] nums, long target)    {
-        long sum = 0;
+    long getCost(int[] nums, long p) {
+        long ans = 0;
         for (int num : nums)
-            sum += Math.abs(target - num);
-        return sum;
+            ans += Math.abs((long)num - p);
+        return ans;
+    }
+
+    boolean isPalindrome(long num)  {
+        char[] ch = numToCharArr(num);
+        int i = 0;
+        int j = ch.length - 1;
+        while (i < j)   {
+            if (ch[i] != ch[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    
+    long getNextGreaterPolindrome(long num) {
+        char[] ch = numToCharArr(num);
+        char[] flip = flip(ch);
+        if (compare(ch, flip) < 0)
+            return charArrToNum(flip);
+        int c = 1;
+        for (int i = ch.length % 2 == 0 ? (ch.length - 1) / 2 : ch.length / 2; i >= 0; i--)    {
+            int bit = ch[i] - '0' + c;
+            ch[i] = (char)('0' + bit % 10);
+            c = bit / 10;
+        }
+        if (c == 1)
+            return smallestWithNBit(ch.length + 1);
+        return charArrToNum(flip(ch));
+    }
+    
+    long getNextSmallerPolindrome(long num) {
+        char[] ch = numToCharArr(num);
+        char[] flip = flip(ch);
+        if (compare(ch, flip) > 0)
+            return charArrToNum(flip);
+        int c = 1;
+        for (int i = ch.length % 2 == 0 ? (ch.length - 1) / 2 : ch.length / 2; i >= 0; i--)    {
+            int bit = ch[i] - '0' - c;
+            ch[i] = (char)('0' + (bit < 0 ? bit + 10 : bit));
+            c = bit < 0 ? 1 : 0;
+        }
+        if (c == 1 || ch[0] == '0')
+            return largestWithNBit(ch.length - 1);
+        return charArrToNum(flip(ch));
+    }
+    
+    char[] numToCharArr(long num)   {
+        long t = num;
+        int n = 0;
+        while (t != 0)  {
+            t /= 10;
+            n++;
+        }
+        char[] ch = new char[n];
+        t = num;
+        for (int i = n - 1; i >= 0; i--)    {
+            ch[i] = (char)((int)(t % 10) + '0');
+            t /= 10;
+        }
+        
+        return ch;
+    }
+    
+    char[] flip(char[] ch)   {
+        int i = 0;
+        int j = ch.length - 1;
+        char[] ret = new char[ch.length];
+        ret = ch.clone();
+        while (i <= j)  {
+            ret[j--] = ret[i++];
+        }
+        return ret;
+    }
+    
+    int compare(char[] a, char[] b) {
+        for (int i = 0; i < a.length; i++)  {
+            if (a[i] == b[i])
+                continue;
+            if (a[i] < b[i])
+                return -1;
+            return 1;
+        }
+        return 0;
+    }
+    
+    long charArrToNum(char[] ch)    {
+        long num = 0;
+        for (char c : ch)   {
+            num = num * 10 + (c - '0');
+        }
+        
+        return num;
+    }
+    
+    long smallestWithNBit(int n)    {
+        long num = 1;
+        for (int i = 0; i < n - 2; i++)
+            num = num * 10;
+        num = num * 10 + 1;
+        return num;
+    }
+    
+    long largestWithNBit(int n)    {
+        long num = 9;
+        for (int i = 0; i < n - 1; i++)
+            num = num * 10 + 9;
+        return num;
     }
 }
