0646fbbfa2633d0c45926919aa4c405a7ee68259
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-04-13 16:11:51 -0500

    Time: 43 ms (6.85%), Space: 56.3 MB (58.90%) - LeetHub

--- 2968-apply-operations-to-maximize-frequency-score/2968-apply-operations-to-maximize-frequency-score.java
+++ 2968-apply-operations-to-maximize-frequency-score/2968-apply-operations-to-maximize-frequency-score.java
@@ -1,32 +1,33 @@
 class Solution {
     public int maxFrequencyScore(int[] nums, long k) {
         int n = nums.length;
-        int lo = 0;
         Arrays.sort(nums);
+        long[] presum = new long[n];
+        int l = 0;
         int ans = 0;
-        int p = -1;
-        long presum = 0;
-        long sufsum = 0;
-        for (int hi = 0; hi < n; hi++)  {
-            sufsum += nums[hi];
-            while (true)    {
-                int mid = lo + (hi - lo) / 2;
-                long median = nums[mid];
-                for (int i = p + 1; i <= mid; i++)  {
-                    presum += nums[i];
-                    sufsum -= nums[i];
+        for (int r = 0; r < n; r++) {
+            presum[r] = (r == 0 ? 0 : presum[r - 1]) + nums[r];
+            if (!check(presum, nums, l, r, k))  {
+                int lo = l;
+                int hi = r;
+                while (lo <= hi)    {
+                    int mid = lo + (hi - lo) / 2;
+                    if (check(presum, nums, mid, r, k))
+                        hi = mid - 1;
+                    else
+                        lo = mid + 1;
                 }
-                p = mid;
-                long need = median * (2 * mid - lo - hi + 1) - presum + sufsum;
-                if (need > k) {
-                    presum -= nums[lo];
-                    lo++;
-                    continue;
-                }
-                break;
+                l = lo;
             }
-            ans = Math.max(ans, hi - lo + 1);
+            ans = Math.max(ans, r - l + 1);
         }
         return ans;
+    }
+    
+    boolean check(long[] presum, int[] nums, int lo, int hi, long k) {
+        int mid = lo + (hi - lo) / 2;
+        long pre = (long)nums[mid] * (mid - lo + 1) - (presum[mid] - (lo == 0 ? 0 : presum[lo - 1]));
+        long suf = (long)(presum[hi] - presum[mid] - (long)(hi - mid) * nums[mid]);
+        return pre + suf <= k;
     }
 }
