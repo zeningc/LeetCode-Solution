0212430198119f2f250578da10baa26010bbd2f9
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-04-13 14:07:17 -0500

    Time: 16 ms (6.25%), Space: 59.6 MB (5.29%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -1,16 +1,25 @@
 class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
-        int n = nums.length;
-        int[] ans = new int[n];
-        int sum = 0;
-        for (int i = 1; i < n; i++) 
-            sum += nums[i] - nums[0];
-        
-        ans[0] = sum;
-        for (int i = 1; i < n; i++) {
-            sum += i * (nums[i] - nums[i - 1]);
-            sum -= (n - i) * (nums[i] - nums[i - 1]);
-            ans[i] = sum;
+        List<int[]> numList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++)
+            numList.add(new int[] {i, nums[i]});
+        Collections.sort(numList, (a, b) -> a[1] - b[1]);
+        long sum = 0;
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++)   {
+            int[] cur = numList.get(i);
+            int idx = cur[0];
+            int num = cur[1];
+            ans[idx] += (int)((long)i * num - sum);
+            sum += num;
+        }
+        sum = 0;
+        for (int i = nums.length - 1; i >= 0; i--)   {
+            int[] cur = numList.get(i);
+            int idx = cur[0];
+            int num = cur[1];
+            ans[idx] += (int)(sum - (long)(nums.length - i - 1) * num);
+            sum += num;
         }
         
         return ans;
