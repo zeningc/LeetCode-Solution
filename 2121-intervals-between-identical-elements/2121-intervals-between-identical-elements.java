a91693f46793b9f797253c7bf3b6460cfd86af46
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-04-15 16:09:44 -0500

    Time: 152 ms (37.36%), Space: 70.9 MB (62.64%) - LeetHub

--- 2121-intervals-between-identical-elements/2121-intervals-between-identical-elements.java
+++ 2121-intervals-between-identical-elements/2121-intervals-between-identical-elements.java
@@ -1,21 +1,19 @@
 class Solution {
     public long[] getDistances(int[] arr) {
-        Map<Integer, Integer> freq = new HashMap<>();
-        Map<Integer, Long> sum = new HashMap<>();
+        Map<Integer, Long> m = new HashMap<>();
+        Map<Integer, Long> freq = new HashMap<>();
         long[] ans = new long[arr.length];
         for (int i = 0; i < arr.length; i++)    {
-            int f = freq.getOrDefault(arr[i], 0);
-            ans[i] += (long)f * i - sum.getOrDefault(arr[i], 0L);
-            freq.put(arr[i], freq.getOrDefault(arr[i], 0) + 1);
-            sum.put(arr[i], sum.getOrDefault(arr[i], 0L) + i);
+            freq.put(arr[i], freq.getOrDefault(arr[i], 0L) + 1);
+            m.put(arr[i], m.getOrDefault(arr[i], 0L) + i);
+            ans[i] += freq.get(arr[i]) * i - m.get(arr[i]);
         }
         freq = new HashMap<>();
-        sum = new HashMap<>();
-        for (int i = arr.length - 1; i >= 0; i--)    {
-            int f = freq.getOrDefault(arr[i], 0);
-            ans[i] += sum.getOrDefault(arr[i], 0L) - (long)f * i;
-            freq.put(arr[i], freq.getOrDefault(arr[i], 0) + 1);
-            sum.put(arr[i], sum.getOrDefault(arr[i], 0L) + i);
+        m = new HashMap<>();
+        for (int i = arr.length - 1; i >= 0; i--)   {
+            freq.put(arr[i], freq.getOrDefault(arr[i], 0L) + 1);
+            m.put(arr[i], m.getOrDefault(arr[i], 0L) + i);
+            ans[i] += m.get(arr[i]) - freq.get(arr[i]) * i;
         }
         return ans;
     }
