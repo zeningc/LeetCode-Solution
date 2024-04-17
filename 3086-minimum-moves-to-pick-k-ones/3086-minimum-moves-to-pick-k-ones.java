2ea84ee57396ab4c4e99e345c9785dbd9296a944
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-04-16 21:44:34 -0500

    Time: 19 ms (29.15%), Space: 57.5 MB (42.03%) - LeetHub

--- 3086-minimum-moves-to-pick-k-ones/3086-minimum-moves-to-pick-k-ones.java
+++ 3086-minimum-moves-to-pick-k-ones/3086-minimum-moves-to-pick-k-ones.java
@@ -1,108 +1,39 @@
 class Solution {
     public long minimumMoves(int[] nums, int k, int maxChanges) {
-        int n = nums.length;
-        int[] cnt1 = new int[n];
+        List<Integer> arr = new ArrayList<>();
         for (int i = 0; i < nums.length; i++)
-            cnt1[i] = (i == 0 ? 0 : cnt1[i - 1]) + (nums[i] == 0 ? 0 : 1);
-
-        int[] cnt2 = new int[n];
-        for (int i = n - 1; i >= 0; i--)
-            cnt2[i] = (i == n - 1 ? 0 : cnt2[i + 1]) + (nums[i] == 0 ? 0 : 1);
-
-        long[] presum1 = new long[n];
-
-        for (int i = 0; i < n; i++)
-            presum1[i] = i == 0 ? 0 : presum1[i - 1] + cnt1[i - 1];
-
-        long[] presum2 = new long[n];
-
-        for (int i = n - 1; i >= 0; i--)
-            presum2[i] = i == n - 1 ? 0 : presum2[i + 1] + cnt2[i + 1];
-
+            if (nums[i] == 1)
+                arr.add(i);
+        maxChanges = Math.min(maxChanges, k);
         long ans = Long.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            ans = Math.min(ans, cntMove(cnt1, cnt2, presum1, presum2, nums, i, k, maxChanges));
-        }
+        int m = arr.size();
+        if (k - maxChanges <= m && maxChanges >= 0)
+            ans = Math.min(ans, helper(arr, k - maxChanges) + 2 * maxChanges);
+        if (k - maxChanges + 1 <= m && maxChanges >= 1)
+            ans = Math.min(ans, helper(arr, k - maxChanges + 1) + 2 * (maxChanges - 1));
+        if (k - maxChanges + 2 <= m && maxChanges >= 2)
+            ans = Math.min(ans, helper(arr, k - maxChanges + 2) + 2 * (maxChanges - 2));
+        if (k - maxChanges + 3 <= m && maxChanges >= 3)
+            ans = Math.min(ans, helper(arr, k - maxChanges + 3) + 2 * (maxChanges - 3));
+        
         return ans;
     }
-
-    long cntMove(int[] cnt1, int[] cnt2, long[] presum1, long[] presum2,  int[] nums, int i, int k, int maxChanges)   {
-        long cur = 0;
-        int n = nums.length;
-        int init = 0;
-        if (nums[i] == 1)   {
-            init++;
-            k--;
+    
+    long helper(List<Integer> nums, int cnt)   {
+        if (cnt == 0)
+            return 0;
+        long sum = 0;
+        for (int i = 0; i < cnt; i++)   {
+            sum += Math.abs(nums.get(i) - nums.get(cnt / 2));
         }
-        int consume = 0;
-        if (k > 0 && i > 0 && nums[i - 1] == 1)  {
-            cur++;
-            k--;
-            consume++;
+        long ans = sum;
+        for (int i = 0; i + cnt < nums.size(); i++) {
+            long d = nums.get(i + cnt / 2 + 1) - nums.get(i + cnt / 2);
+            sum = sum + d * (cnt / 2 + 1) - d * (cnt - (cnt / 2 + 1)) - Math.abs(nums.get(i + cnt / 2 + 1) - nums.get(i)) + Math.abs(nums.get(i + cnt / 2 + 1) - nums.get(i + cnt));
+            ans = Math.min(ans, sum);
         }
-        if (k > 0 && i < n - 1 && nums[i + 1] == 1)  {
-            cur++;
-            k--;
-            consume++;
-        }
-
-        if (k == 0)
-            return cur;
-
-        int usedChanges = Math.min(k, maxChanges);
-        k -= usedChanges;
-        cur += usedChanges * 2;
-
-        if (k == 0)
-            return cur;
-
-        int lo = 0;
-        int hi = Math.max(i + 1, n - i);
-
-        while (lo <= hi)    {
-            int mid = lo + (hi - lo) / 2;
-            if (check(cnt1, consume + init, i, mid, k))
-                hi = mid - 1;
-            else
-                lo = mid + 1;
-        }
-
-        int r = lo;
-
-        cur += checkLeft(presum1, cnt1, i - r + 1, i);
-        cur += checkRight(presum2, cnt2, i, i + r - 1);
-
-        return cur - consume;
+        
+        return ans;
     }
-
-    boolean check(int[] cnt, int consume, int i, int r, int k) {
-        return getCnt(cnt, i - r + 1, i + r - 1) - consume >= k;
-    }
-
-
-    int getCnt(int[] cnt, int lo, int hi)   {
-        if (hi >= cnt.length)
-            hi = cnt.length - 1;
-        if (lo < 0)
-            lo = 0;
-        return cnt[hi] - (lo == 0 ? 0 : cnt[lo - 1]);
-    }
-
-    long checkLeft(long[] presum1, int[] cnt1, int lo, int hi)  {
-        int n = presum1.length;
-        if (hi >= n)
-            hi = n - 1;
-        if (lo < 0)
-            lo = 0;
-        return presum1[hi] - (lo == 0 ? 0 : presum1[lo - 1]) - (lo == 0 ? 0 : (long)cnt1[lo - 1] * (hi - lo + 1));
-    }
-
-    long checkRight(long[] presum2, int[] cnt2, int lo, int hi)  {
-        int n = presum2.length;
-        if (hi >= n)
-            hi = n - 1;
-        if (lo < 0)
-            lo = 0;
-        return presum2[lo] - (hi == n - 1 ? 0 : presum2[hi + 1]) - (hi == n - 1 ? 0 : (long)cnt2[hi + 1] * (hi - lo + 1));
-    }
+    
 }
