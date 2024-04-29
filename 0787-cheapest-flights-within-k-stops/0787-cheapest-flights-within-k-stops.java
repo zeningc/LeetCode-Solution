deefab840fa95b8d136277ad4f9a45b9e13f3746
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-04-29 16:29:02 -0700

    Time: 5 ms (96.51%), Space: 44 MB (79.44%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,32 +1,16 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        Map<Integer, List<int[]>> map = new HashMap<>();
-        for (int[] f : flights)
-            map.computeIfAbsent(f[0], x -> new ArrayList<>()).add(new int[] {f[1], f[2]});
-        pq.offer(new int[] {src, 0, 0});
-        boolean[][] vis = new boolean[n][k + 3];
-        while (!pq.isEmpty())   {
-            int[] cur = pq.poll();
-            int u = cur[0];
-            int c = cur[1];
-            int step = cur[2];
-            if (vis[u][step])
-                continue;
-            vis[u][step] = true;
-            if (step >= k + 2)
-                continue;
-            if (u == dst)
-                return c;
-            if (!map.containsKey(u))
-                continue;
-            for (int[] nxt : map.get(u))    {
-                if (vis[nxt[0]][step + 1])
-                    continue;
-                pq.offer(new int[] {nxt[0], c + nxt[1], step + 1});
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE / 2);
+        dp[src] = 0;
+        for (int i = 0; i <= k; i++) {
+            int[] next = dp.clone();
+            for (int[] f : flights) {
+                next[f[1]] = Math.min(next[f[1]], dp[f[0]] + f[2]);
             }
+            dp = next;
         }
         
-        return -1;
+        return dp[dst] == Integer.MAX_VALUE / 2 ? -1 : dp[dst];
     }
 }
