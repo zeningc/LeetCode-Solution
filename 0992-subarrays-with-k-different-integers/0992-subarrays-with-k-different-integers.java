85ec2bccffaa3f3abd4352554e135d13f6bca182
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-05-23 18:36:52 -0500

    Time: 49 ms (16.74%), Space: 47.6 MB (24.01%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -3,21 +3,21 @@ class Solution {
         return helper(nums, k) - helper(nums, k - 1);
     }
     
-    int helper(int[] nums, int cnt) {
-        int n = nums.length;
-        int ans = 0;
-        int lo = 0;
+    int helper(int[] nums, int k)   {
+        int cnt = 0;
         Map<Integer, Integer> map = new HashMap<>();
-        for (int hi = 0; hi < n; hi++)  {
+        int lo = 0;
+        for (int hi = 0; hi < nums.length; hi++) {
             map.put(nums[hi], map.getOrDefault(nums[hi], 0) + 1);
-            while (map.size() > cnt)    {
+            while (map.size() > k)  {
                 map.put(nums[lo], map.get(nums[lo]) - 1);
                 if (map.get(nums[lo]) == 0)
                     map.remove(nums[lo]);
                 lo++;
             }
-            ans += hi - lo + 1;
+            cnt += hi - lo + 1;
         }
-        return ans;
+        
+        return cnt;
     }
 }
