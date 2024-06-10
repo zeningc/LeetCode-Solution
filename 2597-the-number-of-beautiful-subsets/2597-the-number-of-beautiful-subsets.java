c2499ebf5a0ebe91d3dae1b2d3cc631aef8860bd
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-06-10 16:08:17 -0500

    Time: 7 ms (90.13%), Space: 44 MB (90.98%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
@@ -1,39 +1,42 @@
 class Solution {
     public int beautifulSubsets(int[] nums, int k) {
-        Map<Integer, Integer> freq = new HashMap<>();
+        Map<Integer, Integer> f = new HashMap<>();
+        Map<Integer, List<int[]>> m = new HashMap<>();
         for (int num : nums)
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
-        Map<Integer, List<int[]>> modMap = new HashMap<>();
-        for (Map.Entry<Integer, Integer> e : freq.entrySet())   {
-            if (!modMap.containsKey(e.getKey() % k))
-                modMap.put(e.getKey() % k, new ArrayList<>());
-            modMap.get(e.getKey() % k).add(new int[] {e.getKey(), e.getValue()});
+            f.put(num, f.getOrDefault(num, 0) + 1);
+        for (Map.Entry<Integer, Integer> e : f.entrySet())  {
+            int key = e.getKey();
+            int val = e.getValue();
+            m.computeIfAbsent(key % k, x -> new ArrayList<>()).add(new int[] {key, val});
         }
-        int ret = 1;
         int[] twos = new int[21];
         twos[0] = 1;
         for (int i = 1; i <= 20; i++)
             twos[i] = twos[i - 1] * 2;
-        for (Map.Entry<Integer, List<int[]>> e : modMap.entrySet()) {
-            List<int[]> list = e.getValue();
-            Collections.sort(list, (a, b) -> a[0] - b[0]);
-            int take = 0;
-            int notTake = 1;
-            for (int i = 0; i < list.size(); i++)   {
-                int takeCopy = take;
-                int notTakeCopy = notTake;
-                if (i > 0 && list.get(i)[0] == list.get(i - 1)[0] + k)    {
-                    take = notTakeCopy * (int)(twos[list.get(i)[1]] - 1);
-                    
+        int ret = 1;
+        for (Map.Entry<Integer, List<int[]>> e : m.entrySet())    {
+            List<int[]> l = e.getValue();
+            Collections.sort(l, (a, b) -> a[0] - b[0]);
+            int preKey = -1;
+            int preTake = 0;
+            int preNotTake = 1;
+            for (int[] cur : l) {
+                int key = cur[0];
+                int val = cur[1];
+                int notTake = (preTake + preNotTake);
+                int take = 0;
+                if (key - k == preKey)  {
+                    take = preNotTake * (twos[val] - 1);
                 }
                 else    {
-                    take = (takeCopy + notTakeCopy) * (int)(twos[list.get(i)[1]] - 1);
+                    take = (preTake + preNotTake) * (twos[val] - 1);
                 }
-                notTake = (takeCopy + notTakeCopy);
+                preTake = take;
+                preNotTake = notTake;
+                preKey = key;
             }
-            ret *= (take + notTake);
+            ret *= (preTake +preNotTake);
         }
-        
         return ret - 1;
     }
 }
