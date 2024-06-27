5c53c96e297f3c3611700ffefb6c2d5e1ac4f7ce
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-06-27 18:53:30 -0500

    Time: 2 ms (99.27%), Space: 48 MB (34.46%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,12 +1,51 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int x1 = 0;
+        int x2 = 0;
+        for (int num : nums)    {
+            if (cnt1 > 0 && num == x1)  {
+                cnt1++;
+                continue;
+            }
+            
+            if (cnt2 > 0 && num == x2)  {
+                cnt2++;
+                continue;
+            }
+            
+            if (cnt1 == 0)  {
+                x1 = num;
+                cnt1++;
+                continue;
+            }
+            
+            if (cnt2 == 0)  {
+                x2 = num;
+                cnt2++;
+                continue;
+            }
+            
+            cnt1--;
+            cnt2--;
+        }
+        
+        int freq1 = 0;
+        int freq2 = 0;
+        for (int num : nums)    {
+            if (cnt1 > 0 && num == x1)
+                freq1++;
+            if (cnt2 > 0 && num == x2)
+                freq2++;
+        }
+        
         List<Integer> ans = new ArrayList<>();
-        Map<Integer, Integer> m = new HashMap<>();
-        for (int num : nums)
-            m.put(num, m.getOrDefault(num, 0) + 1);
-        for (Map.Entry<Integer, Integer> e : m.entrySet())
-            if (e.getValue() > nums.length / 3)
-                ans.add(e.getKey());
+        if (freq1 > nums.length / 3)
+            ans.add(x1);
+        if (freq2 > nums.length / 3)
+            ans.add(x2);
+        
         return ans;
     }
 }
