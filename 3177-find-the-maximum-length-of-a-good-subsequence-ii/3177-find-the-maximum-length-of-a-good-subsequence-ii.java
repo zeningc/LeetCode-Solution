9162a855dd397504b651346194456202fa28d125
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-07-24 19:39:46 -0500

    Time: 275 ms (22.60%), Space: 46.2 MB (85.16%) - LeetHub

--- 3177-find-the-maximum-length-of-a-good-subsequence-ii/3177-find-the-maximum-length-of-a-good-subsequence-ii.java
+++ 3177-find-the-maximum-length-of-a-good-subsequence-ii/3177-find-the-maximum-length-of-a-good-subsequence-ii.java
@@ -1,61 +1,54 @@
 class Solution {
     public int maximumLength(int[] nums, int k) {
         int n = nums.length;
-        int[][] dp = new int[k + 1][n];
-        
         int ans = 0;
-        int[][][] pre = new int[n + 1][2][2];
+        int[][] pre = new int[n + 1][4];
         for (int i = 0; i < n; i++) {
-            pre[i][0][0] = -1;
-            pre[i][0][1] = 0;
-            pre[i][1][0] = -1;
-            pre[i][1][1] = 0;
+            pre[i][0] = -1;
+            pre[i][1] = 0;
+            pre[i][2] = -1;
+            pre[i][3] = 0;
         }
         
         for (int x = 0; x <= k; x++)    {
-            int nxtFirstKey = -1;
-            int nxtFirstValue = 0;
-            int nxtSecondKey = -1;
-            int nxtSecondValue = 0;
-            Map<Integer, Integer> max = new HashMap<>();
-            int[][][] nxt = new int[n + 1][2][2];
-            nxt[0][0][0] = -1;
-            nxt[0][0][1] = 0;
-            nxt[0][1][0] = -1;
-            nxt[0][1][1] = 0;
+            Map<Integer, Integer> maxMap = new HashMap<>();
+            int[][] nxt = new int[n + 1][4];
+            nxt[0][0] = -1;
+            nxt[0][1] = 0;
+            nxt[0][2] = -1;
+            nxt[0][3] = 0;
             
             for (int y = 0; y < n; y++) {
-                
+                int cur = 0;
                 if (x != 0)  {
-                    if (pre[y][0][0] != nums[y])
-                        dp[x][y] = Math.max(dp[x][y], pre[y][0][1] + 1);
-                    else if (pre[y][1][0] != nums[y])
-                        dp[x][y] = Math.max(dp[x][y], pre[y][1][1] + 1);
-                    
+                    if (pre[y][0] != nums[y])
+                        cur = Math.max(cur, pre[y][1] + 1);
+                    else if (pre[y][2] != nums[y])
+                        cur = Math.max(cur, pre[y][3] + 1);
                 }
                 
-                dp[x][y] = Math.max(dp[x][y], max.getOrDefault(nums[y], 0) + 1);
+                cur = Math.max(cur, maxMap.getOrDefault(nums[y], 0) + 1);
                 
-                if (nxtFirstKey == -1 || nxtFirstKey != -1 && nxtFirstValue <= dp[x][y]) {
-                    if (nxtFirstKey != -1 && nxtFirstValue <= dp[x][y]) {
-                        nxtSecondKey = nxtFirstKey;
-                        nxtSecondValue = nxtFirstValue;
+                nxt[y + 1][0] = nxt[y][0];
+                nxt[y + 1][1] = nxt[y][1];
+                nxt[y + 1][2] = nxt[y][2];
+                nxt[y + 1][3] = nxt[y][3];
+                
+                if (nxt[y][0] == -1 || nxt[y][0] != -1 && nxt[y][1] <= cur) {
+                    if (nxt[y][0] != -1 && nxt[y][1] <= cur) {
+                        nxt[y + 1][2] = nxt[y][0];
+                        nxt[y + 1][2] = nxt[y][1];
                     }
-                    nxtFirstKey = nums[y];
-                    nxtFirstValue = dp[x][y];
+                    nxt[y + 1][0] = nums[y];
+                    nxt[y + 1][1] = cur;
                 }
-                else if (nxtSecondKey == -1 || nxtSecondKey != -1 && nxtSecondValue <= dp[x][y])   {
-                    nxtSecondKey = nums[y];
-                    nxtSecondValue = dp[x][y];
+                else if (nxt[y][2] == -1 || nxt[y][2] != -1 && nxt[y][3] <= cur)   {
+                    nxt[y + 1][2] = nums[y];
+                    nxt[y + 1][3] = cur;
                 }
                 
-                nxt[y + 1][0][0] = nxtFirstKey;
-                nxt[y + 1][0][1] = nxtFirstValue;
-                nxt[y + 1][1][0] = nxtSecondKey;
-                nxt[y + 1][1][1] = nxtSecondValue;
-                
-                max.put(nums[y], Math.max(max.getOrDefault(nums[y], 0), dp[x][y]));
-                ans = Math.max(ans, dp[x][y]);
+                maxMap.put(nums[y], Math.max(maxMap.getOrDefault(nums[y], 0), cur));
+                ans = Math.max(ans, cur);
             }
             
             pre = nxt;
