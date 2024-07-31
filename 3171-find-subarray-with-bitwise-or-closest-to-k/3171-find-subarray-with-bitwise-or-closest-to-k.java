f7ea026d48f61bbf036bf0c9d4a075464e7b30b5
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-07-31 18:54:51 -0500

    Time: 114 ms (79.21%), Space: 78.9 MB (14.44%) - LeetHub

--- 3171-find-subarray-with-bitwise-or-closest-to-k/3171-find-subarray-with-bitwise-or-closest-to-k.java
+++ 3171-find-subarray-with-bitwise-or-closest-to-k/3171-find-subarray-with-bitwise-or-closest-to-k.java
@@ -2,26 +2,26 @@ class Solution {
     public int minimumDifference(int[] nums, int k) {
         int n = nums.length;
         int[][] presum = new int[n + 1][32];
-        
-        for (int i = 1; i <= n; i++)
+        for (int i = 0; i < n; i++)
             for (int j = 0; j < 32; j++)
-                presum[i][j] = presum[i - 1][j] + ((nums[i - 1] & (1 << j)) == 0 ? 0 : 1);
-        
-        int res = 0;
+                presum[i + 1][j] = presum[i][j] + ((nums[i] & (1 << j)) == 0 ? 0 : 1);
         int lo = 0;
+        int cur = 0;
         int ans = Integer.MAX_VALUE;
-        for (int hi = 0; hi < n; hi++) {
-            res |= nums[hi];
-            while (res > k) {
-                ans = Math.min(ans, Math.abs(res - k));
+        for (int hi = 0; hi < nums.length; hi++)    {
+            cur |= nums[hi];
+            while (cur > k) {
+                ans = Math.min(ans, Math.abs(cur - k));
+                cur = 0;
                 lo++;
-                res = 0;
-                for (int j = 0; j < 32; j++)
-                    if (presum[hi + 1][j] - presum[lo][j] > 0)
-                        res |= (1 << j);
+                for (int i = 0; i < 32; i++)    {
+                    int cnt = presum[hi + 1][i] - presum[lo][i];
+                    if (cnt > 0)
+                        cur |= (1 << i);
+                }
             }
             if (lo <= hi)
-                ans = Math.min(ans, Math.abs(res - k));
+                ans = Math.min(ans, Math.abs(cur - k));
         }
         
         return ans;
