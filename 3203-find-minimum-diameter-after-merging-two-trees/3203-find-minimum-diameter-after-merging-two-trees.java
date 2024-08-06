dbb569e605525fd65172ccc8d46652f52fff84cc
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-08-05 20:40:49 -0500

    Time: 181 ms (38.78%), Space: 103.1 MB (92.87%) - LeetHub

--- 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.java
+++ 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.java
@@ -1,69 +1,45 @@
 class Solution {
     public int minimumDiameterAfterMerge(int[][] edges1, int[][] edges2) {
-        Map<Integer, List<Integer>> m1 = new HashMap<>();
-        Map<Integer, List<Integer>> m2 = new HashMap<>();
-        
-        int m = edges1.length + 1;
-        int n = edges2.length + 1;
-        for (int[] e : edges1)  {
-            m1.computeIfAbsent(e[0], x -> new ArrayList<>()).add(e[1]);
-            m1.computeIfAbsent(e[1], x -> new ArrayList<>()).add(e[0]);
-        }
-        
-        for (int[] e : edges2)  {
-            m2.computeIfAbsent(e[0], x -> new ArrayList<>()).add(e[1]);
-            m2.computeIfAbsent(e[1], x -> new ArrayList<>()).add(e[0]);
-        }
-        
-        
-        int d1 = bfs(m1);
-        int d2 = bfs(m2);
-        
-        return Math.max((d1 + 1) / 2 + (d2 + 1) / 2 + 1, Math.max(d1, d2));
+        int a = treeDiameter(edges1);
+        int b = treeDiameter(edges2);
+        return Math.max((a + 1) / 2 + (b + 1) / 2 + 1, Math.max(a, b));
     }
     
-    int bfs(Map<Integer, List<Integer>> m)  {
-        Deque<int[]> q = new LinkedList<>();
-        q.offer(new int[] {0, 0, -1});
-        int reach = 0;
-        int len = 0;
-        while (!q.isEmpty())    {
-            int[] cur = q.poll();
-            int u = cur[0];
-            int d = cur[1];
-            int p = cur[2];
-            if (d > len)    {
-                len = d;
-                reach = u;
-            }
-            
-            for (int v : m.getOrDefault(u, new ArrayList<>()))  {
-                if (v == p)
-                    continue;
-                q.offer(new int[] {v, d + 1, u});
-            }
+    public int treeDiameter(int[][] edges) {
+        int n = edges.length + 1;
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int[] edge : edges)   {
+            graph.computeIfAbsent(edge[0], x -> new ArrayList<>()).add(edge[1]);
+            graph.computeIfAbsent(edge[1], x -> new ArrayList<>()).add(edge[0]);
         }
         
-        len = 0;
-        q.offer(new int[] {reach, 0, -1});
-        
-        while (!q.isEmpty())    {
-            int[] cur = q.poll();
-            int u = cur[0];
-            int d = cur[1];
-            int p = cur[2];
-            if (d > len)    {
-                len = d;
-            }
-            
-            for (int v : m.getOrDefault(u, new ArrayList<>()))  {
-                if (v == p)
-                    continue;
-                q.offer(new int[] {v, d + 1, u});
-            }
-        }
-        
-        return len;
+        int[] first = bfs(graph, n, 0);
+        return bfs(graph, n, first[0])[1];
     }
     
+    int[] bfs(Map<Integer, List<Integer>> graph, int n, int start) {
+        Deque<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[n];
+        int lastNode = -1;
+        int dist = -1;
+        q.offer(start);
+        while (!q.isEmpty())    {
+            int size = q.size();
+            while (size-- > 0)  {
+                int u = q.poll();
+                if (vis[u])
+                    continue;
+                lastNode = u;
+                vis[u] = true;
+                for (int v : graph.getOrDefault(u, new ArrayList<>()))  {
+                    if (vis[v])
+                        continue;
+                    q.offer(v);
+                }
+            }
+            dist++;
+        }
+        
+        return new int[] {lastNode, dist};
+    }
 }
