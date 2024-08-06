06c6f626034b1b5e07013c0d8db85db7b9657229
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-08-05 20:31:09 -0500

    Time: 20 ms (35.60%), Space: 47.1 MB (45.31%) - LeetHub

--- 1245-tree-diameter/1245-tree-diameter.java
+++ 1245-tree-diameter/1245-tree-diameter.java
@@ -7,33 +7,23 @@ class Solution {
             graph.computeIfAbsent(edge[1], x -> new ArrayList<>()).add(edge[0]);
         }
         
+        int[] first = bfs(graph, n, 0);
+        return bfs(graph, n, first[0])[1];
+    }
+    
+    int[] bfs(Map<Integer, List<Integer>> graph, int n, int start) {
         Deque<Integer> q = new LinkedList<>();
         boolean[] vis = new boolean[n];
         int lastNode = -1;
-        q.offer(0);
-        while (!q.isEmpty())    {
-            int u = q.poll();
-            if (vis[u])
-                continue;
-            vis[u] = true;
-            lastNode = u;
-            for (int v : graph.getOrDefault(u, new ArrayList<>()))  {
-                if (vis[v])
-                    continue;
-                q.offer(v);
-            }
-        }
-        
-        q.clear();
-        q.offer(lastNode);
-        int dist = 0;
-        vis = new boolean[n];
+        int dist = -1;
+        q.offer(start);
         while (!q.isEmpty())    {
             int size = q.size();
             while (size-- > 0)  {
                 int u = q.poll();
                 if (vis[u])
                     continue;
+                lastNode = u;
                 vis[u] = true;
                 for (int v : graph.getOrDefault(u, new ArrayList<>()))  {
                     if (vis[v])
@@ -44,6 +34,6 @@ class Solution {
             dist++;
         }
         
-        return dist - 1;
+        return new int[] {lastNode, dist};
     }
 }
