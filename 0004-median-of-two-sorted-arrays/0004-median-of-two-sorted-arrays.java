c3b713d65af577fc3e11ece974551d8c5c89153d
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-08-05 21:58:47 -0500

    Time: 1 ms (100.00%), Space: 45.6 MB (92.99%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int size = nums1.length + nums2.length;
+        return (findKthSmallest(nums1, nums2, 0, 0, (size + 1) / 2) + findKthSmallest(nums1, nums2, 0, 0, (size + 2) / 2)) / 2;
+    }
+    
+    double findKthSmallest(int[] nums1, int[] nums2, int start1, int start2, int k)    {
+        if (nums1.length == start1)
+            return nums2[start2 + k - 1];
+        if (nums2.length == start2)
+            return nums1[start1 + k - 1];
+        if (k == 1)
+            return Math.min(nums1[start1], nums2[start2]);
+        int consume = Math.min(k / 2, Math.min(nums1.length - start1, nums2.length - start2));
+        if (nums1[start1 + consume - 1] < nums2[start2 + consume - 1])
+            return findKthSmallest(nums1, nums2, start1 + consume, start2, k - consume);
+        if (nums1[start1 + consume - 1] > nums2[start2 + consume - 1])
+            return findKthSmallest(nums1, nums2, start1, start2 + consume, k - consume);
+        return findKthSmallest(nums1, nums2, start1 + consume, start2, k - consume);
+    }
+}
