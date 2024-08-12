dbda8fadb17d34f49008b71f7058f1ca52ad82ca
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-08-11 23:24:11 -0500

    Time: 16 ms (96.60%), Space: 46.1 MB (27.21%) - LeetHub

--- 3076-shortest-uncommon-substring-in-an-array/3076-shortest-uncommon-substring-in-an-array.java
+++ 3076-shortest-uncommon-substring-in-an-array/3076-shortest-uncommon-substring-in-an-array.java
@@ -3,32 +3,20 @@ class Solution {
         Trie root = new Trie('#');
         String[] ans = new String[arr.length];
         for (String a : arr)    {
-            for (int i = 0; i < a.length(); i++)    {
-                Trie node = root;
-                for (int j = i; j < a.length(); j++)    {
-                    if (node.children[a.charAt(j) - 'a'] == null)
-                        node.children[a.charAt(j) - 'a'] = new Trie(a.charAt(j));
-                    node = node.children[a.charAt(j) - 'a'];
-                    node.cnt++;
-                }
-            }
+            updateWord(root, a, true);
         }
         
         for (int k = 0; k < arr.length; k++)    {
             String a = arr[k];
-            for (int i = 0; i < a.length(); i++)    {
-                Trie node = root;
-                for (int j = i; j < a.length(); j++)    {
-                    node = node.children[a.charAt(j) - 'a'];
-                    node.cnt--;
-                }
-            }
+            updateWord(root, a, false);
             
             String curAns = null;
             
             for (int i = 0; i < a.length(); i++)    {
                 Trie node = root;
                 for (int j = i; j < a.length(); j++)    {
+                    if (curAns != null && (j - i + 1) > curAns.length())
+                        break;
                     if (node.children[a.charAt(j) - 'a'] == null || node.children[a.charAt(j) - 'a'].cnt == 0)  {
                         String sub = a.substring(i, j + 1);
                         if (curAns == null || curAns.length() > (j - i + 1) || curAns.length() == (j - i + 1) && curAns.compareTo(sub) > 0)   {
@@ -40,21 +28,27 @@ class Solution {
                 }
             }
             
+            updateWord(root, a, true);
             
-            for (int i = 0; i < a.length(); i++)    {
-                Trie node = root;
-                for (int j = i; j < a.length(); j++)    {
-                    if (node.children[a.charAt(j) - 'a'] == null)
-                        node.children[a.charAt(j) - 'a'] = new Trie(a.charAt(j));
-                    node = node.children[a.charAt(j) - 'a'];
-                    node.cnt++;
-                }
-            }
             
             ans[k] = curAns == null ? "" : curAns;
         }
         
         return ans;
+    }
+    
+    void updateWord(Trie root, String s, boolean isAdd) {
+        for (int i = 0; i < s.length(); i++)    {
+            Trie node = root;
+            for (int j = i; j < s.length(); j++)    {
+                if (node.children[s.charAt(j) - 'a'] == null)
+                    node.children[s.charAt(j) - 'a'] = new Trie(s.charAt(j));
+                node = node.children[s.charAt(j) - 'a'];
+                if (isAdd)
+                    node.cnt++;
+                else node.cnt--;
+            }
+        }
     }
 }
 
