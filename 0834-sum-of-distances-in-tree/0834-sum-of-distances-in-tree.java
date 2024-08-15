ed880a79c32276b2e743fe88c40da52aa214c66f
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-08-15 17:32:32 -0500

    Time: 83 ms (15.87%), Space: 67.9 MB (36.30%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.java
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.java
@@ -1,62 +1,49 @@
 class Solution {
-    int[] nodeSum;
-    int[] nodeCnt;
+    int[] subCnt;
+    int[] subSum;
     int[] ans;
     int n;
     Map<Integer, List<Integer>> graph;
     public int[] sumOfDistancesInTree(int n, int[][] edges) {
-        if (n == 1)
-            return new int[] {0};
-        graph = new HashMap<>();
-        nodeSum = new int[n];
-        nodeCnt = new int[n];
+        subCnt = new int[n];
+        subSum = new int[n];
         ans = new int[n];
         this.n = n;
-        for (int[] edge : edges)
-        {
-            int u = edge[0];
-            int v = edge[1];
-            graph.computeIfAbsent(u, a -> new LinkedList<Integer>()).add(v);
-            graph.computeIfAbsent(v, a -> new LinkedList<Integer>()).add(u);
+        graph = new HashMap<>();
+        for (int[] e : edges)   {
+            graph.computeIfAbsent(e[0], x -> new ArrayList<>()).add(e[1]);
+            graph.computeIfAbsent(e[1], x -> new ArrayList<>()).add(e[0]);
         }
         
+        dfs(0, -1);
         dfs1(0, -1);
-        dfs2(0, -1);
         return ans;
     }
     
-    int[] dfs1(int u, int p)
-    {
-        int curSum = 0;
-        int curCnt = 0;
-        for (int v : graph.get(u))
-        {
+    int[] dfs(int u, int p) {
+        int[] sub = new int[2];
+        for (int v : graph.getOrDefault(u, new ArrayList<>()))  {
             if (v == p)
                 continue;
-            int[] summary = dfs1(v, u);
-            int cnt = summary[0];
-            int childSum = summary[1];
-            curCnt += cnt;
-            curSum += childSum + cnt;
+            int[] nxt = dfs(v, u);
+            sub[0] += nxt[0];
+            sub[1] += nxt[1] + nxt[0];
         }
-        nodeSum[u] = curSum;
-        nodeCnt[u] = curCnt + 1;
-        return new int[] {curCnt + 1, curSum};
+        sub[0]++;
+        subCnt[u] = sub[0];
+        subSum[u] = sub[1];
+        return sub;
     }
     
-    void dfs2(int u, int p)
-    {
+    void dfs1(int u, int p) {
         if (p != -1)
-            ans[u] = ans[p] - nodeCnt[u] + n - nodeCnt[u];
+            ans[u] = ans[p] - 2 * subCnt[u] + n;
         else
-            ans[u] = nodeSum[u];
-        
-        for (int v : graph.get(u))
-        {
+            ans[u] = subSum[u];
+        for (int v : graph.getOrDefault(u, new ArrayList<>()))  {
             if (v == p)
                 continue;
-            dfs2(v, u);
+            dfs1(v, u);
         }
-        
     }
 }
