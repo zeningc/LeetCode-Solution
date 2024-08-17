527f151f5aec82fbedec545257a1c0db49619569
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-08-17 18:53:43 -0500

    Time: 95 ms (68.42%), Space: 140.8 MB (63.16%) - LeetHub

--- 2581-count-number-of-possible-root-nodes/2581-count-number-of-possible-root-nodes.java
+++ 2581-count-number-of-possible-root-nodes/2581-count-number-of-possible-root-nodes.java
@@ -1,63 +1,65 @@
 class Solution {
-    Map<Integer, Set<Integer>> guessMap;
+    int n;
     Map<Integer, List<Integer>> graph;
-    int ans;
+    Map<Integer, Set<Integer>> grps;
     int k;
+    int ans;
+    int[] subOrder;
+    int[] fromRootReverse;
+    int[] fromRootOrder;
     public int rootCount(int[][] edges, int[][] guesses, int k) {
-        guessMap = new HashMap<>();
+        n = edges.length + 1;
         graph = new HashMap<>();
+        grps = new HashMap<>();
         ans = 0;
+        subOrder = new int[n];
+        fromRootReverse = new int[n];
+        fromRootOrder = new int[n];
         this.k = k;
-        for (int[] edge : edges)
-        {
+        for (int[] edge : edges)    {
             int u = edge[0];
             int v = edge[1];
-            graph.computeIfAbsent(u, x -> new LinkedList<>()).add(v);
-            graph.computeIfAbsent(v, x -> new LinkedList<>()).add(u);
+            graph.computeIfAbsent(u, x -> new ArrayList<>()).add(v);
+            graph.computeIfAbsent(v, x -> new ArrayList<>()).add(u);
         }
-        
-        for (int[] g : guesses)
-            guessMap.computeIfAbsent(g[1], x -> new HashSet<>()).add(g[0]);
-        
-        int cnt = dfs(0, -1);
-        dfs1(0, -1, cnt);
-        
+        for (int[] guess : guesses)    {
+            int u = guess[0];
+            int v = guess[1];
+            grps.computeIfAbsent(u, x -> new HashSet<>()).add(v);
+        }
+        dfs(0, -1, 0, 0);
+        dfs1(0, -1);
         return ans;
     }
     
-    int dfs(int u, int p)
-    {
-        int cnt = 0;
-        for (int v : graph.get(u))
-        {
-            if (p == v)
+    int dfs(int u, int p, int order, int rev) {
+        int ret = 0;
+        fromRootReverse[u] = rev;
+        fromRootOrder[u] = order;
+        for (int v : graph.getOrDefault(u, new ArrayList<>()))  {
+            if (v == p)
                 continue;
-            cnt += dfs(v, u);
-            if (guessMap.containsKey(v) && guessMap.get(v).contains(u))
-                cnt++;
+            int addOrder = 0;
+            int addReverse = 0;
+            if (grps.getOrDefault(u, new HashSet<>()).contains(v))  {
+                ret++;
+                addOrder = 1;
+            }
+            if (grps.getOrDefault(v, new HashSet<>()).contains(u))
+                addReverse = 1;
+            ret += dfs(v, u, order + addOrder, rev + addReverse);
         }
-        return cnt;
+        subOrder[u] = ret;
+        return ret;
     }
     
-    void dfs1(int u, int p, int cnt)
-    {
-        
-        if (cnt >= k)
-        {
+    void dfs1(int u, int p) {
+        if (p == -1 && subOrder[u] >= k || p != -1 && subOrder[0] - fromRootOrder[u] + fromRootReverse[u] >= k)
             ans++;
-        }
-        if (!graph.containsKey(u))
-            return;
-        for (int v : graph.get(u))
-        {
-            if (p == v)
+        for (int v : graph.getOrDefault(u, new ArrayList<>()))  {
+            if (v == p)
                 continue;
-            int temp = cnt;
-            if (guessMap.containsKey(v) && guessMap.get(v).contains(u))
-                temp--;
-            if (guessMap.containsKey(u) && guessMap.get(u).contains(v))
-                temp++;
-            dfs1(v, u, temp);
+            dfs1(v, u);
         }
     }
 }
