aaad27ad8d43f58f20cb2f7eb81b7b9c8e9df89f
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-08-26 20:15:51 -0500

    Time: 70 ms (53.00%), Space: 104.8 MB (29.00%) - LeetHub

--- 1847-closest-room/1847-closest-room.java
+++ 1847-closest-room/1847-closest-room.java
@@ -1,39 +1,41 @@
 class Solution {
     public int[] closestRoom(int[][] rooms, int[][] queries) {
-        int[][] qs = new int[queries.length][];
-        
-        for (int i = 0; i < queries.length; i++)
-            qs[i] = new int[] {queries[i][0], queries[i][1], i};
-        
-        Arrays.sort(qs, (a, b) -> b[1] - a[1]);
+        int n = rooms.length;
+        int m = queries.length;
+        List<Integer> list = new ArrayList<>();
+        List<Integer> queryIdx = new ArrayList<>();
+        for (int i = 0; i < m; i++)
+            queryIdx.add(i);
+        Collections.sort(queryIdx, (a, b) -> queries[b][1] - queries[a][1]);
         Arrays.sort(rooms, (a, b) -> b[1] - a[1]);
-        int p = 0;
-        int[] ans = new int[queries.length];
-        TreeSet<Integer> treeSet = new TreeSet<>();
-        for (int i = 0; i < qs.length; i++)    {
-            int minSize = qs[i][1];
-            while (p < rooms.length && rooms[p][1] >= minSize)  {
-                treeSet.add(rooms[p][0]);
-                p++;
+        int j = 0;
+        int[] ans = new int[m];
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int i : queryIdx)  {
+            while (j < rooms.length && rooms[j][1] >= queries[i][1])    {
+                set.add(rooms[j][0]);
+                j++;
             }
-            int idx = qs[i][2];
-            int preferId = qs[i][0];
-            if (treeSet.contains(preferId))  {
-                ans[idx] = preferId;
-                continue;
-            }
-            Integer lo = treeSet.lower(preferId);
-            Integer hi = treeSet.higher(preferId);
-            if (lo == null || hi == null)   {
-                
-                ans[idx] = lo == null ? (hi == null ? -1 : hi) : lo;
+            
+            if (set.isEmpty())  {
+                ans[i] = -1;
                 continue;
             }
             
-            if (hi - preferId < preferId - lo)
-                ans[idx] = hi;
-            else
-                ans[idx] = lo;
+            Integer floor = set.floor(queries[i][0]);
+            Integer ceiling = set.ceiling(queries[i][0]);
+            if (floor != null && ceiling != null)  {
+                if (Math.abs(floor - queries[i][0]) <= Math.abs(ceiling - queries[i][0]))
+                    ans[i] = floor;
+                else
+                    ans[i] = ceiling;
+                continue;
+            }
+            
+            if (floor == null)
+                ans[i] = ceiling;
+            else if (ceiling == null)
+                ans[i] = floor;
         }
         
         return ans;
