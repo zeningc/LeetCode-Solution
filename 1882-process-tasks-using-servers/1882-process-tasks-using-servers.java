5955711c100bbdceae78f508f50ab12fb541fd27
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-09-09 20:26:15 -0500

    Time: 629 ms (5.30%), Space: 114.4 MB (5.30%) - LeetHub

--- 1882-process-tasks-using-servers/1882-process-tasks-using-servers.java
+++ 1882-process-tasks-using-servers/1882-process-tasks-using-servers.java
@@ -1,27 +1,38 @@
 class Solution {
     public int[] assignTasks(int[] servers, int[] tasks) {
-        PriorityQueue<int[]> idle = new PriorityQueue<>((a, b) -> a[1] != b[1] ? a[1] - b[1] : a[0] - b[0]);
-        for (int i = 0; i < servers.length; i++)
-            idle.offer(new int[] {i, servers[i]});
-        
-        PriorityQueue<int[]> work = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        int time = 0;
-        int[] ans = new int[tasks.length];
-        for (int i = 0; i < tasks.length; i++)  {
-            time = Math.max(time, i);
-            if (idle.isEmpty() && work.peek()[1] > time)
-                time = work.peek()[1];
-            while (!work.isEmpty() && work.peek()[1] <= time)   {
-                int[] server = work.poll();
-                idle.offer(new int[] {server[0], servers[server[0]]});
+        int n = servers.length;
+        int m = tasks.length;
+        Deque<Integer> taskQueue = new LinkedList<>();
+        PriorityQueue<Integer> serverQueue = new PriorityQueue<>((a, b) -> servers[a] == servers[b] ? a - b : servers[a] - servers[b]);
+        for (int i = 0; i < n; i++)
+            serverQueue.offer(i);
+
+        TreeSet<Integer> timeSet = new TreeSet<>();
+        Map<Integer, List<Integer>> serverFreeTime = new HashMap<>();
+        for (int i = 0; i < m; i++)
+            timeSet.add(i);
+        int pre = -1;
+        int[] ans = new int[m];
+        while (timeSet.higher(pre) != null)   {
+            int t = timeSet.higher(pre);
+            if (t < m)
+                taskQueue.offer(t);
+            
+            for (int serverToFreeIdx : serverFreeTime.getOrDefault(t, new ArrayList<>()))
+                serverQueue.offer(serverToFreeIdx);
+            
+            while (!taskQueue.isEmpty() && !serverQueue.isEmpty())  {
+                int taskIdx = taskQueue.poll();
+                int serverIdx = serverQueue.poll();
+                ans[taskIdx] = serverIdx;
+                serverFreeTime.computeIfAbsent(t + tasks[taskIdx], x -> new ArrayList<>()).add(serverIdx);
+                if (!timeSet.contains(t + tasks[taskIdx]))  {
+                    timeSet.add(t + tasks[taskIdx]);
+                }
             }
-            
-            int[] server = idle.poll();
-            ans[i] = server[0];
-            
-            work.offer(new int[] {server[0], time + tasks[i]});
+            pre = t;
         }
-        
+    
         return ans;
     }
 }
