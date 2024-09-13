4981d0a66e046908ced232bbd2ba71c9408253e4
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-09-12 21:45:02 -0500

    Time: 78 ms (61.22%), Space: 111.6 MB (5.28%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.java
+++ 0815-bus-routes/0815-bus-routes.java
@@ -3,48 +3,38 @@ class Solution {
         if (source == target)
             return 0;
         Map<Integer, Set<Integer>> nodeToLine = new HashMap<>();
-        Set<Integer> sources = new HashSet<>();
-        Set<Integer> targets = new HashSet<>();
         for (int i = 0; i < routes.length; i++) {
             for (int j = 0; j < routes[i].length; j++)  {
-                if (routes[i][j] == source)
-                    sources.add(i);
-                if (routes[i][j] == target)
-                    targets.add(i);
                 nodeToLine.computeIfAbsent(routes[i][j], x -> new HashSet<>()).add(i);
             }
         }
         
         
-        Map<Integer, Set<Integer>> graph = new HashMap<>();
-        for (Set<Integer> value : nodeToLine.values())   {
-            List<Integer> valueList = new ArrayList<>(value);
-            for (int i = 0; i < valueList.size(); i++)  {
-                for (int j = i + 1; j < valueList.size(); j++)  {
-                    graph.computeIfAbsent(valueList.get(i), x -> new HashSet<>()).add(valueList.get(j));
-                    graph.computeIfAbsent(valueList.get(j), x -> new HashSet<>()).add(valueList.get(i));
-                }
-            }
-        }
+        
 
         Deque<Integer> q = new LinkedList<>();
-        for (int s : sources)
-            q.offer(s);
-        int busCnt = 1;
-        Set<Integer> vis = new HashSet<>();
+        q.offer(source);
+        int busCnt = 0;
+        Set<Integer> visRoute = new HashSet<>();
+        Set<Integer> visStop = new HashSet<>();
         while (!q.isEmpty())    {
             int size = q.size();
             while (size-- > 0)  {
                 int u = q.poll();
-                if (vis.contains(u))
+                if (visStop.contains(u))
                     continue;
-                vis.add(u);
-                if (targets.contains(u))
+                visStop.add(u);
+                if (target == u)
                     return busCnt;
-                for (int v : graph.getOrDefault(u, new HashSet<>()))    {
-                    if (vis.contains(v))
+                for (int r : nodeToLine.getOrDefault(u, new HashSet<>()))    {
+                    if (visRoute.contains(r))
                         continue;
-                    q.offer(v);
+                    visRoute.add(r);
+                    for (int v : routes[r]) {
+                        if (visStop.contains(v))
+                            continue;
+                        q.offer(v);
+                    }
                 }
             }
             busCnt++;
