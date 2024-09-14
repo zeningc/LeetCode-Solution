3627c255f2546997cd2490ab522f37daa9bb23b1
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-09-14 17:06:23 -0500

    Time: 5 ms (52.07%), Space: 42.7 MB (10.69%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.java
+++ 0269-alien-dictionary/0269-alien-dictionary.java
@@ -1,68 +1,50 @@
 class Solution {
     public String alienOrder(String[] words) {
-        Map<Character, Set<Character>> edges;
-        Map<Character, Integer> inDegree;
-        int n = words.length;
-        String prev = words[0];
-        edges = new HashMap<>();
-        inDegree = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
         for (String word : words)   {
             for (char c : word.toCharArray())   {
-                if (!inDegree.containsKey(c))   {
-                    inDegree.put(c, 0);
-                }
+                set.add(c - 'a');
             }
         }
-        for (int i = 1; i < n; i++) {
-            if (prev.length() > words[i].length() && prev.substring(0, words[i].length()).equals(words[i])) {
-                return "";
-            }
-            for (int j = 0; j < Math.min(prev.length(), words[i].length()); j++) {
-                if (prev.charAt(j) == words[i].charAt(j))   {
+        
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        Map<Integer, Set<Integer>> in = new HashMap<>();
+        for (int i = 1; i < words.length; i++)  {
+            boolean flag = false;
+            for (int k = 0; k < Math.min(words[i].length(), words[i - 1].length()); k++) {
+                int a = words[i - 1].charAt(k) - 'a';
+                int b = words[i].charAt(k) - 'a';
+                if (a == b)
                     continue;
-                }
-                char u = prev.charAt(j);
-                char v = words[i].charAt(j);
-                if (!edges.containsKey(u))   {
-                    edges.put(u, new HashSet<>());
-                }
-                if (edges.get(u).contains(v))   {
-                    break;
-                }
-                edges.get(u).add(v);
-                inDegree.put(v, inDegree.get(v) + 1);
+                    
+                in.computeIfAbsent(b, x -> new HashSet<>()).add(a);
+                graph.computeIfAbsent(a, x -> new HashSet<>()).add(b);
+                flag = true;
                 break;
             }
-            prev = words[i];
+            if (!flag && words[i - 1].length() > words[i].length())
+                return "";
         }
         
-        StringBuilder ans = new StringBuilder();
-        Deque<Character> queue = new LinkedList<>();
-        for (char c : inDegree.keySet())    {
-            if (inDegree.get(c) == 0)   {
-                queue.offer(c);
+        int[] inDegree = new int[26];
+        for (int i : set)
+            inDegree[i] = in.getOrDefault(i, new HashSet<>()).size();
+        Deque<Integer> q = new LinkedList<>();
+        for (int i : set)
+            if (inDegree[i] == 0)
+                q.offer(i);
+        StringBuilder sb = new StringBuilder();
+        while (!q.isEmpty())    {
+            int u = q.poll();
+            
+            sb.append((char)(u + 'a'));
+            for (int v : graph.getOrDefault(u, new HashSet<>()))    {
+                inDegree[v]--;
+                if (inDegree[v] == 0)
+                    q.offer(v);
             }
         }
         
-        while (!queue.isEmpty())    {
-            char c = queue.poll();
-            ans.append(c);
-            if (edges.containsKey(c))   {
-                for (char v : edges.get(c)) {
-                    inDegree.put(v, inDegree.get(v) - 1);
-                    if (inDegree.get(v) == 0)   {
-                        queue.offer(v);
-                    }
-                }
-            }
-        }
-        
-        if (ans.length() == inDegree.size())    {
-            return ans.toString();
-        }
-        return "";
+        return sb.length() == set.size() ? sb.toString() : "";
     }
-    
-    
 }
-
