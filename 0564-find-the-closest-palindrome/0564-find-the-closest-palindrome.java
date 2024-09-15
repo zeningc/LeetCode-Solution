871d18c64e3525e9f5f879540b4e11d84f0c39ff
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-09-15 12:14:48 -0500

    Time: 3 ms (32.08%), Space: 41.8 MB (63.96%) - LeetHub

--- 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.java
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.java
@@ -1,75 +1,82 @@
 class Solution {
-    public String nearestPalindromic(String n) {
-        long nNum = Long.valueOf(n);
-        if (nNum < 10)
-            return String.valueOf(nNum - 1);
-        int posToFlip = n.length() % 2 == 0 ? n.length() / 2 - 1 : n.length() / 2;
-        String toFlipStr = n.substring(0, posToFlip + 1);
-        long toFlipNum = Long.valueOf(toFlipStr);
-        String a = getFlipNum(n, toFlipStr, toFlipNum, posToFlip, -1);
-        String b = getFlipNum(n, toFlipStr, toFlipNum, posToFlip, 1);
-        String c = getFlipNum(n, toFlipStr, toFlipNum, posToFlip, 0);
+    public String nearestPalindromic(String input) {
+        long inputNumber = Long.valueOf(input);
+        if (inputNumber < 10)
+            return String.valueOf(inputNumber - 1);
         
-        return getAns(n, a, getAns(n, b, c));
+        int midIndex = input.length() % 2 == 0 ? input.length() / 2 - 1 : input.length() / 2;
+        String leftHalfStr = input.substring(0, midIndex + 1);
+        long leftHalfNum = Long.valueOf(leftHalfStr);
+        
+        String smallerPalindrome = generatePalindrome(input, leftHalfStr, leftHalfNum, midIndex, -1);
+        String largerPalindrome = generatePalindrome(input, leftHalfStr, leftHalfNum, midIndex, 1);
+        String exactPalindrome = generatePalindrome(input, leftHalfStr, leftHalfNum, midIndex, 0);
+        
+        return findClosestPalindrome(input, smallerPalindrome, findClosestPalindrome(input, largerPalindrome, exactPalindrome));
     }
     
-    String getFlipNum(String n, String toFlipStr, long toFlipNum, int posToFlip, int delta) {
-        String a = null;
-        if (delta == -1 && isPowerOfTen(toFlipStr))    {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < n.length() - 1; i++)
-                sb.append('9');
-            return sb.toString();
+    String generatePalindrome(String input, String leftHalfStr, long leftHalfNum, int midIndex, int adjustment) {
+        if (adjustment == -1 && isPowerOfTen(leftHalfStr)) {
+            StringBuilder ninePalindrome = new StringBuilder();
+            for (int i = 0; i < input.length() - 1; i++)
+                ninePalindrome.append('9');
+            return ninePalindrome.toString();
         }
-        if (delta == 1 && isPowerOfTenMinusOne(toFlipStr)) {
-            StringBuilder sb = new StringBuilder();
-            sb.append('1');
-            for (int i = 0; i < n.length() - 1; i++)
-                sb.append('0');
-            sb.append('1');
-            return sb.toString();
+        
+        if (adjustment == 1 && isPowerOfTenMinusOne(leftHalfStr)) {
+            StringBuilder tenPalindrome = new StringBuilder();
+            tenPalindrome.append('1');
+            for (int i = 0; i < input.length() - 1; i++)
+                tenPalindrome.append('0');
+            tenPalindrome.append('1');
+            return tenPalindrome.toString();
         }
-        toFlipNum += delta;
-        String modifiedToFlipStr = String.valueOf(toFlipNum);
-        return flip(modifiedToFlipStr, n.length() % 2 == 0 ? posToFlip : posToFlip - 1);
+        
+        leftHalfNum += adjustment;
+        String modifiedLeftHalfStr = String.valueOf(leftHalfNum);
+        return createFullPalindrome(modifiedLeftHalfStr, input.length() % 2 == 0 ? midIndex : midIndex - 1);
     }
     
-    boolean isPowerOfTen(String toFlipStr) {
-        if (toFlipStr.charAt(0) != '1')
+    boolean isPowerOfTen(String leftHalfStr) {
+        if (leftHalfStr.charAt(0) != '1')
             return false;
-        for (int i = 1; i < toFlipStr.length(); i++)
-            if (toFlipStr.charAt(i) != '0')
+        for (int i = 1; i < leftHalfStr.length(); i++)
+            if (leftHalfStr.charAt(i) != '0')
                 return false;
         return true;
     }
     
-    boolean isPowerOfTenMinusOne(String toFlipStr)  {
-        for (char c : toFlipStr.toCharArray())
+    boolean isPowerOfTenMinusOne(String leftHalfStr) {
+        for (char c : leftHalfStr.toCharArray())
             if (c != '9')
                 return false;
         return true;
     }
     
-    String getAns(String n, String a, String b) {
-        long nNum = Long.valueOf(n);
-        long aNum = Long.valueOf(a);
-        long bNum = Long.valueOf(b);
-        if (nNum == aNum)
-            return b;
-        if (nNum == bNum)
-            return a;
-        if (Math.abs(nNum - aNum) < Math.abs(nNum - bNum))
-            return a;
-        if (Math.abs(nNum - aNum) > Math.abs(nNum - bNum))
-            return b;
-        if (aNum < bNum)
-            return a;
-        return b;
+    String findClosestPalindrome(String input, String candidate1, String candidate2) {
+        long inputNumber = Long.valueOf(input);
+        long candidate1Number = Long.valueOf(candidate1);
+        long candidate2Number = Long.valueOf(candidate2);
+        
+        if (inputNumber == candidate1Number)
+            return candidate2;
+        if (inputNumber == candidate2Number)
+            return candidate1;
+        
+        long diff1 = Math.abs(inputNumber - candidate1Number);
+        long diff2 = Math.abs(inputNumber - candidate2Number);
+        
+        if (diff1 < diff2)
+            return candidate1;
+        if (diff2 < diff1)
+            return candidate2;
+        
+        return candidate1Number < candidate2Number ? candidate1 : candidate2;
     }
     
-    String flip(String numStr, int pos)    {
-        StringBuilder partToReverse = new StringBuilder(numStr.substring(0, pos + 1));
-        partToReverse.reverse();
-        return numStr + partToReverse.toString();
+    String createFullPalindrome(String leftHalfStr, int midIndex) {
+        StringBuilder reversedPart = new StringBuilder(leftHalfStr.substring(0, midIndex + 1));
+        reversedPart.reverse();
+        return leftHalfStr + reversedPart.toString();
     }
 }
