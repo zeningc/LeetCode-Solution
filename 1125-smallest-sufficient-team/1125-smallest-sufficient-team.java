50ff5e707e33e6f23e92d1247e25a8172f57cbca
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-09-15 21:27:40 -0500

    Time: 251 ms (14.74%), Space: 43.1 MB (76.84%) - LeetHub

---
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] smallestSufficientTeam(String[] req_skills, List<List<String>> people) {
+        int skillCnt = 0;
+        Map<String, Integer> skillIdMap = new HashMap<>();
+        for (String skill : req_skills)
+            skillIdMap.put(skill, skillCnt++);
+        int requireSkillBit = (1 << skillCnt) - 1;
+        int[] peopleSkillBit = new int[people.size()];
+        for (int i = 0; i < people.size(); i++)  {
+            List<String> skills = people.get(i);
+            int skillBit = 0;
+            for (String skill : skills)
+                skillBit |= (1 << skillIdMap.get(skill));
+            peopleSkillBit[i] = skillBit;
+        }
+        long[] dp = new long[1 << skillCnt];
+        Arrays.fill(dp, Integer.MAX_VALUE / 2);
+        dp[0] = 0;
+        for (int i = 0; i < (1 << skillCnt); i++)   {
+            for (int j = 0; j < people.size(); j++) {
+                if (cnt(dp[i]) + 1 < cnt(dp[i | peopleSkillBit[j]]))
+                    dp[i | peopleSkillBit[j]] = dp[i] | (1L << j);
+            }
+        }
+        List<Integer> ansList = new ArrayList<>();
+        for (int i = 0; i < people.size(); i++)
+            if ((dp[(1 << skillCnt) - 1] & (1L << i)) != 0)
+                ansList.add(i);
+        
+        int[] ans = new int[ansList.size()];
+        for (int i = 0; i < ansList.size(); i++)
+            ans[i] = ansList.get(i);
+        
+        return ans;
+    }
+    
+    int cnt(long bit)    {
+        int cnt = 0;
+        while (bit != 0)    {
+            bit &= (bit - 1);
+            cnt++;
+        }
+        return cnt;
+    }
+}
