8242491359c9340fa2195a302441ea1d49b873ed
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-09-21 23:11:31 -0500

    Time: 6 ms (41.66%), Space: 42.3 MB (50.07%) - LeetHub

--- 0772-basic-calculator-iii/0772-basic-calculator-iii.java
+++ 0772-basic-calculator-iii/0772-basic-calculator-iii.java
@@ -9,10 +9,11 @@ class Solution {
         Deque<Character> ops = new LinkedList<>();
         Deque<Integer> nums = new LinkedList<>();
         nums.push(0);
-        s = s.replace(" ", "");
         
         for (int i = 0; i < s.length(); i++)    {
             char c = s.charAt(i);
+            if (c == ' ')
+                continue;
             if (c == '(')   {
                 ops.push('(');
                 continue;
