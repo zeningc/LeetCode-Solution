cee7fd63f148de2d15dd6e5ec7a61bff38012897
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-09-21 19:22:48 -0500

    Time: 2051 ms (7.14%), Space: 40.9 MB (70.17%) - LeetHub

--- 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.java
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.java
@@ -1,33 +1,26 @@
 class Solution {
     public int minimumTimeRequired(int[] jobs, int k) {
         int n = jobs.length;
-        int[] time = new int[1 << n];
-        
-        for (int i = 0; i < (1 << n); i++)  {
-            int sum = 0;
-            for (int j = 0; j < n; j++) {
-                if ((i & (1 << j)) != 0)
-                    sum += jobs[j];
-            }
-            time[i] = sum;
-        }
-        
-        int[][] dp = new int[k][1 << n];
-        
-        for (int i = 0; i < (1 << n); i++)
-            dp[0][i] = time[i];
-        
-        for (int i = 1; i < k; i++) {
-            for (int state = 0; state < (1 << n); state++)  {
-                dp[i][state] = Integer.MAX_VALUE;
-                for (int subset = state; subset >= 0; subset = (subset - 1) & state) {
-                    dp[i][state] = Math.min(dp[i][state], Math.max(dp[i - 1][state - subset], time[subset]));
-                    if (subset == 0)
-                        break;
+        int[] dp = new int[1 << n];
+        Arrays.fill(dp, Integer.MAX_VALUE / 2);
+        dp[0] = 0;
+        for (int i = 0; i < k; i++) {
+            for (int state = (1 << n) - 1; state >= 0; state--) {
+                for (int subState = state; subState > 0; subState = (subState - 1) & state)  {
+                    int curWorkerState = subState;
+                    int preWorkerState = state - curWorkerState;
+                    if (dp[preWorkerState] >= Integer.MAX_VALUE / 2)
+                        continue;
+                    int curAssignment = 0;
+                    for (int j = 0; j < n; j++)
+                        if ((curWorkerState & (1 << j)) != 0)
+                            curAssignment += jobs[j];
+                    dp[state] = Math.min(dp[state], Math.max(dp[preWorkerState], curAssignment));
+                    if (subState == 0) break;
                 }
             }
         }
         
-        return dp[k - 1][(1 << n) - 1];
+        return dp[(1 << n) - 1];
     }
 }
