6e3cf4ba3b5569acd4910b085ca8d0a9150299ee
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-09-30 23:22:46 -0500

    Time: 1 ms (98.71%), Space: 42.1 MB (63.61%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,24 +1,37 @@
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        Set<String> nodes = new HashSet<>();
-        for (List<String> equation : equations)
-            for (String node : equation)
-                nodes.add(node);
-        UnionFind uf = new UnionFind(nodes);
-        for (int i = 0; i < values.length; i++) {
-            String x = equations.get(i).get(0);
-            String y = equations.get(i).get(1);
-            double w = values[i];
-            uf.union(x, y, w);
+        Map<String, Integer> strToId = new HashMap<>();
+        int id = 0;
+        for (List<String> equation : equations) {
+            if (!strToId.containsKey(equation.get(0)));
+                strToId.put(equation.get(0), id++);
+            if (!strToId.containsKey(equation.get(1)));
+                strToId.put(equation.get(1), id++);
+        }
+        
+        UnionFind uf = new UnionFind(id);
+        int n = equations.size();
+        for (int i = 0; i < n; i++) {
+            List<String> equation = equations.get(i);
+            String x = equation.get(0);
+            String y = equation.get(1);
+            
+            uf.union(strToId.get(x), strToId.get(y), values[i]);
         }
         
         
-        double[] ans = new double[queries.size()];
-        
-        for (int i = 0; i < queries.size(); i++)    {
-            String x = queries.get(i).get(0);
-            String y = queries.get(i).get(1);
-            ans[i] = !uf.isConnected(x, y) ? -1 : uf.query(x, y);
+        int m = queries.size();
+        double[] ans = new double[m];
+        for (int i = 0; i < m; i++) {
+            List<String> query = queries.get(i);
+            String x = query.get(0);
+            String y = query.get(1);
+            if (!strToId.containsKey(x) || !strToId.containsKey(y)) {
+                ans[i] = -1.0;
+                continue;
+            }
+            
+            ans[i] = uf.query(strToId.get(x), strToId.get(y));
         }
         
         return ans;
@@ -26,45 +39,51 @@ class Solution {
 }
 
 class UnionFind {
-    Map<String, String> parents;
-    Map<String, Double> weights;
     
-    public UnionFind(Set<String> nodes)  {
-        parents = new HashMap<>();
-        weights = new HashMap<>();
-        for (String node : nodes)   {
-            parents.put(node, node);
-            weights.put(node, 1.0);
+    int[] parents;
+    double[] weights;
+    
+    
+    public UnionFind(int n) {
+        parents = new int[n];
+        weights = new double[n];
+        
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
+            weights[i] = 1.0;
         }
     }
     
-    public double query(String x, String y)    {
-        return weights.get(x) / weights.get(y);
-    }
-    
-    public String find(String x)  {
-        if (!parents.get(x).equals(x))    {
-            String root = find(parents.get(x));
-            weights.put(x, weights.get(x) * weights.get(parents.get(x)));
-            parents.put(x, root);
-            
+    int find(int x) {
+        if (parents[x] != x)    {
+            int t = parents[x];
+            parents[x] = find(parents[x]);
+            weights[x] *= weights[t];
         }
-        return parents.get(x);
+        
+        return parents[x];
     }
     
-    public boolean isConnected(String x, String y)  {
-        return parents.containsKey(x) && parents.containsKey(y) && find(x).equals(find(y));
+    boolean isConnected(int x, int y)   {
+        return find(x) == find(y);
     }
     
-    // x / y = value
-    public void union(String x, String y, double value)    {
-        String rootX = find(x);
-        String rootY = find(y);
-        if (rootX.equals(rootY))
+    
+    void union(int x, int y, double ratio)    {
+        int xRoot = find(x);
+        int yRoot = find(y);
+        
+        if (xRoot == yRoot)
             return;
         
-        parents.put(rootX, rootY);
-        weights.put(rootX, value * weights.get(y) / weights.get(x));
+        parents[xRoot] = parents[yRoot];
+        weights[xRoot] = ratio * weights[y] / weights[x];
     }
     
+    double query(int x, int y)  {
+        if (!isConnected(x, y))
+            return -1.0;
+        
+        return weights[x] / weights[y];
+    }
 }
