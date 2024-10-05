b1280ec955eb26eaf5c050ac69a78613d7ac7696
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-05 15:17:08 -0500

    Time: 2 ms (31.37%), Space: 41.9 MB (89.07%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,89 +1,56 @@
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        Map<String, Integer> strToId = new HashMap<>();
-        int id = 0;
-        for (List<String> equation : equations) {
-            if (!strToId.containsKey(equation.get(0)));
-                strToId.put(equation.get(0), id++);
-            if (!strToId.containsKey(equation.get(1)));
-                strToId.put(equation.get(1), id++);
-        }
-        
-        UnionFind uf = new UnionFind(id);
-        int n = equations.size();
-        for (int i = 0; i < n; i++) {
+        Map<String, List<Pair<String, Double>>> graph = new HashMap<>();
+        for (int i = 0; i < equations.size(); i++)  {
             List<String> equation = equations.get(i);
-            String x = equation.get(0);
-            String y = equation.get(1);
-            
-            uf.union(strToId.get(x), strToId.get(y), values[i]);
+            double ratio = values[i];
+            String a = equation.get(0);
+            String b = equation.get(1);
+            graph.computeIfAbsent(a, x -> new ArrayList<>()).add(new Pair<String, Double>(b, ratio));
+            graph.computeIfAbsent(b, x -> new ArrayList<>()).add(new Pair<String, Double>(a, (double)1.0 / ratio));
         }
         
-        
-        int m = queries.size();
-        double[] ans = new double[m];
-        for (int i = 0; i < m; i++) {
+        double[] ans = new double[queries.size()];
+        for (int i = 0; i < queries.size(); i++)    {
             List<String> query = queries.get(i);
-            String x = query.get(0);
-            String y = query.get(1);
-            if (!strToId.containsKey(x) || !strToId.containsKey(y)) {
+            String source = query.get(0);
+            String dest = query.get(1);
+            if (!graph.containsKey(source) || !graph.containsKey(dest)) {
                 ans[i] = -1.0;
                 continue;
             }
-            
-            ans[i] = uf.query(strToId.get(x), strToId.get(y));
+            Set<String> vis = new HashSet<>();
+            Deque<Pair<String, Double>> q = new LinkedList<>();
+            q.offer(new Pair<String, Double>(source, (double)1.0));
+            while (!q.isEmpty())    {
+                Pair<String, Double> cur = q.poll();
+                String u = cur.getKey();
+                
+                if (vis.contains(u))
+                    continue;
+                
+                vis.add(u);
+                double w = cur.getValue();
+                
+                if (u.equals(dest))  {
+                    ans[i] = w;
+                    break;
+                }
+                
+                for (Pair<String, Double> nxt : graph.getOrDefault(u, new ArrayList<>()))   {
+                    String v = nxt.getKey();
+                    if (vis.contains(v))
+                        continue;
+                    
+                    double nxtW = nxt.getValue();
+                    q.offer(new Pair<String, Double>(v, w * nxtW));
+                }
+            }
+            if (!vis.contains(dest))
+                ans[i] = -1.0;
         }
+        
         
         return ans;
-    }
-}
-
-class UnionFind {
-    
-    int[] parents;
-    double[] weights;
-    
-    
-    public UnionFind(int n) {
-        parents = new int[n];
-        weights = new double[n];
-        
-        for (int i = 0; i < n; i++) {
-            parents[i] = i;
-            weights[i] = 1.0;
-        }
-    }
-    
-    int find(int x) {
-        if (parents[x] != x)    {
-            int t = parents[x];
-            parents[x] = find(parents[x]);
-            weights[x] *= weights[t];
-        }
-        
-        return parents[x];
-    }
-    
-    boolean isConnected(int x, int y)   {
-        return find(x) == find(y);
-    }
-    
-    
-    void union(int x, int y, double ratio)    {
-        int xRoot = find(x);
-        int yRoot = find(y);
-        
-        if (xRoot == yRoot)
-            return;
-        
-        parents[xRoot] = parents[yRoot];
-        weights[xRoot] = ratio * weights[y] / weights[x];
-    }
-    
-    double query(int x, int y)  {
-        if (!isConnected(x, y))
-            return -1.0;
-        
-        return weights[x] / weights[y];
     }
 }
