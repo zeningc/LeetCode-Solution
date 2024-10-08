8f4e194002b7cf346a8841bce0f002aa49279ecb
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-07 22:31:55 -0500

    Time: 13 ms (41.34%), Space: 44.2 MB (74.17%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -1,28 +1,40 @@
 class Solution {
     public int swimInWater(int[][] grid) {
-        int n = grid.length;
+        int m = grid.length;
+        int n = grid[0].length;
+        
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        int[][] dir = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        
+        List<int[]> minPoints = new ArrayList<>();
+        int minHeight = Integer.MAX_VALUE;
+        
         pq.offer(new int[] {0, 0, grid[0][0]});
-        int[][] dir = new int[][] {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        boolean[][] vis = new boolean[n][n];
+        boolean[][] vis = new boolean[m][n];
         while (!pq.isEmpty())   {
-            int[] poll = pq.poll();
-            int x = poll[0];
-            int y = poll[1];
-            int h = poll[2];
+            int[] cur = pq.poll();
+            int x = cur[0];
+            int y = cur[1];
+            
             if (vis[x][y])
                 continue;
+            
             vis[x][y] = true;
-            if (x == n - 1 && y == n - 1)
-                return h;
+            int v = cur[2];
+            if (x == m - 1 && y == n - 1)
+                return v;
             for (int[] d : dir) {
-                int nx = d[0] + x;
-                int ny = d[1] + y;
-                if (nx < 0 || nx > n - 1 || ny < 0 || ny > n - 1 || vis[nx][ny])
+                int nx = x + d[0];
+                int ny = y + d[1];
+                
+                if (nx < 0 || nx > m - 1 || ny < 0 || ny > n - 1 || vis[nx][ny])
                     continue;
-                pq.offer(new int[] {nx, ny, Math.max(h, grid[nx][ny])});
+                
+                pq.offer(new int[] {nx, ny, Math.max(v, grid[nx][ny])});
             }
+        
         }
+        
         return -1;
     }
 }
