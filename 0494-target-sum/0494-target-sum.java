66b787c4c59885d08ad649c1536e6f12334a8181
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-10 19:43:00 -0500

    Time: 16 ms (55.35%), Space: 45 MB (21.76%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,23 +1,25 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
+        int OFFSET = 1000;
         int n = nums.length;
-        int sum = 0;
-        for (int num : nums)
-            sum += num;
-        int offset = Math.max(sum, Math.abs(target));
-        int[][] dp = new int[n + 1][2 * offset + 1];
-        dp[0][offset] = 1;
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 0; j <= 2 * offset; j++)
-            {
-                if (j - nums[i - 1] >= 0)
-                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
-                if (j + nums[i - 1] <= 2 * offset)
-                    dp[i][j] += dp[i - 1][j + nums[i - 1]];
+        int[][] dp = new int[n][2 * OFFSET + 1];
+        dp[0][nums[0] + OFFSET] += 1;
+        dp[0][-nums[0] + OFFSET] += 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= 2 * OFFSET; j++)   {
+                if (j - nums[i] >= 0)
+                    dp[i][j] += dp[i - 1][j - nums[i]];
+                if (j + nums[i] <= 2 * OFFSET)
+                    dp[i][j] += dp[i - 1][j + nums[i]];
             }
         }
         
-        return dp[n][target + offset];
+        return dp[n - 1][target + OFFSET];
     }
 }
+
+/*
+
+dp[i][j] ith jit
+
+*/
