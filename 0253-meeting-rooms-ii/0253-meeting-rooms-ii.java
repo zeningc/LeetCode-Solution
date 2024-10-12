31fe482df968790bf551d2109efef91e3f9edcbe
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-12 12:02:32 -0500

    Time: 3 ms (99.86%), Space: 45.7 MB (87.74%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,24 +1,26 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        int ans=0;
-        int n=intervals.length;
-        int[] start=new int[n];
-        int[] end=new int[n];
-        for(int i=0;i<n;i++)    {
-            start[i]=intervals[i][0];
-            end[i]=intervals[i][1];
+        int ans = 0;
+        int n = intervals.length;
+        int[] startTime = new int[n];
+        int[] endTime = new int[n];
+    
+        for (int i = 0; i < n; i++) {
+            startTime[i] = intervals[i][0];
+            endTime[i] = intervals[i][1];
         }
-        Arrays.sort(start);
-        Arrays.sort(end);
-        int endPointer=0;
-        for(int startPointer=0;startPointer<n;startPointer++)    {
-            if(end[endPointer]>start[startPointer]) {
-                ans+=1;
-            }
-            else    {
-                endPointer++;
-            }
+        
+        Arrays.sort(startTime);
+        Arrays.sort(endTime);
+        
+        int end = 0;
+        for (int start = 0; start < n; start++) {
+            if (startTime[start] < endTime[end])
+                ans++;
+            else
+                end++;
         }
+        
         return ans;
     }
 }
