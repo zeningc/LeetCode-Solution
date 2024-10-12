b2c9f2e69cc47016153076c4eb95d73acf0ec33a
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-11 22:03:09 -0500

    Time: 29 ms (48.84%), Space: 48.5 MB (9.74%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.java
+++ 0218-the-skyline-problem/0218-the-skyline-problem.java
@@ -1,36 +1,24 @@
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
-        TreeMap<Integer, List<int[]>> m = new TreeMap<>();
+        TreeMap<Integer, List<int[]>> points = new TreeMap<>();
         for (int[] building : buildings)    {
-            int lo = building[0];
-            int hi = building[1];
-            int h = building[2];
-            m.computeIfAbsent(lo, x -> new ArrayList<>()).add(new int[] {h, 1});
-            m.computeIfAbsent(hi, x -> new ArrayList<>()).add(new int[] {h, -1});
+            points.computeIfAbsent(building[0], x -> new ArrayList<>()).add(new int[] {building[2], 1});
+            points.computeIfAbsent(building[1], x -> new ArrayList<>()).add(new int[] {building[2], -1});
         }
-        List<List<Integer>> ans = new ArrayList<>();
+        
         TreeMap<Integer, Integer> freq = new TreeMap<>();
-        int pre = -1;
-        for (Map.Entry<Integer, List<int[]>> e : m.entrySet())  {
-            int pos = e.getKey();
-            List<int[]> list = e.getValue();
-            for (int[] l : list)  {
-                int h = l[0];
-                int op = l[1];
-                if (op == 1)    {
-                    freq.put(h, freq.getOrDefault(h, 0) + 1);
-                }
-                else    {
-                    freq.put(h, freq.getOrDefault(h, 0) - 1);
-                }
-                
-                if (freq.get(h) == 0)
-                    freq.remove(h);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (Map.Entry<Integer, List<int[]>> entry : points.entrySet())   {
+            int pos = entry.getKey();
+            List<int[]> list = entry.getValue();
+            for (int[] cur : list)  {
+                freq.put(cur[0], freq.getOrDefault(cur[0], 0) + cur[1]);
+                if (freq.get(cur[0]) == 0)
+                    freq.remove(cur[0]);
             }
             int cur = freq.isEmpty() ? 0 : freq.lastKey();
-            if (cur != pre)
+            if (ans.isEmpty() || cur != ans.get(ans.size() - 1).get(1))
                 ans.add(List.of(pos, cur));
-            pre = cur;
         }
         
         return ans;
