5977ed274e31ede6efacf175ccc2910cda4e3d6a
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-13 12:18:52 -0500

    Time: 71 ms (94.61%), Space: 76.6 MB (30.65%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -1,95 +1,41 @@
 class NumArray {
-    SegmentTree segTree;
+    int[] tree;
+    int n;
+    int[] nums;
     public NumArray(int[] nums) {
-        segTree = new SegmentTree(0, nums.length - 1, 0);
-        for (int i = 0; i < nums.length; i++)    {
-            segTree.update(i, nums[i]);
+        this.nums = nums;
+        n = nums.length;
+        tree = new int[n + 1];
+        for (int i = 1; i <= n; i++)    {
+            tree[i] += nums[i - 1];
+            int nxt = i + (i & -i);
+            if (nxt <= n)
+                tree[nxt] += tree[i];
         }
     }
     
     public void update(int index, int val) {
-        segTree.update(index, val);
+        int delta = val - nums[index];
+        nums[index] = val;
+        int p = index + 1;
+        while (p <= n)  {
+            tree[p] += delta;
+            p += (p & -p);
+        }
+    }
+    
+    private int preSum(int i)   {
+        int p = i;
+        int sum = 0;
+        while (p > 0)   {
+            sum += tree[p];
+            p -= (p & -p);
+        }
+        return sum;
     }
     
     public int sumRange(int left, int right) {
-        return segTree.query(left, right);
-    }
-}
-
-class SegmentTree   {
-    Node root;
-    public SegmentTree(int lo, int hi, int initVal)  {
-        root = buildTree(lo, hi, initVal);
-    }
-    
-    private Node buildTree(int lo, int hi, int initVal)  {
-        if (lo == hi)   {
-            return new Node(lo, hi, initVal);
-        }
-        
-        int mid = lo + (hi - lo) / 2;
-        Node left = buildTree(lo, mid, initVal);
-        Node right = buildTree(mid + 1, hi, initVal);
-        return new Node(lo, hi, initVal, left, right);
-    }
-    
-    public void update(int idx, int val)    {
-        update(root, idx, val);
-    }
-    
-    private void update(Node node, int idx, int val)   {
-        if (idx == node.lo && idx == node.hi)   {
-            node.sum = val;
-            return;
-        }
-        int mid = node.lo + (node.hi - node.lo) / 2;
-        
-        if (idx <= mid)
-            update(node.left, idx, val);
-        else
-            update(node.right, idx, val);
-        node.sum = node.left.sum + node.right.sum;
-    }
-    
-    public int query(int lo, int hi)    {
-        return query(root, lo, hi);
-    }
-    
-    private int query(Node node, int lo, int hi)    {
-        if (lo == node.lo && hi == node.hi)    {
-            return node.sum;
-        }
-        
-        int mid = node.lo + (node.hi - node.lo) / 2;
-        if (hi <= mid)  {
-            return query(node.left, lo, hi);
-        }
-        else if (lo > mid)  {
-            return query(node.right, lo, hi);
-        }
-        return query(node.left, lo, mid) + query(node.right, mid + 1, hi);
-    }
-}
-
-class Node  {
-    int sum;
-    int lo;
-    int hi;
-    Node left;
-    Node right;
-    
-    public Node(int lo, int hi, int sum)   {
-        this.lo = lo;
-        this.hi = hi;
-        this.sum = sum;
-    }
-    
-    public Node(int lo, int hi, int sum, Node left, Node right)   {
-        this.lo = lo;
-        this.hi = hi;
-        this.left = left;
-        this.right = right;
-        this.sum = sum;
+        return preSum(right + 1) - preSum(left);
     }
 }
 
