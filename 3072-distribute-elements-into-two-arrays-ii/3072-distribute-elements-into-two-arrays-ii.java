4d5ec14cdf1cf228c6f38186e8909f626407c587
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-13 12:38:56 -0500

    Time: 210 ms (61.82%), Space: 71.8 MB (21.82%) - LeetHub

--- 3072-distribute-elements-into-two-arrays-ii/3072-distribute-elements-into-two-arrays-ii.java
+++ 3072-distribute-elements-into-two-arrays-ii/3072-distribute-elements-into-two-arrays-ii.java
@@ -1,143 +1,95 @@
 class Solution {
     public int[] resultArray(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums)
+            pq.offer(num);
         Map<Integer, Integer> numToId = new HashMap<>();
-        int[] sortedNums = nums.clone();
-        Arrays.sort(sortedNums);
         int cnt = 0;
-        for (int num : sortedNums)    {
-            if (numToId.containsKey(num))
+        while (!pq.isEmpty())   {
+            int pop = pq.poll();
+            if (numToId.containsKey(pop))
                 continue;
-            numToId.put(num, cnt);
-            cnt++;
+            numToId.put(pop, cnt++);
         }
         
-        List<Integer> arr1 = new ArrayList<>();
-        List<Integer> arr2 = new ArrayList<>();
-        
-        SegmentTree st1 = new SegmentTree(0, cnt, 0);
-        SegmentTree st2 = new SegmentTree(0, cnt, 0);
-        
-        arr1.add(nums[0]);
-        arr2.add(nums[1]);
-        st1.update(numToId.get(nums[0]), numToId.get(nums[0]), 1);
-        st2.update(numToId.get(nums[1]), numToId.get(nums[1]), 1);
-        for (int i = 2; i < nums.length; i++)   {
-            int curId = numToId.get(nums[i]);
-            int cnt1 = st1.query(curId + 1, cnt);
-            int cnt2 = st2.query(curId + 1, cnt);
+        int[] tree1 = new int[cnt + 1];
+        int[] tree2 = new int[cnt + 1];
+        List<Integer> num1 = new ArrayList<>();
+        List<Integer> num2 = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++)   {
+            if (i == 0) {
+                num1.add(nums[i]);
+                update(tree1, numToId.get(nums[i]), 1);
+                continue;
+            }
+            if (i == 1) {
+                num2.add(nums[i]);
+                update(tree2, numToId.get(nums[i]), 1);
+                continue;
+            }
+            
+            int cnt1 = rangeQuery(tree1, numToId.get(nums[i]) + 1, cnt - 1);
+            int cnt2 = rangeQuery(tree2, numToId.get(nums[i]) + 1, cnt - 1);
             if (cnt1 > cnt2)    {
-                arr1.add(nums[i]);
-                st1.update(curId, curId, 1);
+                num1.add(nums[i]);
+                update(tree1, numToId.get(nums[i]), 1);
             }
-            else if (cnt2 > cnt1)   {
-                arr2.add(nums[i]);
-                st2.update(curId, curId, 1);
+            else if (cnt1 < cnt2)   {
+                num2.add(nums[i]);
+                update(tree2, numToId.get(nums[i]), 1);
             }
-            else if (cnt1 == cnt2)  {
-                if (arr1.size() > arr2.size())  {
-                    arr2.add(nums[i]);
-                    st2.update(curId, curId, 1);
-                }
-                else    {
-                    arr1.add(nums[i]);
-                    st1.update(curId, curId, 1);
-                }
+            else if (num1.size() < num2.size())   {
+                num1.add(nums[i]);
+                update(tree1, numToId.get(nums[i]), 1);
             }
+            else if (num1.size() > num2.size())   {
+                num2.add(nums[i]);
+                update(tree2, numToId.get(nums[i]), 1);
+            }
+            else    {
+                num1.add(nums[i]);
+                update(tree1, numToId.get(nums[i]), 1);
+            }
+            
         }
         
-        int[] ret = new int[nums.length];
-        for (int i = 0; i < arr1.size(); i++)
-            ret[i] = arr1.get(i);
+        int[] ans = new int[nums.length];
+        int i = 0;
+        for (int num : num1)
+            ans[i++] = num;
+        for (int num : num2)
+            ans[i++] = num;
+        return ans;
+    }
+    
+    
+    void update(int[] tree, int idx, int delta) {
+        int n = tree.length;
         
-        for (int i = 0; i < arr2.size(); i++)
-            ret[i + arr1.size()] = arr2.get(i);
+        int p = idx + 1;
+        while (p < n)   {
+            tree[p] += delta;
+            p += (p & -p);
+        }
         
-        return ret;
+        return;
+    }
+    
+    int presum(int[] tree, int idx)    {
+        int p = idx;
+        int sum = 0;
+        while (p > 0)   {
+            sum += tree[p];
+            p -= (p & -p);
+        }
+        
+        return sum;
+    }
+    
+    int rangeQuery(int[] tree, int left, int right)    {
+        if (left > right)
+            return 0;
+        return presum(tree, right + 1) - presum(tree, left);
     }
 }
 
-class SegmentTree   {
-    Node root;
-    
-    public SegmentTree(int lo, int hi, int val)    {
-        
-        this.root = buildTree(lo, hi, val);
-    }
-    
-    private Node buildTree(int lo, int hi, int val)  {
-        if (lo == hi)
-            return new Node(lo, hi, val);
-        int mid = lo + (hi - lo) / 2;
-        Node cur = new Node(lo, hi, val);
-        cur.left = buildTree(lo, mid, val);
-        cur.right = buildTree(mid + 1, hi, val);
-        cur.val = cur.left.val + cur.right.val;
-        return cur;
-    }
-    
-    public void update(int lo, int hi, int val)  {
-        update(root, lo, hi, val);
-    }
-    
-    private void update(Node node, int lo, int hi, int val)  {
-        if (node.lo == lo && node.hi == hi) {
-            node.lazyTag += val;
-            node.val += val;
-            return;
-        }
-        
-        updateLazyTag(node);
-        
-        int mid = node.lo + (node.hi - node.lo) / 2;
-        if (lo <= mid)
-            update(node.left, lo, Math.min(hi, mid), val);
-        if (hi > mid)
-            update(node.right, Math.max(mid + 1, lo), hi, val);
-        node.val = node.left.val + node.right.val;
-    }
-    
-    public int query(int lo, int hi)  {
-        return query(root, lo, hi);
-    }
-    
-    private int query(Node node, int lo, int hi)  {
-        if (node.lo == lo && node.hi == hi) {
-            return node.val;
-        }
-        
-        updateLazyTag(node);
-        int ret = 0;
-        int mid = node.lo + (node.hi - node.lo) / 2;
-        if (lo <= mid)
-            ret += query(node.left, lo, Math.min(hi, mid));
-        if (hi > mid)
-            ret += query(node.right, Math.max(mid + 1, lo), hi);
-        node.val = node.left.val + node.right.val;
-        return ret;
-    }
-    
-    private void updateLazyTag(Node node)    {
-        if (node.lazyTag != 0 && node.left != null && node.right != null)  {
-            node.left.val += node.lazyTag * (node.left.hi - node.left.lo + 1);
-            node.right.val += node.lazyTag * (node.right.hi - node.right.lo + 1);
-            node.left.lazyTag += node.lazyTag;
-            node.right.lazyTag += node.lazyTag;
-            node.lazyTag = 0;
-        }
-    }
-}
-
-class Node  {
-    int val;
-    Node left;
-    Node right;
-    int lo;
-    int hi;
-    int lazyTag;
-    
-    public Node(int lo, int hi, int val)    {
-        this.lo = lo;
-        this.hi = hi;
-        this.val = val;
-    }
-}
