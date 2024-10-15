2901dd6be7000dd73c60f35ea7328b9f99f5e72c
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-14 22:14:50 -0500

    Time: 7 ms (37.69%), Space: 42.1 MB (59.83%) - LeetHub

--- 0772-basic-calculator-iii/0772-basic-calculator-iii.java
+++ 0772-basic-calculator-iii/0772-basic-calculator-iii.java
@@ -1,58 +1,70 @@
 class Solution {
     Map<Character, Integer> priority = Map.of(
-            '(', 0,
-            ')', 0,
-            '+', 1,
-            '-', 1,
-            '*', 2,
-            '/', 2
+        '(', 0,
+        ')', 0,
+        '+', 1,
+        '-', 1,
+        '*', 2,
+        '/', 2
     );
     public int calculate(String s) {
-        Deque<Character> ops = new LinkedList<>();
-        Deque<Integer> nums = new LinkedList<>();
-        nums.push(0);
+        Deque<Character> opStack = new LinkedList<>();
+        Deque<Integer> numStack = new LinkedList<>();
+        numStack.push(0);
         s = s.replace(" ", "");
-        for (int i = 0; i < s.length(); i++)    {
-            char c = s.charAt(i);
+        int idx = 0;
+        int n = s.length();
+        while (idx < n) {
+            char c = s.charAt(idx);
+            
             if (c == '(')   {
-                ops.push('(');
+                opStack.push('(');
+                idx++;
                 continue;
             }
-
+            
             if (c == ')')   {
-                while (ops.peek() != '(')   {
-                    calc(ops, nums);
+                while (opStack.peek() != '(')   {
+                    calc(numStack, opStack);
                 }
-                ops.pop();
+                opStack.pop();
+                idx++;
                 continue;
             }
-
-            if (Character.isDigit(c)) {
-                int j = i;
+            
+            if (Character.isDigit(c))   {
+                int j = idx;
                 int num = 0;
-                while (j < s.length() && Character.isDigit(s.charAt(j)))  {
-                    num = num * 10 + s.charAt(j) - '0';
+                while (j < n && Character.isDigit(s.charAt(j))) {
+                    num = num * 10 + (s.charAt(j) - '0');
                     j++;
                 }
-                nums.push(num);
-                i = j - 1;
+                numStack.push(num);
+                idx = j;
                 continue;
             }
-            if (i > 0 && (s.charAt(i - 1) == '(' || s.charAt(i - 1) == '+' || s.charAt(i - 1) == '-' ))
-                nums.push(0);
-            while (!ops.isEmpty() && priority.get(ops.peek()) >= priority.get(c))
-                calc(ops, nums);
             
-            ops.push(c);
+            
+            if (c == '-' || c == '+' || c == '*' || c == '/')   {
+                if (idx > 0 && (s.charAt(idx - 1) == '(' || s.charAt(idx - 1) == '+' || s.charAt(idx - 1) == '-'))
+                    numStack.push(0);
+                while (!opStack.isEmpty() && priority.get(c) <= priority.get(opStack.peek()))
+                    calc(numStack, opStack);
+                opStack.push(c);
+                idx++;
+                continue;
+            }
+            
+            
         }
-
-        while (!ops.isEmpty())
-            calc(ops, nums);
-
-        return nums.pop();
+        
+        while (!opStack.isEmpty())
+            calc(numStack, opStack);
+        
+        return numStack.pop();
     }
-
-    void calc(Deque<Character> ops, Deque<Integer> nums)    {
+    
+    void calc(Deque<Integer> nums, Deque<Character> ops)    {
         int num2= nums.pop();
         int num1 = nums.pop();
         char c = ops.pop();
