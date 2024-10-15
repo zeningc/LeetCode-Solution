fa1a3a50e77240f31935cfad8b05435bf0653425
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-15 17:03:42 -0500

    Time: 53 ms (73.47%), Space: 77.4 MB (37.66%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.java
+++ 0815-bus-routes/0815-bus-routes.java
@@ -2,39 +2,40 @@ class Solution {
     public int numBusesToDestination(int[][] routes, int source, int target) {
         if (source == target)
             return 0;
+        Map<Integer, List<Integer>> stopToRoute = new HashMap<>();
         
-        Map<Integer, List<Integer>> stopToRoutes = new HashMap<>();
-        for (int i = 0; i < routes.length; i++)  {
+        for (int i = 0; i < routes.length; i++) {
             int[] route = routes[i];
-            for (int r : route)
-                stopToRoutes.computeIfAbsent(r, x -> new ArrayList<>()).add(i);
-            
+            for (int r : route) {
+                stopToRoute.computeIfAbsent(r, x -> new ArrayList<>()).add(i);
+            }
         }
-        int dist = 0;
-        Set<Integer> vis = new HashSet<>();
+        
         Deque<Integer> q = new LinkedList<>();
+        Set<Integer> visStop = new HashSet<>();
         Set<Integer> visRoute = new HashSet<>();
         q.offer(source);
-        vis.add(source);
-        
+        int dist = 0;
         while (!q.isEmpty())    {
             int size = q.size();
             while (size-- > 0)  {
                 int u = q.poll();
+                if (visStop.contains(u))
+                    continue;
+                visStop.add(u);
                 
                 if (u == target)
                     return dist;
                 
-                for (int idx : stopToRoutes.getOrDefault(u, new ArrayList<>()))   {
-                    if (visRoute.contains(idx))
+                for (int route : stopToRoute.getOrDefault(u, new ArrayList<>()))   {
+                    if (visRoute.contains(route))
                         continue;
-                    for (int v : routes[idx]) {
-                        if (vis.contains(v))
+                    visRoute.add(route);
+                    for (int v : routes[route]) {
+                        if (visStop.contains(v))
                             continue;
-                        vis.add(v);
                         q.offer(v);
                     }
-                    visRoute.add(idx);
                 }
             }
             dist++;
