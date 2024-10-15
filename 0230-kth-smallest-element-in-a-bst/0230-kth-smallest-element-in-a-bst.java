f11db90ca30a8498be5b0d1dd1d71d1c7bd6f68e
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-15 17:15:59 -0500

    Time: 2 ms (10.60%), Space: 44.5 MB (25.56%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -15,34 +15,42 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
+        int target = dfs(root) - k + 1;
         TreeNode p1 = root;
         int cnt = 0;
         while (p1 != null)  {
-            TreeNode p2 = p1.left;
+            TreeNode p2 = p1.right;
             if (p2 != null) {
-                while (p2.right != null && p2.right != p1)
-                    p2 = p2.right;
+                while (p2.left != null && p2.left != p1)
+                    p2 = p2.left;
                 
-                if (p2.right != p1) {
-                    p2.right = p1;
-                    p1 = p1.left;
+                if (p2.left != p1) {
+                    p2.left = p1;
+                    p1 = p1.right;
                     continue;
                 }
                 
                 cnt++;
-                if (cnt == k)
+                if (cnt == target)
                     return p1.val;
-                p2.right = null;
-                p1 = p1.right;
+                p2.left = null;
+                p1 = p1.left;
                 continue;
             }
             
             cnt++;
-            if (cnt == k)
+            if (cnt == target)
                 return p1.val;
-            p1 = p1.right;
+            p1 = p1.left;
         }
         
         return -1;
+    }
+    
+    int dfs(TreeNode node)  {
+        if (node == null)
+            return 0;
+        int cnt = 0;
+        return dfs(node.left) + dfs(node.right) + 1;
     }
 }
