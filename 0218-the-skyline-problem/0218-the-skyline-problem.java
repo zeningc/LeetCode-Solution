277b885d505757109a3faa284f57d07fee0b64a9
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-15 17:46:32 -0500

    Time: 26 ms (57.11%), Space: 48.5 MB (9.31%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.java
+++ 0218-the-skyline-problem/0218-the-skyline-problem.java
@@ -1,24 +1,32 @@
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
-        TreeMap<Integer, List<int[]>> points = new TreeMap<>();
+        TreeMap<Integer, List<int[]>> map = new TreeMap<>();
         for (int[] building : buildings)    {
-            points.computeIfAbsent(building[0], x -> new ArrayList<>()).add(new int[] {building[2], 1});
-            points.computeIfAbsent(building[1], x -> new ArrayList<>()).add(new int[] {building[2], -1});
+            map.computeIfAbsent(building[0], x -> new ArrayList<>()).add(new int[] {building[2], 1});
+            map.computeIfAbsent(building[1], x -> new ArrayList<>()).add(new int[] {building[2], -1});
         }
         
         TreeMap<Integer, Integer> freq = new TreeMap<>();
+        
         List<List<Integer>> ans = new ArrayList<>();
-        for (Map.Entry<Integer, List<int[]>> entry : points.entrySet())   {
-            int pos = entry.getKey();
+        int pre = -1;
+        for (Map.Entry<Integer, List<int[]>> entry : map.entrySet())    {
             List<int[]> list = entry.getValue();
-            for (int[] cur : list)  {
-                freq.put(cur[0], freq.getOrDefault(cur[0], 0) + cur[1]);
-                if (freq.get(cur[0]) == 0)
-                    freq.remove(cur[0]);
+            
+            for (int[] l : list)    {
+                if (l[1] == 1)  {
+                    freq.put(l[0], freq.getOrDefault(l[0], 0) + 1);
+                    continue;
+                }
+                
+                freq.put(l[0], freq.getOrDefault(l[0], 0) - 1);
+                if (freq.get(l[0]) == 0)
+                    freq.remove(l[0]);
             }
             int cur = freq.isEmpty() ? 0 : freq.lastKey();
-            if (ans.isEmpty() || cur != ans.get(ans.size() - 1).get(1))
-                ans.add(List.of(pos, cur));
+            if (cur != pre)
+                ans.add(List.of(entry.getKey(), cur));
+            pre = cur;
         }
         
         return ans;
