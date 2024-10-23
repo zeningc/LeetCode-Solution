83b0d6cceda97cd40792789209ca4f01518476b6
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-23 12:52:55 -0500

    Time: 52 ms (21.67%), Space: 47.6 MB (45.08%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,23 +1,32 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return helper(nums, k) - helper(nums, k - 1);
-    }
-    
-    int helper(int[] nums, int k)   {
-        int cnt = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        int lo = 0;
-        for (int hi = 0; hi < nums.length; hi++) {
-            map.put(nums[hi], map.getOrDefault(nums[hi], 0) + 1);
-            while (map.size() > k)  {
-                map.put(nums[lo], map.get(nums[lo]) - 1);
-                if (map.get(nums[lo]) == 0)
-                    map.remove(nums[lo]);
-                lo++;
+        int p = 0;
+        int q = 0;
+        Map<Integer, Integer> freq1 = new HashMap<>();
+        Map<Integer, Integer> freq2 = new HashMap<>();
+        int n = nums.length;
+        int ans = 0;
+        for (int hi = 0; hi < n; hi++)  {
+            freq1.put(nums[hi], freq1.getOrDefault(nums[hi], 0) + 1);
+            freq2.put(nums[hi], freq2.getOrDefault(nums[hi], 0) + 1);
+            
+            while (freq1.size() >= k)    {
+                freq1.put(nums[p], freq1.get(nums[p]) - 1);
+                if (freq1.get(nums[p]) == 0)
+                    freq1.remove(nums[p]);
+                p++;
             }
-            cnt += hi - lo + 1;
+            
+            while (freq2.size() >= k + 1)    {
+                freq2.put(nums[q], freq2.get(nums[q]) - 1);
+                if (freq2.get(nums[q]) == 0)
+                    freq2.remove(nums[q]);
+                q++;
+            }
+            
+            ans += p - q;
         }
         
-        return cnt;
+        return ans;
     }
 }
