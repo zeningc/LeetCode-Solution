740bba34c366395ee3d1235c3634317ae2aae815
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-26 21:24:57 -0500

    Time: 58 ms (21.33%), Space: 58.8 MB (42.37%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,19 +1,21 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        long sum = 0;
-        int lo = 0;
         long ans = 0;
-        Set<Integer> set = new HashSet<>();
+        long curSum = 0;
+        Map<Integer, Integer> freq = new HashMap<>();
+        int lo = 0;
         for (int hi = 0; hi < nums.length; hi++)    {
-            while (hi - lo + 1 > k || set.contains(nums[hi]))   {
-                set.remove(nums[lo]);
-                sum -= nums[lo];
+            freq.put(nums[hi], freq.getOrDefault(nums[hi], 0) + 1);
+            curSum += nums[hi];
+            if (hi - lo + 1 > k)    {
+                freq.put(nums[lo], freq.getOrDefault(nums[lo], 0) - 1);
+                if (freq.get(nums[lo]) == 0)
+                    freq.remove(nums[lo]);
+                curSum -= nums[lo];
                 lo++;
             }
-            set.add(nums[hi]);
-            sum += nums[hi];
-            if (hi - lo + 1 == k)
-                ans = Math.max(ans, sum);
+            if (freq.size() == k)
+                ans = Math.max(ans, curSum);
         }
         
         return ans;
