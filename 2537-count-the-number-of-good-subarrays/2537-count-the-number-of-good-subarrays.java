adb0b844a1e960a7c19fd62a037e58e0f0553397
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-27 09:54:09 -0500

    Time: 57 ms (65.00%), Space: 58.5 MB (71.47%) - LeetHub

--- 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.java
+++ 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.java
@@ -1,23 +1,19 @@
 class Solution {
     public long countGood(int[] nums, int k) {
-        long ans = 0;
         Map<Integer, Integer> freq = new HashMap<>();
+        long cnt = 0;
+        long ans = 0;
         int lo = 0;
-        long pair = 0;
-        for (int hi = 0; hi < nums.length; hi++)    {
-            int oldFreq = freq.getOrDefault(nums[hi], 0);
-            pair -= (long)oldFreq * (oldFreq - 1) / 2;
-            pair += (long)(oldFreq + 1) * oldFreq / 2;
-            freq.put(nums[hi], oldFreq + 1);
+        for (int hi = 0; hi < nums.length; hi++)   {
+            if (freq.containsKey(nums[hi]))
+                cnt += freq.get(nums[hi]);
+            freq.put(nums[hi], freq.getOrDefault(nums[hi], 0) + 1);
             
-            while (pair >= k)   {
-                oldFreq = freq.get(nums[lo]);
-                pair -= (long)oldFreq * (oldFreq - 1) / 2;
-                pair += (long)(oldFreq - 1) * (oldFreq - 2) / 2;
-                if (oldFreq - 1 == 0)
+            while (cnt >= k)    {
+                freq.put(nums[lo], freq.get(nums[lo]) - 1);
+                cnt -= freq.get(nums[lo]);
+                if (freq.get(nums[lo]) == 0)
                     freq.remove(nums[lo]);
-                else
-                    freq.put(nums[lo], oldFreq - 1);
                 lo++;
             }
             
@@ -27,10 +23,3 @@ class Solution {
         return ans;
     }
 }
-
-/*
-1 in a subarray, how to calculate the number of "good" pair?
-nums[i] == nums[j] == nums[k] => (i, j) (i, k), (j, k) => 3
-n * (n + 1) / 2 => n: number of same element in the array
-2. [lo, hi] is not good but [0.. lo - 1, hi] is good
-*/
