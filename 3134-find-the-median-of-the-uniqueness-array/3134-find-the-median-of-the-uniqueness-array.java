cf668fb95f47a0285785b1720a6aaaf0a565d707
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-10-31 20:17:29 -0500

    Time: 655 ms (28.57%), Space: 57.9 MB (65.00%) - LeetHub

--- 3134-find-the-median-of-the-uniqueness-array/3134-find-the-median-of-the-uniqueness-array.java
+++ 3134-find-the-median-of-the-uniqueness-array/3134-find-the-median-of-the-uniqueness-array.java
@@ -1,12 +1,12 @@
 class Solution {
     public int medianOfUniquenessArray(int[] nums) {
         int n = nums.length;
-        long total = (long)n * (n + 1) / 2;
+        long target = ((long)n * (n + 1) / 2 + 1) / 2;
         int lo = 1;
         int hi = n;
         while (lo <= hi)    {
             int mid = lo + (hi - lo) / 2;
-            if (check(nums, mid, total))
+            if (check(nums, mid) >= target)
                 hi = mid - 1;
             else
                 lo = mid + 1;
@@ -15,29 +15,28 @@ class Solution {
         return lo;
     }
     
-    boolean check(int[] nums, int mid, long total)   {
-        long target = (total + 1) / 2;
+    long check(int[] nums, int n) {
+        long ans = 0;
         int lo = 0;
-        long cnt = 0;
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> freq = new HashMap<>();
         for (int hi = 0; hi < nums.length; hi++)    {
-            map.put(nums[hi], map.getOrDefault(nums[hi], 0) + 1);
-            while (map.size() > mid) {
-                map.put(nums[lo], map.get(nums[lo]) - 1);
-                if (map.get(nums[lo]) == 0)
-                    map.remove(nums[lo]);
+            freq.put(nums[hi], freq.getOrDefault(nums[hi], 0) + 1);
+            while (freq.size() > n)  {
+                freq.put(nums[lo], freq.get(nums[lo]) - 1);
+                if (freq.get(nums[lo]) == 0)
+                    freq.remove(nums[lo]);
                 lo++;
             }
-            
-            cnt += (hi - lo + 1);
+            ans += (hi - lo + 1);
         }
-        
-        return cnt >= target;
+        return ans;
     }
 }
-// 4 3 5 4
-// 43 35 54
-// 435 354
-// 4354
-    
-// 1 1 1 1 2 2 2 3 3 3
+
+
+
+/*
+x x x x x o
+
+
+*/
