248d4e3980f2ef0228a5fff72d68fbba5d266c82
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-11-02 13:35:50 -0500

    Time: 155 ms (30.86%), Space: 156.8 MB (14.58%) - LeetHub

--- 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.java
+++ 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.java
@@ -1,60 +1,67 @@
 class Solution {
     public int[] maximizeXor(int[] nums, int[][] queries) {
-        int n = nums.length;
-        int m = queries.length;
-        int[] ans = new int[m];
+        Trie root = new Trie(-1);
+        
         Arrays.sort(nums);
         List<Integer> idx = new ArrayList<>();
-        for (int i = 0; i < m; i++)
+        for (int i = 0; i < queries.length; i++)
             idx.add(i);
         Collections.sort(idx, (a, b) -> queries[a][1] - queries[b][1]);
-        Trie root = new Trie('#');
-        int k = 0;
-        for (int i : idx) {
-            while (k < nums.length && nums[k] <= queries[i][1]) {
-                Trie node = root;
-                for (int j = 31; j >= 0; j--)   {
-                    int c = (nums[k] & (1 << j)) == 0 ? 0 : 1;
-                    if (node.children[c] == null)
-                        node.children[c] = new Trie((char)(c + '0'));
-                    node = node.children[c];
+        
+        int[] ans = new int[queries.length];
+        int p = 0;
+        for (int q = 0; q < queries.length; q++)    {
+            Trie node;
+            int curIdx = idx.get(q);
+            while (p < nums.length && nums[p] <= queries[curIdx][1])   {
+                node = root;
+                for (int i = 31; i >= 0; i--)   {
+                    int d = (nums[p] & (1 << i)) == 0 ? 0 : 1;
+                    if (node.children[d] == null)
+                        node.children[d] = new Trie(d);
+                    node = node.children[d];
                 }
-                node.num = nums[k];
-                k++;
+                node.num = nums[p];
+                p++;
             }
-            int x = queries[i][0];
-            Trie node = root;
-            for (int j = 31; j >= 0; j--)   {
-                if (node == null)
-                    break;
-                int c = (x & (1 << j)) == 0 ? 0 : 1;
-                if (c == 1)   {
-                    if (node.children[0] != null)   {
-                        node = node.children[0];
+            
+            if (p == 0) {
+                ans[curIdx] = -1;
+                continue;
+            }
+            
+            node = root;
+            for (int i = 31; i >= 0; i--)    {
+                int curDigit = (queries[curIdx][0] & (1 << i)) == 0 ? 0 : 1;
+                if (curDigit == 0)  {
+                    if (node.children[1] != null)   {
+                        node = node.children[1];
                         continue;
                     }
-                    node = node.children[1];
+                    node = node.children[0];
                     continue;
                 }
-                if (node.children[1] != null)   {
-                    node = node.children[1];
+                
+                if (node.children[0] != null)   {
+                    node = node.children[0];
                     continue;
                 }
-                node = node.children[0];
+                node = node.children[1];
             }
-            ans[i] = node == null ? -1 : node.num ^ x;
+            ans[curIdx] = queries[curIdx][0] ^ node.num;
         }
-        
         return ans;
     }
+    
 }
+
 class Trie  {
+    int d;
     Trie[] children;
-    char c;
     int num;
-    public Trie(char c)   {
+    
+    public Trie(int d)  {
+        this.d = d;
         children = new Trie[2];
-        this.c = c;
-        this.num = -1;
     }
 }
