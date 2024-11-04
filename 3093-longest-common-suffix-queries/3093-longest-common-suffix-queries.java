490a9a1466147e0fbf48d96439d348820dec1b87
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-11-04 17:25:21 -0600

    Time: 144 ms (12.00%), Space: 213.1 MB (15.68%) - LeetHub

--- 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.java
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.java
@@ -1,21 +1,38 @@
 class Solution {
     public int[] stringIndices(String[] wordsContainer, String[] wordsQuery) {
         Trie root = new Trie('#');
-        for (int i = 0; i < wordsContainer.length; i++)
-            Trie.insertWord(root, wordsContainer[i], i);
         
-        int[] ans = new int[wordsQuery.length];
-        for (int i = 0; i < wordsQuery.length; i++) {
+        for (int i = 0; i < wordsContainer.length; i++)  {
+            String word = wordsContainer[i];
             Trie node = root;
-            String word = wordsQuery[i];
-            ans[i] = node.idx;
             for (int j = word.length() - 1; j >= 0; j--)   {
                 char c = word.charAt(j);
+                node.ids.add(i);
+                if (node.children[c - 'a'] == null)
+                    node.children[c - 'a'] = new Trie(c);
+                node = node.children[c - 'a'];
+            }
+            node.ids.add(i);
+        }
+        
+        int[] ans = new int[wordsQuery.length];
+        
+        for (int i = 0; i < wordsQuery.length; i++) {
+            Trie node = root;
+            for (int j = wordsQuery[i].length() - 1; j >= 0; j--)   {
+                char c = wordsQuery[i].charAt(j);
                 if (node.children[c - 'a'] == null)
                     break;
                 node = node.children[c - 'a'];
-                ans[i] = node.idx;
             }
+            
+            int ansIdx = -1;
+            for (int idx : node.ids)    {
+                if (ansIdx == -1 || wordsContainer[ansIdx].length() > wordsContainer[idx].length() || wordsContainer[ansIdx].length() == wordsContainer[idx].length() && idx < ansIdx)  {
+                    ansIdx = idx;
+                }
+            }
+            ans[i] = ansIdx;
         }
         
         return ans;
@@ -23,34 +40,13 @@ class Solution {
 }
 
 class Trie  {
-    int minLen;
-    int idx;
     char c;
     Trie[] children;
+    List<Integer> ids;
     
-    public Trie(char c)   {
+    public Trie(char c) {
         this.c = c;
         children = new Trie[26];
-        minLen = Integer.MAX_VALUE;
-        idx = Integer.MAX_VALUE;
-    }
-    
-    static void insertWord(Trie root, String word, int idx)  {
-        int len = word.length();
-        Trie node = root;
-        for (int i = word.length() - 1; i >= 0; i--)    {
-            if (node.minLen > len || node.minLen == len && idx < node.idx)  {
-                node.minLen = len;
-                node.idx = idx;
-            }
-            char c = word.charAt(i);
-            if (node.children[c - 'a'] == null)
-                node.children[c - 'a'] = new Trie(c);
-            node = node.children[c - 'a'];
-        }
-        if (node.minLen > len || node.minLen == len && idx < node.idx)  {
-            node.minLen = len;
-            node.idx = idx;
-        }
+        ids = new ArrayList<>();
     }
 }
