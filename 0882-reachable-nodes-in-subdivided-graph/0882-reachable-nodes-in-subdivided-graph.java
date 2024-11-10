bda7009beb874351449744a36aabe70315429b32
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-11-10 09:39:23 -0600

    Time: 34 ms (45.71%), Space: 50.5 MB (66.96%) - LeetHub

--- 0882-reachable-nodes-in-subdivided-graph/0882-reachable-nodes-in-subdivided-graph.java
+++ 0882-reachable-nodes-in-subdivided-graph/0882-reachable-nodes-in-subdivided-graph.java
@@ -1,73 +1,68 @@
 class Solution {
     public int reachableNodes(int[][] edges, int maxMoves, int n) {
         Map<Integer, List<int[]>> graph = new HashMap<>();
-        int nEdge = edges.length;
-        boolean[] visEdge = new boolean[nEdge];
+        for (int[] edge : edges)    {
+            int u = edge[0];
+            int v = edge[1];
+            int d = edge[2];
+            graph.computeIfAbsent(u, x -> new ArrayList<>()).add(new int[] {v, d});
+            graph.computeIfAbsent(v, x -> new ArrayList<>()).add(new int[] {u, d});
+        }
+        
         int[] vis = new int[n];
         Arrays.fill(vis, -1);
-        for (int i = 0; i < nEdge; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int w = edges[i][2];
-            if (!graph.containsKey(u))
-                graph.put(u, new LinkedList<>());
-            if (!graph.containsKey(v))
-                graph.put(v, new LinkedList<>());
-            graph.get(u).add(new int[] {v, i, w});
-            graph.get(v).add(new int[] {u, i, w});
-        }
-        int ans = 0;
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        pq.offer(new int[] {0, 0, -1});
-        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        pq.offer(new int[] {0, 0});
         while (!pq.isEmpty())   {
-            int[] curr = pq.poll();
-            int u = curr[1];
-            int currW = curr[0];
-            int fromEdge = curr[2];
+            int[] cur = pq.poll();
+            int u = cur[0];
+            int d = cur[1];
             if (vis[u] != -1)
                 continue;
-            vis[u] = currW;
-            if (fromEdge != -1) {
-                ans += edges[fromEdge][2] + 1;
-                visEdge[fromEdge] = true;
-            }
-            if (!graph.containsKey(u))   
+            vis[u] = d;
+            
+            if (!graph.containsKey(u))
                 continue;
-            for (int[] dest : graph.get(u)) {
-                int v = dest[0];
-                int e = dest[1];
-                int w = dest[2];
+            
+            for (int[] nxt : graph.get(u))  {
+                int v = nxt[0];
+                int nd = nxt[1];
+                
                 if (vis[v] != -1)
                     continue;
-                if (currW + w + 1 <= maxMoves)
-                    pq.offer(new int[] {currW + w + 1, v, e});
+                
+                pq.offer(new int[] {v, d + nd + 1});
             }
         }
         
-        for (int i = 0; i < n; i++) {
-            if (vis[i] == -1)
+        int ans = 0;
+        for (int i = 0; i < edges.length; i++)  {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int d = edges[i][2];
+            if ((vis[u] == -1 || vis[u] > maxMoves) && (vis[v] == -1 || vis[v] > maxMoves))
                 continue;
-            int remainMoves = maxMoves - vis[i];
-            if (!graph.containsKey(i))   
+            
+            if (vis[u] == -1 || vis[u] > maxMoves)   {
+                ans += Math.min(maxMoves - vis[v], d);
                 continue;
-            for (int[] dest : graph.get(i)) {
-                int v = dest[0];
-                int e = dest[1];
-                int w = dest[2];
-                if (visEdge[e])
-                    continue;
-                if (vis[v] == -1)   {
-                    ans += remainMoves;
-                    continue;
-                }
-                int vRemainMoves = maxMoves - vis[v];
-                ans += Math.min(edges[e][2], vRemainMoves + remainMoves);
-                visEdge[e] = true;
             }
+            
+            
+            if (vis[v] == -1 || vis[v] > maxMoves)   {
+                ans += Math.min(maxMoves - vis[u], d);
+                continue;
+            }
+            
+            
+            ans += Math.min(maxMoves - vis[u] + maxMoves - vis[v], d);
         }
         
-        return ans + 1;
+        for (int i = 0; i < n; i++)
+            if (vis[i] >= 0 && vis[i] <= maxMoves)
+                ans++;
         
+        return ans;
     }
 }
+
