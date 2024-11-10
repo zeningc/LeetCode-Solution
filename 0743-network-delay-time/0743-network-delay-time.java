119cfdb7f05fbe85fcc09f8345ddec26f5398bac
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-11-10 10:47:42 -0600

    Time: 15 ms (53.27%), Space: 48.5 MB (52.38%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,42 +1,35 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, -1);
-        Map<Integer, List<int[]>> edges = new HashMap<>();
-        for (int[] time : times)    {
-            if (!edges.containsKey(time[0]))
-                edges.put(time[0], new LinkedList<>());
-            edges.get(time[0]).add(new int[] {time[1], time[2]});
-        }
+        int cnt = 0;
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for (int[] time : times)
+            graph.computeIfAbsent(time[0], x -> new ArrayList<>()).add(new int[] {time[1], time[2]});
         
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        pq.offer(new int[] {k, 0});
-        dist[k] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         boolean[] vis = new boolean[n + 1];
-        while (!pq.isEmpty())    {
-            int[] curr = pq.poll();
-            int u = curr[0];
-            int currTime = curr[1];
+        pq.offer(new int[] {k, 0});
+        while (!pq.isEmpty())   {
+            int[] cur = pq.poll();
+            int u = cur[0];
+            int t = cur[1];
+            
             if (vis[u])
                 continue;
             vis[u] = true;
-            dist[u] = currTime;
-            if (edges.containsKey(u))   {
-                for (int[] dest : edges.get(u)) {
-                    int v = dest[0];
-                    int w = dest[1];
-                    if (vis[v])
-                        continue;
-                    pq.offer(new int[] {v, currTime + w});
-                }
+            cnt++;
+            if (cnt == n)
+                return t;
+            
+            for (int[] nxt : graph.getOrDefault(u, new ArrayList<>()))  {
+                int v = nxt[0];
+                int timeToTravel = nxt[1];
+                if (vis[v])
+                    continue;
+                pq.offer(new int[] {v, t + timeToTravel});
             }
+            
         }
-        int ans = -1;
-        for (int i = 1; i <= n; i++)    {
-            if (dist[i] == -1)
-                return -1;
-            ans = Math.max(ans, dist[i]);
-        }
-        return ans;
+        
+        return -1;
     }
 }
