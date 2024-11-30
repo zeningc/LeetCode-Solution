53b6399122389a06177988ad8357c01e6f09faf8
Zening Chen <59945561+zeningc@users.noreply.github.com>
2024-11-30 13:30:09 -0600

    Time: 8 ms (87.77%), Space: 45.1 MB (93.89%) - LeetHub

--- 1136-parallel-courses/1136-parallel-courses.java
+++ 1136-parallel-courses/1136-parallel-courses.java
@@ -1,43 +1,36 @@
 class Solution {
     public int minimumSemesters(int n, int[][] relations) {
-        Map<Integer, List<Integer>> edges = new HashMap<>();
+        List<Integer>[] map = new List[n + 1];
         int[] indeg = new int[n + 1];
-        
-        for (int i = 1; i < n + 1; i++) {
-            edges.put(i, new LinkedList<>());
+        for (int[] r : relations)   {
+            if (map[r[0]] == null)
+                map[r[0]] = new ArrayList<>();
+            map[r[0]].add(r[1]);
+            indeg[r[1]]++;
         }
         
-        for (int[] relation : relations)    {
-            int u = relation[0];
-            int v = relation[1];
-            edges.get(u).add(v);
-            indeg[v]++;
-        }
-        
-        Deque<Integer> queue = new LinkedList<>();
-        int count = 0;
-        for (int i = 1; i < n + 1; i++) {
-            if (indeg[i] == 0)  {
-                queue.offer(i);
-                count++;
-            }
-        }
+        Deque<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++)
+            if (indeg[i] == 0)
+                q.offer(i);
+        int cnt = 0;
         int ans = 0;
-        while (!queue.isEmpty())    {
-            int size = queue.size();
-            for (int i = 0; i < size; i++)  {
-                int u = queue.poll();
-                for (int v : edges.get(u))  {
+        while (!q.isEmpty())    {
+            int size = q.size();
+            while (size-- > 0)  {
+                int u = q.poll();
+                cnt++;
+                if (map[u] == null)
+                    continue;
+                for (int v : (List<Integer>)map[u])    {
                     indeg[v]--;
-                    if (indeg[v] == 0)  {
-                        count++;
-                        queue.offer(v);
-                    }
+                    if (indeg[v] == 0)
+                        q.offer(v);
                 }
             }
             ans++;
         }
         
-        return count == n ? ans : -1;
+        return cnt == n ? ans : -1;
     }
 }
