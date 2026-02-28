af8e6704458e29aaea4667eab03c7548bd3c1632
Zening Chen <59945561+zeningc@users.noreply.github.com>
2026-02-28 14:57:32 -0600

    Time: 0 ms (100%), Space: 44.2 MB (75.5%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo < hi)    {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] > nums[mid + 1])
+                hi = mid;
+            else
+                lo = mid + 1;
+        }
+
+        return lo;
+    }
+}
